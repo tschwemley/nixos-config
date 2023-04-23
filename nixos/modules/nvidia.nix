88c97287a134f59db41d49c19999dcfa8e660e58
Tyler Schwemley <tjschwem@gmail.com>
2023-04-23 14:47:19 -0400

    update nvidia config, fonts, stateversion

--- nixos/modules/nvidia.nix
+++ nixos/modules/nvidia.nix
@@ -8,8 +8,8 @@
   hardware.opengl.enable = true;
 
   # Optionally, you may need to select the appropriate driver version for your specific GPU.
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
 
   # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
-  hardware.nvidia.modesetting.enable = true;	
+  #hardware.nvidia.modesetting.enable = true;	
 }
