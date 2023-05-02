985ef55d6d1ab51e7ae97633b70db500bb0bf7a6
Tyler Schwemley <tjschwem@gmail.com>
2023-05-02 16:15:04 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -5,18 +5,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
 
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager = {
-		url = "github:nix-community/home-manager";
-		inputs.nixpkgs.follows = "nixpkgs";
-	};
+    nixos-generators.url = "github:nix-community/nixos-generators";
+	nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
 
-	nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+	nixos-hardware.url = "github:nixos/nixos-hardware";
+    #disko.url = "github:nix-community/disko";
+    #disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs @ {
@@ -24,6 +21,7 @@
     home-manager,
     flake-parts,
 	nixos-generators,
+	nixos-hardware,
     ...
   }:
     flake-parts.lib.mkFlake { inherit inputs; } {
