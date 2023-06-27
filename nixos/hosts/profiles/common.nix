ed4e263fc5d5d3dfba6e5136b9906026f86232cd
Tyler Schwemley <me@tylerschwemley.com>
2023-06-26 20:01:06 -0400

    fix broken paths

--- nixos/hosts/profiles/common.nix
+++ nixos/hosts/profiles/common.nix
@@ -20,9 +20,6 @@ in {
     wget
   ];
 
-  home-manager.useGlobalPkgs = true;
-  # home-manager.users.root = import homeConfigurations.common;
-  
   # TODO: I'm not sure if I want this enabled on every host or not. Possibly move out
   services.openssh.enable = true;
 }
