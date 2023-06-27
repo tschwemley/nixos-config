3acfb4bb73b7b57e8d10c3b85d044201c6d4e3a7
Tyler Schwemley <me@tylerschwemley.com>
2023-06-26 21:29:29 -0400

    wip

--- nixos/users/schwem.nix
+++ nixos/users/schwem.nix
@@ -7,15 +7,11 @@
     };
   };
 
-  # home-manager.users.schwem = (import ../../home/profiles/pc.nix {
-  # home-manager.users.schwem = (homeConfigurations.pc {
-	home-manager.users.schwem = {
-	  imports = [homeConfigurations.pc];
+  home-manager.users.schwem = (import ../../home/profiles/pc.nix {
 	  home.stateVersion = "23.05";
 	  home.username = "schwem";
 	  home.homeDirectory = "/home/schwem";
-	};
-  # });
+  });
   # home-manager.users.schwem = {
   #   imports = [
   #    (homeConfigurations.pc
