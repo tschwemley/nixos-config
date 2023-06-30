d44130eb45699bd27c9cd0a256bbd902ecc8142d
Tyler Schwemley <me@tylerschwemley.com>
2023-06-29 21:58:33 -0400

    update charizard function calls

--- nixos/users/k3s.nix
+++ nixos/users/k3s.nix
@@ -4,19 +4,11 @@
       isNormalUser = true;
       home = "/home/k3s";
       extraGroups = ["wheel" "k3s"];
-      # uid = 1000;
       # openssh.authorizedKeys.keys = keys;
     };
-
-    # root.openssh.authorizedKeys.keys = keys;
   };
 
   home-manager.users.k3s.imports = [
     ../../home/profiles/server.nix
   ];
-
-  # boot.initrd.network.ssh.authorizedKeys = keys;
-  # security.sudo.wheelNeedsPassword = false;
-
-  # imports = [ ./zsh.nix ];
 }
