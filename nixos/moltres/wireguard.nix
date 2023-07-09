e5d1f8af555f99c47458e84ea51ad9ab34c97dbc
Tyler Schwemley <me@tylerschwemley.com>
2023-07-09 11:44:16 -0400

    try another method to auto connect wg

--- nixos/moltres/wireguard.nix
+++ nixos/moltres/wireguard.nix
@@ -1,3 +1,6 @@
+let
+	publicKey = "DXGnAkpq/sM4Dw6vl26gmdggE8ItGi9H/61GQ8nWR14=";
+in
 {
   networking.firewall.allowedUDPPorts = [51820];
   networking.wireguard.interfaces = {
@@ -6,9 +9,13 @@
       listenPort = 51820;
       privateKeyFile = "/persist/wireguard/private";
 
+	  # use postUp instead of persistentKeepalive because persistentKeepalive doesn't auto-connect
+	  # to the peer. This is apparently because the private key is set after the persistentKeepalive
+	  # which happens because we're setting the private key from a file.
+	  postUp = ["wg set wgnet0 peer ${publicKey} persistent-keepalive 25"];
       peers = [
         {
-          publicKey = "DXGnAkpq/sM4Dw6vl26gmdggE8ItGi9H/61GQ8nWR14=";
+		  inherit publicKey; 
 
           # Forward all the traffic via VPN.
           allowedIPs = ["0.0.0.0/0"];
@@ -16,7 +23,7 @@
           endpoint = "wg.schwem.io:51820";
 
           # Send keepalives every 25 seconds. Important to keep NAT tables alive.
-          persistentKeepalive = 25;
+          # persistentKeepalive = 25;
         }
       ];
     };
