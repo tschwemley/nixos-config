1c7e8225fe9945f0ddbe4c67d3afb16dc5482f6c
Tyler Schwemley <me@tylerschwemley.com>
2023-07-15 14:14:07 -0400

    wip

---
+++ home/neovim/db.nix
@@ -0,0 +1,9 @@
+{ pkgs, ... }: {
+	programs.neovim = {
+		plugins = with pkgs.vimPlugins; [
+		  vim-dadbod
+		  vim-dadbod-ui
+		  vim-dadbod-completion
+		];
+	};
+}
--- home/neovim/lsp.nix
+++ home/neovim/lsp.nix
@@ -7,12 +7,15 @@
     nodePackages.yaml-language-server
   ];
 
-  programs.neovim.extraLuaConfig = "require 'schwem.lsp'";
-  programs.neovim.plugins = with pkgs.vimPlugins; [
-      lsp-zero-nvim
-      nvim-lspconfig
-	  # TODO: remove these if unnecesasry
-      #mason-nvim
-      #mason-lspconfig-nvim
-  ];
+  programs.neovim = {
+	  extraLuaConfig = "require 'schwem.lsp'";
+	  plugins = with pkgs.vimPlugins; [
+		  lsp-zero-nvim
+		  nvim-lspconfig
+		  # TODO: remove these if unnecesasry
+		  #mason-nvim
+		  #mason-lspconfig-nvim
+	  ];
+
+  };
 }
