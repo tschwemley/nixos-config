5f0b04642fd21b6e803b2bd85eb24db2b3d82d04
Tyler Schwemley <me@tylerschwemley.com>
2023-07-15 15:24:31 -0400

    update ip for k3s

--- nixos/modules/services/k3s/agent.nix
+++ nixos/modules/services/k3s/agent.nix
@@ -11,6 +11,6 @@
   services.k3s.tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
   services.k3s.serverAddr = lib.mkDefault "10.0.0.1:6443";
   # TODO: make this a passable argument or option value
-  services.k3s.extraFlags = "--node-ip 10.0.0.2 --container-runtime-endpoint unix:///run/containerd/containerd.sock";
+  services.k3s.extraFlags = "--node-external-ip 10.0.0.2 --container-runtime-endpoint unix:///run/containerd/containerd.sock";
   # services.k3s.extraFlags = "--node-ip ${config.networking.wireguardIp} --container-runtime-endpoint unix:///run/containerd/containerd.sock";
 }
