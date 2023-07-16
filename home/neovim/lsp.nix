6643d90d5b4c5b75cdad5515a1867c6404e65bbe
Tyler Schwemley <me@tylerschwemley.com>
2023-07-16 17:19:53 -0400

    typo

--- home/neovim/lsp.nix
+++ home/neovim/lsp.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     gopls
-	lua-lanaguage-server
+	lua-language-server
     nixd
     nodePackages.intelephense
     nodePackages.typescript-language-server
