267399c9a0469c20d2bb96081ff8e2c89498d9db
Tyler Schwemley <me@tylerschwemley.com>
2023-07-16 18:59:20 -0400

    update k3s config

--- nixos/modules/services/k3s/server.nix
+++ nixos/modules/services/k3s/server.nix
@@ -2,5 +2,10 @@
   imports = [./.];
 
   networking.firewall.allowedTCPPorts = [6443];
-  services.k3s.extraFlags = "--disable traefik --node-external-ip 10.0.0.1 --flannel-backend=wireguard-native --flannel-external-ip --container-runtime-endpoint unix:///run/containerd/containerd.sock";
+  services.k3s = {
+	  role = "server";
+	  clusterInit = true;
+	  tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
+	  extraFlags = "--disable traefik --node-ip 10.0.0.1 --node-external-ip 10.0.0.1 --flannel-backend=wireguard-native --flannel-external-ip --container-runtime-endpoint unix:///run/containerd/containerd.sock";
+  };
 }
