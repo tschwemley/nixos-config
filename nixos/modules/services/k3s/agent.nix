2ede12ced7be9e0c8cf26cc8bb95c6408c404303
Tyler Schwemley <me@tylerschwemley.com>
2023-07-16 18:44:44 -0400

    update k3s config

--- nixos/modules/services/k3s/agent.nix
+++ nixos/modules/services/k3s/agent.nix
@@ -5,12 +5,12 @@
 }: {
   imports = [./.];
 
-  sops.secrets.k3s-server-token.sopsFile = ./secrets.yaml;
-  services.k3s.role = "agent";
-  # generated random string
-  services.k3s.tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
-  services.k3s.serverAddr = lib.mkDefault "10.0.0.1:6443";
-  # TODO: make this a passable argument or option value
-  services.k3s.extraFlags = "--node-external-ip 10.0.0.2 --container-runtime-endpoint unix:///run/containerd/containerd.sock";
-  # services.k3s.extraFlags = "--node-ip ${config.networking.wireguardIp} --container-runtime-endpoint unix:///run/containerd/containerd.sock";
+  services.k3s = {
+	  role = "agent";
+	  tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
+	  serverAddr = lib.mkDefault "10.0.0.1:6443";
+	  # TODO: make this a passable argument or option value
+	  # services.k3s.extraFlags = "--node-ip ${config.networking.wireguardIp} --container-runtime-endpoint unix:///run/containerd/containerd.sock";
+	  extraFlags = "--node-ip 10.0.0.2 --node-external-ip 10.0.0.2 --container-runtime-endpoint unix:///run/containerd/containerd.sock";
+  };
 }
