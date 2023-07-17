98ed9b6259b86f1f8006c9763888043bcfac6810
Tyler Schwemley <me@tylerschwemley.com>
2023-07-17 19:53:05 -0400

    testing

--- home/programs/neovim/modules/plugins/better-escape.nix
+++ home/programs/neovim/modules/plugins/better-escape.nix
@@ -1,4 +1,4 @@
-{vimPlugins, ...}: {
+{vimPlugins}: {
   type = "lua";
   plugin = vimPlugins.better-escape-nvim;
   config = "require('better-escape').setup()";
--- home/programs/neovim/modules/plugins/default.nix
+++ home/programs/neovim/modules/plugins/default.nix
@@ -1,9 +1,7 @@
 # { vimPlugins, submodule}:
 {submodule, ...}: let
-  betterEscape = import ./better-escape.nix;
-  comment = import ./comment.nix;
-  # betterEscape = import ./better-escape.nix { inherit vimPlugins; };
-  # comment = import ./comment.nix { inherit vimPlugins; };
+  betterEscape = import ./better-escape.nix { inherit vimPlugins; };
+  comment = import ./comment.nix { inherit vimPlugins; };
 in {
   programs.neovim.plugins = with submodule; [
     betterEscape
