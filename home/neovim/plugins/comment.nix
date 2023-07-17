a3e0a11d357491e558f8d399ee40ba064a7f6e50
Tyler Schwemley <me@tylerschwemley.com>
2023-07-17 12:17:08 -0400

    testing

--- home/neovim/plugins/comment.nix
+++ home/neovim/plugins/comment.nix
@@ -1,7 +1,5 @@
-{ vimPlugins, types }: types.submodule {
-	pluginWithConfigType = {
-		type = "lua";	
-		plugin = vimPlugins.comment-nvim;
-		config = "require('Comment').setup()";
-	};
+{ vimPlugins }: {
+	type = "lua";	
+	plugin = vimPlugins.comment-nvim;
+	config = "require('Comment').setup()";
 }
