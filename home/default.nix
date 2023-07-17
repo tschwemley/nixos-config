0e7cf6b9e1676780bc572e8707646f8f72fa3c15
Tyler Schwemley <me@tylerschwemley.com>
2023-07-17 17:34:16 -0400

    move nvim xdg config

--- home/default.nix
+++ home/default.nix
@@ -2,19 +2,7 @@
   inputs,
   withSystem,
   ...
-}: let
-  # mkHomeConfig = username: profiles:
-  #   inputs.home-manager.lib.homeManagerConfiguration {
-  #  inherit pkgs;
-  #     modules = [
-  # {
-  # 	home.username = username;
-  # 	home.homeDirectory = "/home/${username}";
-  # }
-  #       ./profiles/common.nix
-  #     ] ++ profiles;
-  #   };
-in {
+}: {
   flake.homeConfigurations = {
     # schwem = mkHomeConfig "schwem" [ ./profiles/pc.nix ];
     schwem = withSystem "x86_64-linux" ({pkgs, ...}:
