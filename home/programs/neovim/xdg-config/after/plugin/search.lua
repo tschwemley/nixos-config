6cbdd153cd1b64f65df582c3c420876a118be9c1
Tyler Schwemley <me@tylerschwemley.com>
2023-07-17 20:02:19 -0400

    update keymaps

--- home/programs/neovim/xdg-config/after/plugin/search.lua
+++ home/programs/neovim/xdg-config/after/plugin/search.lua
@@ -1,8 +1,9 @@
 local wk = require('which-key')
 
 wk.register({
-	f = {'<cmd>Telescope find_files<cr>', 'Find Files...',},
+	f = {'<cmd>Telescope find_files<cr>', 'Find Files',},
 	s = {
+		name = 'Search...',
 		b = {'<cmd>Telescope buffers<cr>', 'Search Buffers',},
 		k = {'<cmd>Telescope keymaps<cr>', 'Search Keybindings',},
 		t = {'<cmd>Telescope live_grep<cr>', 'Search Text',},
