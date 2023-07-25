9bec56e761a4293424df297e458cbbab4a806c23
Tyler Schwemley <me@tylerschwemley.com>
2023-07-24 23:10:53 -0400

    serverAddr requiered

--- nixos/modules/services/k3s/agent.nix
+++ nixos/modules/services/k3s/agent.nix
@@ -8,7 +8,7 @@
   services.k3s = {
     role = "agent";
     tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
-    # serverAddr = lib.mkDefault "https://10.0.0.1:6443";
+    serverAddr = lib.mkDefault "https://10.0.0.1:6443";
     extraFlags = "--server https://10.0.0.1:6443 --node-ip 10.0.0.2 --node-external-ip 10.0.0.2 --container-runtime-endpoint unix:///run/containerd/containerd.sock";
   };
 }
