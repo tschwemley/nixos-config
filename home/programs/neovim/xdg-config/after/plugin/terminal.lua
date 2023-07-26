14ceea1fa3669793533005dda48f31f0a46a27f0
tjschwem@gmail.com <Tyler Schwemley>
2023-07-26 16:58:01 -0400

    remove sudo for wheel on pc and alter nvim terminal opts

--- home/programs/neovim/xdg-config/after/plugin/terminal.lua
+++ home/programs/neovim/xdg-config/after/plugin/terminal.lua
@@ -1,12 +1,15 @@
-require('toggleterm').setup({open_mapping = [[<C-\>]]})
+require('toggleterm').setup({
+   direction = 'vertical',
+   open_mapping = [[<C-\>]],
+})
 
 require('which-key').register({
 	t = {
 		name = 'Terminal',
-		f = { '<cmd>ToggleTerm dir=float<cr>', 'Floating Terminal'},
-		h = { '<cmd>ToggleTerm dir=horizontal<cr>', 'Horizontal Terminal'},
-		t = { '<cmd>ToggleTerm dir=float<cr>', 'Terminal Tab'},
-		v = { '<cmd>ToggleTerm dir=vertical<cr>', 'Vertical Terminal'},
+		f = { '<cmd>ToggleTerm direction=float<cr>', 'Floating Terminal'},
+		h = { '<cmd>ToggleTerm direction=horizontal<cr>', 'Horizontal Terminal'},
+		t = { '<cmd>ToggleTerm direction=float<cr>', 'Terminal Tab'},
+		v = { '<cmd>ToggleTerm direction=vertical<cr>', 'Vertical Terminal'},
 	},
    { prefix = '<leader>', }
 })
