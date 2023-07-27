87380091af0d07ce578ac3b5498159497f0d70a2
tjschwem@gmail.com <Tyler Schwemley>
2023-07-26 21:19:18 -0400

    update zvim config

--- home/programs/neovim/xdg-config/after/plugin/terminal.lua
+++ home/programs/neovim/xdg-config/after/plugin/terminal.lua
@@ -1,5 +1,5 @@
 require('toggleterm').setup({
-   direction = 'vertical',
+   direction = 'float',
    open_mapping = [[<C-\>]],
 })
 
@@ -7,9 +7,9 @@ require('which-key').register({
 	t = {
 		name = 'Terminal',
 		f = { '<cmd>ToggleTerm direction=float<cr>', 'Floating Terminal'},
-		h = { '<cmd>ToggleTerm direction=horizontal<cr>', 'Horizontal Terminal'},
+		h = { '<cmd>ToggleTerm direction=horizontal<cr> size=10', 'Horizontal Terminal'},
 		t = { '<cmd>ToggleTerm direction=float<cr>', 'Terminal Tab'},
-		v = { '<cmd>ToggleTerm direction=vertical<cr>', 'Vertical Terminal'},
+		v = { '<cmd>ToggleTerm direction=vertical size=60<cr>', 'Vertical Terminal'},
 	},
    { prefix = '<leader>', }
 })
