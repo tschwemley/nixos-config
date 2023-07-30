3937704801763de83d63e9dcbe13378d695f414c
tjschwem@gmail.com <Tyler Schwemley>
2023-07-30 19:25:56 -0400

    add dependency to unfuck myself

--- flake.nix
+++ flake.nix
@@ -65,6 +65,7 @@
             manix
             nixos-generators.packages.${system}.nixos-generate
             pkgs.sops
+            sqlite # this is for if the nix-store fucks me for the Nth time
             ssh-to-age
             wireguard-tools
           ];
