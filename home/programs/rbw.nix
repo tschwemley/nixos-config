989942265e02d66727434614a338b94b439162a6
tjschwem@gmail.com <Tyler Schwemley>
2023-07-30 22:14:20 -0400

    update rbw

--- home/programs/rbw.nix
+++ home/programs/rbw.nix
@@ -1,3 +1,8 @@
-{config, ...}: {
-  programs.rbw.enable = true;
+{config, pkgs, ...}: {
+  # home-manager also wraps this in 'programs' but I don't see need since I don't want hm to manage
+  # my config file
+  home.packages = with pkgs; [
+    rbw
+    pinentry-tty
+  ];
 }
