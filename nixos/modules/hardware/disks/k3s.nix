016e6b69695215a19bfe8a8ba071b3fe9a5d77db
Tyler Schwemley <me@tylerschwemley.com>
2023-08-03 10:41:49 -0400

    update k3s disk config

--- nixos/modules/hardware/disks/k3s.nix
+++ nixos/modules/hardware/disks/k3s.nix
@@ -2,27 +2,14 @@
   diskName,
   useGrub ? false,
   ...
-}: {
-  disko = {
-    devices = {
-      disk = {
-        main = {
-          type = "disk";
-          device = diskName;
-          content = {
-            type = "table";
-            format = "gpt";
-            partitions = [
-              (
-                if useGrub
-                then {
+}: let
+	grubPartitions = [
+               {
                   name = "boot";
                   start = "0";
                   end = "1M";
                   flags = ["bios_grub"];
-                }
-                else {}
-              )
+			  }
               {
                 name = "ESP";
                 start = "1MiB";
@@ -62,7 +49,59 @@
                   };
                 };
               }
-            ];
+	];
+	noGrubPartitions = [
+              {
+                name = "ESP";
+                start = "1MiB";
+                end = "128MiB";
+                bootable = true;
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                };
+              }
+              {
+                name = "root";
+                start = "128MiB";
+                end = "100%";
+                part-type = "primary";
+                content = {
+                  type = "btrfs";
+                  extraArgs = ["-f"];
+                  subvolumes = {
+                    "/rootfs" = {
+                      mountpoint = "/";
+                      mountOptions = ["compress=lzo"];
+                    };
+                    "/home" = {
+                      mountpoint = "/home";
+                      mountOptions = [];
+                    };
+                    "/nix" = {
+                      mountpoint = "/nix";
+                      mountOptions = ["compress=lzo" "noatime"];
+                    };
+                    "/persist" = {
+                      mountpoint = "/persist";
+                      mountOptions = ["compress=lzo" "noatime"];
+                    };
+                  };
+                };
+              }
+	];
+in {
+  disko = {
+    devices = {
+      disk = {
+        main = {
+          type = "disk";
+          device = diskName;
+          content = {
+            type = "table";
+            format = "gpt";
+            partitions = if useGrub then grubPartitions else noGrubPartitions;
           };
         };
       };
