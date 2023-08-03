22bd4f4881a67df40a02365c39cda011e57abb21
tjschwem@gmail.com <Tyler Schwemley>
2023-08-02 21:37:04 -0400

    update hardware accel

--- nixos/modules/hardware/nvidia.nix
+++ nixos/modules/hardware/nvidia.nix
@@ -3,18 +3,25 @@
   pkgs,
   ...
 }: {
-  services.xserver.videoDrivers = lib.mkDefault ["nvidia"];
   hardware.nvidia.modesetting.enable = true;
+  services.xserver.videoDrivers = lib.mkDefault ["nvidia"];
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
   hardware.opengl = {
     enable = true;
     driSupport32Bit = true; # also enable support for 32 bit applications (e.g. wine)
     extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel
       vaapiVdpau
       libvdpau-va-gl
     ];
   };
   environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "nvidia";
+    LIBVA_DRIVER_NAME = "iHD";
+    #LIBVA_DRIVER_NAME = "nvidia";
     # LIBVA_DRIVER_NAME = "vdpau";
   };
 }
