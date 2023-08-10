96787b7d9e747ec4449f7db74bc9c1c52da36b3e
tjschwem@gmail.com <Tyler Schwemley>
2023-08-10 14:41:21 -0400

    remove packages for now

--- packages/default.nix
+++ packages/default.nix
@@ -2,7 +2,7 @@
 
 {
   perSystem = {pkgs, ...}: let 
-    nix-utils = (pkgs.writeShellScriptBin "nix-utils" lib.readFile ./scripts/nix-utils.sh);
+    nix-utils = pkgs.writeShellScriptBin "nix-utils" lib.readFile ./scripts/nix-utils.sh;
   in {
     packages.default = nix-utils;
   };
