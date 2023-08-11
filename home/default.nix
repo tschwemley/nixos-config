0a36468607d5e3544ed201079bb3b88dd97f2cd0
Tyler Schwemley <me@tylerschwemley.com>
2023-08-10 22:27:15 -0400

    move this bullshit

--- home/default.nix
+++ home/default.nix
@@ -8,6 +8,16 @@
       inputs.home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [
+		{
+		  nixpkgs = {
+			config = {
+			  allowUnfree = lib.mkDefault true;
+			  # TODO: check if this is still necessary with my current config
+			  # necessary due to home manager BUG: https://github.com/nix-community/home-manager/issues/2942#issuecomment-1119760100
+			  allowUnfreePredicate = pkg: true;
+			};
+		  };
+		}
           profile
           {
             home.username = user;
--- home/profiles/default.nix
+++ home/profiles/default.nix
@@ -11,14 +11,5 @@
     ../shell
   ];
 
-  nixpkgs = {
-    config = {
-      allowUnfree = lib.mkDefault true;
-      # TODO: check if this is still necessary with my current config
-      # necessary due to home manager BUG: https://github.com/nix-community/home-manager/issues/2942#issuecomment-1119760100
-      allowUnfreePredicate = pkg: true;
-    };
-  };
-
   home.stateVersion = "23.05";
 }
