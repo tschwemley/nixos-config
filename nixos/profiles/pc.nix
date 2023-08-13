abd86039066d8ed01e8ca0c54511840799935a75
tjschwem@gmail.com <Tyler Schwemley>
2023-08-12 22:17:32 -0400

    comment out steam+

--- nixos/profiles/pc.nix
+++ nixos/profiles/pc.nix
@@ -8,7 +8,7 @@
     ../modules/hardware/audio.nix
     ../modules/programs/barrier.nix
     ../modules/services/mullvad.nix
-    ../modules/programs/steam.nix
+#../modules/programs/steam.nix
     ../modules/services/xserver.nix
     ../modules/system/fonts.nix
     ../modules/system/xdg.nix
