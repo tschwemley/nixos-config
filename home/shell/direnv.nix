8b34dd7fbb85c35d45eed7256228d4137467873b
tjschwem@gmail.com <Tyler Schwemley>
2023-08-15 21:56:36 -0400

    how fucking long has that been reversed lol

--- home/shell/direnv.nix
+++ home/shell/direnv.nix
@@ -3,12 +3,5 @@
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
-    # direnvrcExtra = ''
-    #   use_flake() {
-    #     watch_file flake.nix
-    #     watch_file flake.lock
-    #     eval "$(nix print-dev-env --profile "$(direnv_layout_dir)/flake-profile")"
-    #   }
-    # '';
   };
 }
