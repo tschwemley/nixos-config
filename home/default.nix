b367a394103ce0bde9e50c5654a3754d3fac0c0e
Tyler Schwemley <tjschwem@gmail.com>
2023-08-21 17:40:53 -0400

    wip hyprland

--- home/default.nix
+++ home/default.nix
@@ -7,14 +7,17 @@
     withSystem system ({pkgs, ...}:
       inputs.home-manager.lib.homeManagerConfiguration {
         inherit modules pkgs;
+        extraSpecialArgs = {
+          hyprland = inputs.hyprland;  
+        };
       });
 in {
   flake.homeConfigurations = {
     # TODO: this would probably be cleaner if it was extra modules being passed into the profile
     schwem = mkHome ("x86_64-linux" [
       ./profiles/pc.nix
-      inputs.hyprland.homeModules.default
-      {home.username = "schwem";}
+      # inputs.hyprland.homeModules.default
+      # {home.username = "schwem";}
     ]);
     # "work@dev" = mkHome "x86_64-linux" [ ./profiles/work.nix ];
     "work@mac" = mkHome "aarch64-darwin" [ ./profiles/work.nix ];
--- home/wayland/hyprland.nix
+++ home/wayland/hyprland.nix
@@ -1,26 +1,29 @@
-{
+{hyprland, pkgs, ...}: {
   wayland.windowManager.hyprland = {
-    # enable = true;
-    extraConfig = ''
-      $mod = SUPER
-
-      bind = $mod, T, exec, wezterm
-      # bind = , Print, exec, grimblast copy area
-
-      # workspaces
-      # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
-      ${builtins.concatStringsSep "\n" (builtins.genList (
-          x: let
-            ws = let
-              c = (x + 1) / 10;
-            in
-              builtins.toString (x + 1 - (c * 10));
-          in ''
-            bind = $mod, ${ws}, workspace, ${toString (x + 1)}
-            bind = $mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}
-          ''
-        )
-        10)}
-    '';
+    enable = true;
+    settings = {
+      bind = ["$mod, Return, exec, ${pkgs.wezterm}/bin/wezterm"];
+    };
+    # extraConfig = ''
+    #   $mod = SUPER
+    #
+    #   bind = $mod, T, exec, wezterm
+    #   # bind = , Print, exec, grimblast copy area
+    #
+    #   # workspaces
+    #   # binds $mod + [shift +] {1..10} to [move to] workspace {1..10}
+    #   ${builtins.concatStringsSep "\n" (builtins.genList (
+    #       x: let
+    #         ws = let
+    #           c = (x + 1) / 10;
+    #         in
+    #           builtins.toString (x + 1 - (c * 10));
+    #       in ''
+    #         bind = $mod, ${ws}, workspace, ${toString (x + 1)}
+    #         bind = $mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}
+    #       ''
+    #     )
+    #     10)}
+    # '';
   };
 }
