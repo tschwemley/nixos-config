8b98aad766c99bac7a9115ac0ab697c2546f7f21
Tyler Schwemley <tjschwem@gmail.com>
2023-08-20 21:28:31 -0400

    update hydra config

--- nixos/modules/services/hydra.nix
+++ nixos/modules/services/hydra.nix
@@ -1,6 +1,11 @@
 {...}: {
   services.hydra = {
     enable = true;
+    extraConfig = ''
+      <dynamicruncommand>
+        enable = 1
+      </dynamicruncommand>
+    '';
     hydraURL = "http://localhost:3000"; # externally visible URL
     notificationSender = "hydra@localhost"; # e-mail of hydra service
     # a standalone hydra will require you to unset the buildMachinesFiles list to avoid using a nonexistant /etc/nix/machines
