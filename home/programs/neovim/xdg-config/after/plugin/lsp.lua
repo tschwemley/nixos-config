e62b757fc6a0de67a5c522d48d00ae9ec8eb316e
Tyler Schwemley <tjschwem@gmail.com>
2023-08-22 18:44:19 -0400

    update lsp settings

--- home/programs/neovim/xdg-config/after/plugin/lsp.lua
+++ home/programs/neovim/xdg-config/after/plugin/lsp.lua
@@ -16,7 +16,7 @@ require('lspconfig').nil_ls.setup {
    settings = {
       ['nil'] = {
          formatting = {
-            command = { "nixpkgs-fmt" },
+            command = { "nix fmt" },
          },
       },
    },
