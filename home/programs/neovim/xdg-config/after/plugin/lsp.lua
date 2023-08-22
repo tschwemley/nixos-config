db56f07f914c0ac8bbeb05fe75aa193dd49e7be3
Tyler Schwemley <tjschwem@gmail.com>
2023-08-22 16:46:00 -0400

    test nix lsp

--- home/programs/neovim/xdg-config/after/plugin/lsp.lua
+++ home/programs/neovim/xdg-config/after/plugin/lsp.lua
@@ -1,11 +1,13 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-  lsp.buffer_autoformat()
+   lsp.default_keymaps({ buffer = bufnr })
+   lsp.buffer_autoformat()
 end)
 
 -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md for naming
-lsp.setup_servers({'gopls', 'intelephense', 'lua_ls', 'nil_ls', 'tsserver', 'yamlls'})
+-- TODO: remove this after testing different nix servers
+local nixServer = 'nixd' -- nil_ls cmd not found (see if same problem w/ nixd)
+lsp.setup_servers({ 'gopls', 'intelephense', 'lua_ls', nixServer, 'tsserver', 'yamlls' })
 
 lsp.setup()
