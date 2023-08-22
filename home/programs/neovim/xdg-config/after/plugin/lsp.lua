52190cb82b1a36af00c1194ee8b6202b5f6593c3
Tyler Schwemley <tjschwem@gmail.com>
2023-08-22 18:50:34 -0400

    update lsp settings

--- home/programs/neovim/xdg-config/after/plugin/lsp.lua
+++ home/programs/neovim/xdg-config/after/plugin/lsp.lua
@@ -7,8 +7,8 @@ end)
 
 -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md for naming
 -- TODO: remove this after testing different nix servers
-local nixServer = 'nil_ls' -- nixd (not formatting properly I think)
-lsp.setup_servers({ 'gopls', 'intelephense', 'lua_ls', nixServer, 'tsserver', 'yamlls' })
+-- lsp.setup_servers({ 'gopls', 'intelephense', 'lua_ls', nixServer, 'tsserver', 'yamlls' })
+lsp.setup_servers({ 'gopls', 'intelephense', 'lua_ls', 'tsserver', 'yamlls' })
 
 require('lspconfig').nil_ls.setup {
    -- autostart = true,
