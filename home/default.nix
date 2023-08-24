2bbde13bbb4ddd1c90182abe78b91632ce283554
Tyler Schwemley <tjschwem@gmail.com>
2023-08-24 12:04:55 -0400

    enable work config for mac

--- home/default.nix
+++ home/default.nix
@@ -17,10 +17,8 @@
       });
 in {
   flake.homeConfigurations = {
-    # TODO: this would probably be cleaner if it was extra modules being passed into the profile
     schwem = mkHome "x86_64-linux" [./profiles/pc.nix];
-    # inputs.hyprland.homeModules.default
     # "work@dev" = mkHome "x86_64-linux" [ ./profiles/work.nix ];
-    # "work@mac" = mkHome "aarch64-darwin" [./profiles/work.nix];
+    "work@mac" = mkHome "aarch64-darwin" [./profiles/work.nix];
   };
 }
