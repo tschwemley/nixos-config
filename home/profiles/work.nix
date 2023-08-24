e43c8934ec2b4c377147b61ba6678c40709065b1
Tyler Schwemley <tjschwem@gmail.com>
2023-08-24 12:21:10 -0400

    update work profile

--- home/profiles/work.nix
+++ home/profiles/work.nix
@@ -1,5 +1,15 @@
-{
+{pkgs, ...}: let
+  userName = "tschwemley";
+in {
   imports = [
     ./.
   ];
+
+  home = {
+    inherit userName;
+    homeDirectory =
+      if builtins.currentSystem == "x86_64-linux"
+      then "/home/${userName}"
+      else "/Users/${userName}";
+  };
 }
