97762e725edc325d3ab1a2545d6a71eee0bebbce
Tyler Schwemley <tjschwem@gmail.com>
2023-08-25 21:43:26 -0400

    update to use nixpkgs-wayland

--- nixos/modules/system/nix.nix
+++ nixos/modules/system/nix.nix
@@ -1,10 +1,21 @@
-{
+{inputs, ...}: {
   nix.settings = {
     auto-optimise-store = true;
     experimental-features = ["nix-command" "flakes"];
+    substituters = [
+      "https://cache.nixos.org"
+      "https://hyprland.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+    ];
   };
 
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [inputs.nixpkgs-wayland.overlay];
 
   # automatically trigger garbage collection (by default weekly)
   nix.gc.automatic = true;
