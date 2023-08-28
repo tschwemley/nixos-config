abdf29e055b2d056492d0ddb5647cd3c0e9dbea5
Tyler Schwemley <tjschwem@gmail.com>
2023-08-28 17:11:00 -0400

    update user structuring for servers

--- nixos/modules/users/server.nix
+++ nixos/modules/users/server.nix
@@ -1,21 +1,16 @@
-{
-  config,
-  userName ? "server",
-  userSSHKey ? "",
-  ...
-}: {
-  imports = [./.];
-
-  users.users = {
-    ${userName} = {
-      isNormalUser = true;
-      home = "/home/${userName}";
-      extraGroups = ["wheel" "k3s"];
-      openssh.authorizedKeys.keys = [userSSHKey];
-    };
-  };
-
-  home-manager.users.${userName}.imports = [
+{config}: {
+  # don't import default since my servers are root-only headless (for now)
+  home-manager.users.root.imports = [
     ../../../home/profiles/server.nix
   ];
+
+  users = {
+    mutableUsers = false;
+    users = {
+      root = {
+        passwordFile = config.sops.secrets.root_password.path;
+        openssh.authorizedKeys.keys = [(builtins.readFile ../../hosts/${config.networking.hostName}/ssh_key.pub)];
+      };
+    };
+  };
 }
