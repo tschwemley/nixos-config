82b7e780fad5de00af68d6b0e97e8da314cf568b
Tyler Schwemley <tjschwem@gmail.com>
2023-08-27 20:46:59 -0400

    test nix-utils

--- apps/scripts/nix-utils.sh
+++ apps/scripts/nix-utils.sh
@@ -16,7 +16,7 @@ extractSshHostKeys() {
 	echo $2
 	secretsPath=./nixos/hosts/$1/secrets.yaml
 	extractPath="/persist/etc/ssh" 
-	if [ "$2" -ne "" ]; then extractPath="$2"; fi
+	if [ "$2" != "" ]; then extractPath="$2"; fi
 	echo $extractPath
 	exit 69
 	keys_to_extract='["ssh_host_ed25519_key" "ssh_host_ed25519_key_pub" "ssh_host_rsa_key" "ssh_host_rsa_key_pub"]'
