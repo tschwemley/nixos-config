e2d411a90f6a1bad6ab8b10da32f7a0e7bc08ea2
Tyler Schwemley <tjschwem@gmail.com>
2023-09-10 12:45:23 -0400

    update hyprbars config location

--- home/wayland/hyprland.nix
+++ home/wayland/hyprland.nix
@@ -17,7 +17,7 @@
     # '';
 
     plugins = [
-      "/nix/store/yhwdlzii9a29r92a6az579gx1mwa838k-hyprbars-0.1/lib/libhyprbars.so"
+      "$HOME/.config/hypr/libhyprbars.so"
     ];
 
     settings = {
