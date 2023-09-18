085a624817716b67283e2e634515850dec3f63b2
Tyler Schwemley <tjschwem@gmail.com>
2023-09-17 23:05:22 -0400

    update neovim configuration lsp

--- home/programs/neovim/modules/lsp.nix
+++ home/programs/neovim/modules/lsp.nix
@@ -1,4 +1,17 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  lsp-zero = with pkgs;
+    vimUtils.buildVimPluginFrom2Nix {
+      pname = "lsp-zero.nvim";
+      version = "3.x";
+      src = fetchFromGitHub {
+        owner = "VonHeikemen";
+        repo = "lsp-zero.nvim";
+        rev = "53068d66f0313ac7c496e0b04f1f80169ee7980d";
+        sha256 = "sha256-nJ0w4SO616RRUbFky+SH2deGh1uuIzF03QmlqdoIgxY=";
+      };
+      meta.homepage = "https://github.com/VonHeikemen/lsp-zero.nvim/";
+    };
+in {
   programs.neovim = {
     extraPackages = with pkgs; [
       alejandra
@@ -11,7 +24,7 @@
     ];
 
     plugins = with pkgs.vimPlugins; [
-      lsp-zero-nvim
+      lsp-zero
       nvim-lspconfig
     ];
   };
