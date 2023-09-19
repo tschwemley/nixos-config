7cf8f3aca0f43531c649365d4903512a746e7314
Tyler Schwemley <tjschwem@gmail.com>
2023-09-19 12:00:48 -0400

    remove rsync-nvim

--- home/programs/neovim/modules/utils.nix
+++ home/programs/neovim/modules/utils.nix
@@ -1,17 +1,4 @@
-{pkgs, ...}: let
-  rsync-nvim = with pkgs;
-    vimUtils.buildVimPluginFrom2Nix {
-      pname = "rsync-nvim";
-      version = "20230919";
-      src = fetchFromGitHub {
-        owner = "OscarCreator";
-        repo = "rsync.nvim";
-        rev = "bc5789e73083692af2a21c72216d0b5985b929e3";
-        sha256 = "sha256-4wGHDBOmBJEDR0qXpkj3mzlKsD+ScRj/KmsnET8tmEc=";
-      };
-      meta.homepage = "https://github.com/OscarCreator/rsync.nvim/commit/";
-    };
-in {
+{pkgs, ...}: {
   programs.neovim.plugins = with pkgs.vimPlugins; [
     better-escape-nvim
     rsync-nvim
