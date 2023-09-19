ac29938e08b5107655e6a905ece1a3f1dc7198e7
Tyler Schwemley <tjschwem@gmail.com>
2023-09-19 12:27:52 -0400

    temporarily disable which-key for git

--- home/programs/neovim/config/after/plugin/git.lua
+++ home/programs/neovim/config/after/plugin/git.lua
@@ -3,8 +3,4 @@ local neogit = require('neogit')
 require('gitsigns').setup()
 neogit.setup()
 
-require('which-key').register({ {
-   g = { name = 'Git', { '<cmd>Neogit<cr>', '', { buffer = true } }, },
-},
-   { prefix = '<leader>' }
-})
+-- require('which-key').register({})
