f48116bba5e965008b4f373c08038ee9fe894dfa
Tyler Schwemley <tjschwem@gmail.com>
2023-09-22 16:29:05 -0400

    update nnn and telescope for nvim

--- home/programs/nnn.nix
+++ home/programs/nnn.nix
@@ -1,10 +1,20 @@
-{...}: {
+{pkgs, ...}: let
+  plugins = [];
+in {
   programs.nnn = {
     enable = true;
     bookmarks = {
       D = "~/Downloads";
       p = "~/projects";
       n = "~/nixos-config";
+    };
+    plugins = {
+      src = "${pkgs.nnn.src}/plugins";
+      mappings = {
+        f = "finder";
+        p = "preview-tui";
+        v = "imgview";
+      };
     };
   };
 }
