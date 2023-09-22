7a01483040f87cc61a88372ce2e84b4322000f15
Tyler Schwemley <tjschwem@gmail.com>
2023-09-22 13:16:46 -0400

    add notes setup for neovim

---
+++ home/programs/neovim/config/after/plugin/notes.lua
@@ -0,0 +1,23 @@
+require('neorg').setup {
+   load = {
+      ["core.defaults"] = {}
+   }
+}
+
+-- example neorg telescope keybindings mappings
+local neorg_callbacks = require("neorg.core.callbacks")
+
+--     -- Map all the below keybinds only when the "norg" mode is active
+--     keybinds.map_event_to_mode("norg", {
+--         n = { -- Bind keys in normal mode
+--             { "<C-s>", "core.integrations.telescope.find_linkable" },
+--         },
+--
+--         i = { -- Bind in insert mode
+--             { "<C-l>", "core.integrations.telescope.insert_link" },
+--         },
+--     }, {
+--         silent = true,
+--         noremap = true,
+--     })
+-- end)
