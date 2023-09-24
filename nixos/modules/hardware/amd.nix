7b7c52ee9db06ea9b6cb4f36f8deaedb2638863e
Tyler Schwemley <tjschwem@gmail.com>
2023-09-24 18:58:03 -0400

    update amd config

--- nixos/modules/hardware/amd.nix
+++ nixos/modules/hardware/amd.nix
@@ -1,14 +1,23 @@
 {pkgs, ...}: {
-  services.xserver.videoDrivers = ["modesetting"];
+  boot.initrd.kernelModules = ["amdgpu"];
   environment.variables.AMD_VULKANICD = "RADV";
+  hardware.enableRedistributableFirmware = true;
 
   hardware.opengl = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
     extraPackages = with pkgs; [
-      # amdvlkt
+      amdvlk
       rocm-opencl-icd
+      rocm-opencl-runtime
     ];
   };
+
+  # services.xserver.videoDrivers = ["modesetting"];
+  services.xserver.videoDrivers = ["amdgpu"];
+
+  systemd.tmpfiles.rules = [
+    "L+    /opt/rocm/hip   -    -    -     -    ${pkgs.hip}"
+  ];
 }
