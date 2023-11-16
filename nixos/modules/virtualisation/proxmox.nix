c3f708c721877ae413a0b4082b8f0084d6c1e65f
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:39 -0500

    rework machamp config a little bit

--- nixos/modules/virtualisation/proxmox.nix
+++ nixos/modules/virtualisation/proxmox.nix
@@ -1,15 +1,29 @@
 {
   config,
-  modulesPath,
-  bios ? "ovmf",
-  cores ? 2,
-  memory ? 4096,
+  lib,
   ...
 }: {
-  imports = [./proxmox-image.nix];
-
-  proxmox.qemuConf = {
-    inherit bios cores memory;
-    name = config.networking.hostName;
+  proxmox = {
+    qemuConf = {
+      boot = "order=scsci0;ide2;net0";
+      bios = "ovmf";
+      cores = 2;
+      diskSize = "32000";
+      memory = 4096;
+      name = config.networking.hostName;
+      scsihw = "virtio-scsi-single";
+    };
+    qemuExtraConf = {
+      # ide2 = "local:iso/nixos-minimal-23.05.2422.391e8db1f06-x86_64-linux.iso,media=cdrom,size=818M";
+    };
   };
+
+  fileSystems."/" = {
+    device = lib.mkForce "/dev/sda2";
+    fsType = lib.mkForce "btrfs";
+  };
+  fileSystems."/boot".device = lib.mkForce "/dev/sda1";
+
+  # allow the vm to have it's network access set up immediately
+  services.cloud-init.network.enable = true;
 }
--- nixos/profiles/hydra.nix
+++ nixos/profiles/hydra.nix
@@ -1,7 +1,6 @@
 {...}: {
   imports = [
     ./server.nix
-    ./proxmox.nix
     ../modules/services/cloudflared.nix
     ../modules/services/hydra.nix
   ];
