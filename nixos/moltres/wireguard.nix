358cb105df84702fccea9f53ad5668e5e9025255
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:33 -0500

    update wireguard

--- nixos/moltres/wireguard.nix
+++ nixos/moltres/wireguard.nix
@@ -1,21 +1,20 @@
-let
-	publicKey = "DXGnAkpq/sM4Dw6vl26gmdggE8ItGi9H/61GQ8nWR14=";
-in
 {
   networking.firewall.allowedUDPPorts = [51820];
   networking.wireguard.interfaces = {
-    wg0 = {
+    wg0 = let
+      publicKey = "DXGnAkpq/sM4Dw6vl26gmdggE8ItGi9H/61GQ8nWR14=";
+    in {
       ips = ["10.1.1.2/24"];
       listenPort = 51820;
       privateKeyFile = "/persist/wireguard/private";
 
-	  # use postUp instead of persistentKeepalive because persistentKeepalive doesn't auto-connect
-	  # to the peer. This is apparently because the private key is set after the persistentKeepalive
-	  # which happens because we're setting the private key from a file.
-	  postSetup = ["wg set wgnet0 peer ${publicKey} persistent-keepalive 25"];
+      # use postUp instead of persistentKeepalive because persistentKeepalive doesn't auto-connect
+      # to the peer. This is apparently because the private key is set after the persistentKeepalive
+      # which happens because we're setting the private key from a file.
+      postSetup = ["wg set wgnet0 peer ${publicKey} persistent-keepalive 25"];
       peers = [
         {
-		  inherit publicKey; 
+          inherit publicKey;
 
           # Forward all the traffic via VPN.
           allowedIPs = ["0.0.0.0/0"];
