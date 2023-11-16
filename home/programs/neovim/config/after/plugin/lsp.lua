6edced90577d1d84034f4e7217ba96786a09f84d
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:37 -0500

    update lsp keybinds

--- home/programs/neovim/config/after/plugin/lsp.lua
+++ home/programs/neovim/config/after/plugin/lsp.lua
@@ -1,9 +1,18 @@
 local lsp = require('lsp-zero').preset({})
-local which-key = require('which-key')
+local whichKey = require('which-key')
 
 lsp.on_attach(function(client, bufnr)
    lsp.default_keymaps({ buffer = bufnr })
    lsp.buffer_autoformat()
+
+   whichKey.register({
+      l = {
+         name = "LSP",
+         d = { vim.lsp.buf.definition, "Go To Definition", { buffer = true } },
+         D = { vim.lsp.buf.declaration, "Go To Declaration", { buffer = true } },
+         r = { "<cmd>:Telescope lsp.referenes<cr>", "Show References", { buffer = true } },
+      }
+   }, { prefix = '<leader>' })
 end)
 
 -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md for naming
