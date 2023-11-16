3a24e70507e259a38cf27ac4848da3ced5eb1109
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:37 -0500

    update nil ls

--- home/programs/neovim/modules/lsp.nix
+++ home/programs/neovim/modules/lsp.nix
@@ -1,15 +1,15 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    alejandra
-    gopls
-    lua-language-server
-    nil
-    nodePackages.intelephense
-    nodePackages.typescript-language-server
-    nodePackages.yaml-language-server
-  ];
-
   programs.neovim = {
+    extraPackages = with pkgs; [
+      alejandra
+      gopls
+      lua-language-server
+      nil
+      nodePackages.intelephense
+      nodePackages.typescript-language-server
+      nodePackages.yaml-language-server
+    ];
+
     plugins = with pkgs.vimPlugins; [
       lsp-zero-nvim
       nvim-lspconfig
