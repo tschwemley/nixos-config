d7d07ead6f27413180514c46478a829e5e59c340
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:38 -0500

    update nnn and telescope for nvim

--- home/programs/neovim/config/after/plugin/search.lua
+++ home/programs/neovim/config/after/plugin/search.lua
@@ -1,11 +1,12 @@
 require('which-key').register({
-	f = {'<cmd>Telescope find_files<cr>', 'Find Files',},
-	s = {
-		name = 'Search...',
-		b = {'<cmd>Telescope buffers<cr>', 'Search Buffers',},
-		h = {'<cmd>Telescope help_tags<cr>', 'Search Help Tags',},
-		k = {'<cmd>Telescope keymaps<cr>', 'Search Keybindings',},
-		t = {'<cmd>Telescope live_grep<cr>', 'Search Text',},
-		w = {'<cmd>Telescope grep_string<cr>', 'Search Word Under Cursor'},
-	},
-}, {prefix = '<leader>'})
+   f = { '<cmd>Telescope find_files<cr>', 'Find Files', },
+   s = {
+      name = 'Search...',
+      b = { '<cmd>Telescope buffers<cr>', 'Search Buffers', },
+      h = { '<cmd>Telescope help_tags<cr>', 'Search Help Tags', },
+      k = { '<cmd>Telescope keymaps<cr>', 'Search Keybindings', },
+      m = { '<cmd>Telescope man_pages<cr>', 'Search Man Pages', },
+      t = { '<cmd>Telescope live_grep<cr>', 'Search Text', },
+      w = { '<cmd>Telescope grep_string<cr>', 'Search Word Under Cursor' },
+   },
+}, { prefix = '<leader>' })
