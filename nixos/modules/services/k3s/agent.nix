fde206fdf78f649b2e8e3d5510616dc27e13c70d
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:34 -0500

    remove incorrect flag

--- nixos/modules/services/k3s/agent.nix
+++ nixos/modules/services/k3s/agent.nix
@@ -9,6 +9,6 @@
     role = "agent";
     tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
     serverAddr = lib.mkDefault "https://10.0.0.1:6443";
-    extraFlags = "--node-ip 10.0.0.2 --node-external-ip 10.0.0.2 --with-node-name moltres --container-runtime-endpoint unix:///run/containerd/containerd.sock";
+    extraFlags = "--node-ip 10.0.0.2 --node-external-ip 10.0.0.2 --container-runtime-endpoint unix:///run/containerd/containerd.sock";
   };
 }
