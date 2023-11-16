7ae7845ce4966474304773f8f68c1b848add6fed
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:34 -0500

    get rid of nvim_lua_ls call (not using v2 yet)

--- home/xdg-config/nvim/lua/schwem/lsp.lua
+++ home/xdg-config/nvim/lua/schwem/lsp.lua
@@ -7,7 +7,4 @@ end)
 -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md for naming
 lsp.setup_servers({'gopls', 'intelephense', 'lua_ls', 'nixd', 'sqls', 'tsserver', 'yamlls'})
 
--- Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
 lsp.setup()
