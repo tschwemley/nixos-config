fc2d40074fda4bfae4b0d6119a7a42a05b782e4c
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:34 -0500

    update lua config

--- home/programs/neovim/xdg-config/after/plugin/terminal.lua
+++ home/programs/neovim/xdg-config/after/plugin/terminal.lua
@@ -3,12 +3,13 @@ require('toggleterm').setup({open_mapping = [[c-\]]})
 require('which-key').register({
 	t = {
 		name = 'Terminal',
+      prefix = '<leader>',
 		f = { '<cmd>ToggleTerm dir=float<cr>', 'Floating Terminal'},
 		h = { '<cmd>ToggleTerm dir=horizontal<cr>', 'Horizontal Terminal'},
 		t = { '<cmd>ToggleTerm dir=float<cr>', 'Terminal Tab'},
 		v = { '<cmd>ToggleTerm dir=vertical<cr>', 'Vertical Terminal'},
 	},
-	{prefix = '<leader>'}
-}, {
-   [[<C-\>]] = { '<cmd>ToggleTerm dir=float<cr>', 'Floating Terminal'},
+   {
+      [[<C-\>]] = { '<cmd>ToggleTerm dir=float<cr>', 'Floating Terminal'},
+   },
 })
