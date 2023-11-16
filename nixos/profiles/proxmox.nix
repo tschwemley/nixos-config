c3444072545094c645baf9bd92f45993c4260f26
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:35 -0500

    update articuno for testing

---
+++ nixos/profiles/proxmox.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  modulesPath,
+  bios ? "ovmf",
+  cores ? 2,
+  extraImports ? [],
+  memory ? 4096,
+  profile ? "default",
+  ...
+}: {
+  imports = [
+    (modulesPath + "/virtualisation/proxmox-image.nix")
+    "./${profile}.nix"
+  ] ++ extraImports;
+
+  proxmox.qemuConf = {
+    inherit cores memory;
+    name = config.networking.hostName;
+  };
+}
