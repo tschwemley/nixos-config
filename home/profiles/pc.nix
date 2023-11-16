00430fc8c33fabec20af3775d136033134a2bbc8
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:32 -0500

    udpate hm

--- home/profiles/pc.nix
+++ home/profiles/pc.nix
@@ -1,8 +1,11 @@
-{...}: {
+{username ? "schwem", ...}: {
   imports = [
     ../programs/lazygit.nix
     ../programs/reaper.nix
 	../terminals/wezterm.nix
 	../window-managers
   ];
+
+  home.username = username;
+  home.homeDirectory = "/home/${username}";
 }
