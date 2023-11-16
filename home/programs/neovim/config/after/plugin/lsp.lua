5f1a93f666a744ae023663935d45e26048b840b5
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:39 -0500

    add unity and update lsp keybinds

--- home/programs/neovim/config/after/plugin/lsp.lua
+++ home/programs/neovim/config/after/plugin/lsp.lua
@@ -4,7 +4,38 @@ local lsp = require('lsp-zero').preset({
    manage_nvim_cmp = true,
    suggest_lsp_servers = false,
 })
-local whichKey = require('which-key')
+local set_keybinds = function()
+   local whichKey = require('which-key')
+
+   whichKey.register({
+      l = {
+         name = 'LSP',
+         a = { '<cmd>Lspsaga code_action<cr>', 'Code Action', { buffer = true } },
+         c = { '<cmd>Telescope lsp_document_symbols ignore_symbols=class,function,method,property,variable<cr>',
+            'List Constants',
+            { buffer = true } },
+         r = { '<cmd>lua vim.lsp.buf.rename()<cr>', 'Rename', { buffer = true } },
+         R = { '<cmd>Telescope lsp_references<cr>', 'List References', { buffer = true } },
+         m = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,property,variable<cr>', 'List Methods',
+            { buffer = true } },
+         p = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,function,method,variable<cr>',
+            'List Properties',
+            { buffer = true } },
+         s = { '<cmd>Telescope lsp_document_symbols<cr>', 'List Symbols', { buffer = true } },
+         v = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,function,method,property<cr>',
+            'List Variables',
+            { buffer = true } },
+      },
+   }, { prefix = '<leader>' })
+
+   whichKey.register({
+      g = {
+         name = 'Go To...',
+         d = { vim.lsp.buf.definition, 'Go To Definition', { buffer = true } },
+         D = { vim.lsp.buf.declaration, 'Go To Declaration', { buffer = true } },
+      },
+   })
+end
 
 lsp.on_attach(function(client, bufnr)
    lsp.default_keymaps({ buffer = bufnr })
@@ -14,35 +45,8 @@ lsp.on_attach(function(client, bufnr)
       lsp.buffer_autoformat()
    end
 
-   whichKey.register({
-      g = {
-         name = 'Go To...',
-         d = { vim.lsp.buf.definition, 'Go To Definition', { buffer = true } },
-         D = { vim.lsp.buf.declaration, 'Go To Declaration', { buffer = true } },
-      },
-   })
+   set_keybinds()
 end)
-
-whichKey.register({
-   l = {
-      name = 'LSP',
-      a = { '<cmd>Lspsaga code_action', 'Code Action', { buffer = true } },
-      c = { '<cmd>Telescope lsp_document_symbols ignore_symbols=class,function,method,property,variable<cr>',
-         'List Constants',
-         { buffer = true } },
-      r = { '<cmd>lua vim.lsp.buf.rename()<cr>', 'Rename', { buffer = true } },
-      R = { '<cmd>Telescope lsp_references<cr>', 'List References', { buffer = true } },
-      m = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,property,variable<cr>', 'List Methods',
-         { buffer = true } },
-      p = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,function,method,variable<cr>',
-         'List Properties',
-         { buffer = true } },
-      s = { '<cmd>Telescope lsp_document_symbols<cr>', 'List Symbols', { buffer = true } },
-      v = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,function,method,property<cr>',
-         'List Variables',
-         { buffer = true } },
-   },
-}, { prefix = '<leader>' })
 
 -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md for naming
 lsp.setup_servers({ 'gopls', 'intelephense', 'lua_ls', 'tsserver', 'yamlls' })
