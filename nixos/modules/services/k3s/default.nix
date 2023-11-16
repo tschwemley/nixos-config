ed0021b987179587f93a2a83e4096a64a8188d45
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:34 -0500

    update k3s config

--- nixos/modules/services/k3s/default.nix
+++ nixos/modules/services/k3s/default.nix
@@ -1,24 +1,25 @@
 {pkgs, ...}: {
-  # 51820 and 51821 for wg backend
-  networking.firewall.allowedUDPPorts = [51820 51821];
-  # 10250 for kubelet metrics
-  networking.firewall.allowedTCPPorts = [10250];
-  services.k3s.enable = true;
-
   environment.systemPackages = with pkgs; [
     # (pkgs.writeShellScriptBin "k3s-reset-node" (builtins.readFile ./k3s-reset-node))
     (writeShellScriptBin "k3s-reset-node" (builtins.readFile ./k3s-reset-node))
     k9s
   ];
 
-  virtualisation.containerd.enable = true;
-
+  # 51820 and 51821 for wg backend
+  networking.firewall.allowedUDPPorts = [51820 51821];
+  # 10250 for kubelet metrics
+  networking.firewall.allowedTCPPorts = [10250];
+  
+  programs.zsh.shellAliases = {
+    kubectl = "k3s kubectl";
+  };
+  
+  services.k3s.enable = true;
+  sops.secrets.k3s-server-token.sopsFile = ./secrets.yaml;
   systemd.services.k3s = {
     wants = ["containerd.service"];
     after = ["containerd.service"];
   };
 
-  programs.zsh.shellAliases = {
-    kubectl = "k3s kubectl";
-  };
+  virtualisation.containerd.enable = true;
 }
