40005d1c30d716e5e97f5cc93f2e386b223a630f
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:39 -0500

    cleanup

--- nixos/modules/system/systemd-boot.nix
+++ nixos/modules/system/systemd-boot.nix
@@ -2,6 +2,5 @@
   boot.loader.systemd-boot = {
     enable = true;
     editor = false; # leaving true allows for root access to be gained via passing kernel param
-    memtest86.enable = true;
   };
 }
