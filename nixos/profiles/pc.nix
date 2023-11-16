b1ba2f40e92f11898bc20d49202b4f10205b5bbc
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:34 -0500

    update files for pc build

--- nixos/profiles/pc.nix
+++ nixos/profiles/pc.nix
@@ -8,7 +8,6 @@
     inputs.nixos-hardware.nixosModules.common-pc-ssd
     ../modules/hardware/audio.nix
     ../modules/system/fonts.nix
-    ../modules/services/flatpak.nix
     ../modules/services/xserver.nix
     ../modules/users/schwem.nix
   ];
@@ -17,12 +16,13 @@
     TODO: make this specific to Charizard if works - testing a fix for Brave having SIGSEGV that
           may be related to not being able to find a libva driver. If confirmed working then move
           to brave module and make brave a system-level program.
-   */
   environment.systemPackages = with pkgs; [
+    upower
     vaapiVdpau
     # I don't think using the 32 bit version would make sense but here in case it works and the other doesn't
     #driversi686Linux.vaapiVdpau
   ];
+  */
 
   #TODO: move this into programs
   programs.steam = {
