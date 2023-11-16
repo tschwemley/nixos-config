e7727ca06f2ac61a53a9c165b86cd616b5da7867
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:34 -0500

    add {}

--- home/xdg-config/nvim/lua/schwem/lsp.lua
+++ home/xdg-config/nvim/lua/schwem/lsp.lua
@@ -7,4 +7,4 @@ end)
 -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md for naming
 lsp.setup_servers({'gopls', 'intelephense', 'lua_ls', 'nixd', 'sqls', 'tsserver', 'yamlls'})
 
-lsp.setup()
+lsp.setup({})
