8f151000b6fc40e17074eb8296e64ac8e4d0277c
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:33 -0500

    being lazy

--- nixos/modules/services/k3s/agent.nix
+++ nixos/modules/services/k3s/agent.nix
@@ -10,5 +10,7 @@
   # generated random string
   services.k3s.tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
   services.k3s.serverAddr = lib.mkDefault "10.0.0.1:6443";
-  services.k3s.extraFlags = "--node-ip ${config.networking.wireguardIp} --container-runtime-endpoint unix:///run/containerd/containerd.sock";
+  # TODO: make this a passable argument or option value
+  services.k3s.extraFlags = "--node-ip 10.0.0.2 --container-runtime-endpoint unix:///run/containerd/containerd.sock";
+  # services.k3s.extraFlags = "--node-ip ${config.networking.wireguardIp} --container-runtime-endpoint unix:///run/containerd/containerd.sock";
 }
