159b8b29b75ad037878b1288fa24eb73ad20eaf0
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:36 -0500

    remove docker

--- nixos/profiles/pc.nix
+++ nixos/profiles/pc.nix
@@ -17,16 +17,4 @@
 
   # on my personal machines I don't want to be prompted for my pw
   security.sudo.wheelNeedsPassword = false;
-
-  /*
-    TODO: make this specific to Charizard if works - testing a fix for Brave having SIGSEGV that
-          may be related to not being able to find a libva driver. If confirmed working then move
-          to brave module and make brave a system-level program.
-  environment.systemPackages = with pkgs; [
-    upower
-    vaapiVdpau
-    # I don't think using the 32 bit version would make sense but here in case it works and the other doesn't
-    #driversi686Linux.vaapiVdpau
-  ];
-  */
 }
