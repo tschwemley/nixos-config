f3d3ae43f0b923230edb53b27fde77c822ba853f
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:38 -0500

    add php template

---
+++ templates/php/flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Basic PHP Application Template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    sops.url = "github:Mic92/sops-nix";
+  };
+
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+    sops,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
+      ];
+
+      perSystem = {
+        self',
+        config,
+        lib,
+        pkgs,
+        system,
+        ...
+      }: {
+        # makes pkgs available to all perSystem functions
+        _module.args.pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+
+        formatter = pkgs.alejandra;
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            age
+            php
+            phpunit
+            php82Packages.composer
+            php82Packages.psysh
+            pkgs.sops
+          ];
+        };
+      };
+    };
+}
