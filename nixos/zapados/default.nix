80b45bbbc803b4dd6161aa3cd6d1a96c55216b4a
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:33 -0500

    update perms

--- nixos/zapados/default.nix
+++ nixos/zapados/default.nix
@@ -79,7 +79,7 @@ in {
         neededForUsers = true;
       };
       wireguard_private = {
-		mode = "0640";
+		mode = "0644";
         path = "/persist/wireguard/private";
         reloadUnits = ["systemd-networkd" "systemd-resolved"];
       };
