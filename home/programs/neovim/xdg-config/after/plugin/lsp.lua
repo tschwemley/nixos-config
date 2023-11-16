e107ebb96230dee52b8fb035767cf36340ba9c21
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:37 -0500

    enable autoformatting of buffer

--- home/programs/neovim/xdg-config/after/plugin/lsp.lua
+++ home/programs/neovim/xdg-config/after/plugin/lsp.lua
@@ -2,9 +2,10 @@ local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
   lsp.default_keymaps({buffer = bufnr})
+  lsp.buffer_autoformat()
 end)
 
 -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md for naming
-lsp.setup_servers({'gopls', 'intelephense', 'lua_ls', 'nixd', 'tsserver', 'yamlls'})
+lsp.setup_servers({'gopls', 'intelephense', 'lua_ls', 'nil_ls', 'tsserver', 'yamlls'})
 
 lsp.setup()
