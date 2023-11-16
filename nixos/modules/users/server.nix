162792e37aabf89f5cf81b56af5d85acc3768051
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:39 -0500

    update password file attribute key

--- nixos/modules/users/server.nix
+++ nixos/modules/users/server.nix
@@ -8,7 +8,7 @@
     mutableUsers = false;
     users = {
       root = {
-        passwordFile = config.sops.secrets.root_password.path;
+        hashedPasswordFile = config.sops.secrets.root_password.path;
         openssh.authorizedKeys.keys = [(builtins.readFile ../../hosts/${config.networking.hostName}/ssh_key.pub)];
       };
     };
