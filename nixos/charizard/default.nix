50d21c80bcffa9631d0f8ea70f3a8cff1df8693e
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:33 -0500

    tighten up config

--- nixos/charizard/default.nix
+++ nixos/charizard/default.nix
@@ -39,16 +39,16 @@ in {
 
   services.openssh = {
     enable = true;
-    passwordAuthentication = false;
-    kbdInteractiveAuthentication = false;
+    PasswordAuthentication = false;
+    KbdInteractiveAuthentication = false;
     hostKeys = [
       {
         bits = 4096;
-        path = "/persist/etc/ssh/ssh_host_rsa_key";
+        path = "/etc/ssh/ssh_host_rsa_key";
         type = "rsa";
       }
       {
-        path = "/persist/etc/ssh/ssh_host_ed25519_key";
+        path = "/etc/ssh/ssh_host_ed25519_key";
         type = "ed25519";
       }
     ];
