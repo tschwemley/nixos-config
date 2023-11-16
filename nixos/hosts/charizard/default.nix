f630bea5d1092260f54f159bba97c68b14ca0863
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:36 -0500

    update hardware

--- nixos/hosts/charizard/default.nix
+++ nixos/hosts/charizard/default.nix
@@ -14,8 +14,9 @@
 
   hardware = {
     imports = [
-      inputs.nixos-hardware.nixosModules.common-cpu-intel
-      inputs.nixos-hardware.nixosModules.common-gpu-nvidia
+      inputs.nixos-hardware.nixosModules.common-cpu-intel-cpu-only
+      inputs.nixos-hardware.nixosModules.common-gpu-nvidia-nonprime
+      inputs.nixos-hardware.nixosModules.common-pc
       inputs.nixos-hardware.nixosModules.common-pc-ssd
     ];
   };
