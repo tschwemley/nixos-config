c9d57344bcb0686356a19a825faf18ad4a4ab047
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:39 -0500

    update work home profile

--- home/profiles/work.nix
+++ home/profiles/work.nix
@@ -1,16 +1,25 @@
-{system, ...}: let
-  username = "tschwemley";
-in {
+{
+  pkgs,
+  system,
+  ...
+}: {
   imports = [
     ./.
     ../programs/wezterm
   ];
 
-  home = {
+  home = let
+    username = "tschwemley";
+  in {
     inherit username;
     homeDirectory =
       if system == "x86_64-linux"
       then "/home/${username}"
       else "/Users/${username}";
+
+    # work specific programs to install
+    programs = with pkgs; [
+      mysql
+    ];
   };
 }
