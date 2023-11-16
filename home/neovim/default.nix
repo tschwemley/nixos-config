025f10c150b439d14908fbaa2df2e4202d9e8c0c
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:34 -0500

    testing

--- home/neovim/default.nix
+++ home/neovim/default.nix
@@ -19,16 +19,19 @@ in
   programs.neovim = {
     enable = true;
     defaultEditor = lib.mkDefault true;
-    plugins = with pkgs.vimPlugins; [
-      # I don't know where to put these ones yet
-	  comment
-      nvim-luadev
-      toggleterm-nvim
-      which-key-nvim
-
-      # call the doctor, we diagnosing shit
-      trouble-nvim
-    ];
+	plugins = [
+		comment
+	];
+   #  plugins = with pkgs.vimPlugins; [
+   #    # I don't know where to put these ones yet
+	  # comment
+   #    nvim-luadev
+   #    toggleterm-nvim
+   #    which-key-nvim
+			#
+   #    # call the doctor, we diagnosing shit
+   #    trouble-nvim
+   #  ];
     withPython3 = true;
     withNodeJs = true;
     vimAlias = true;
