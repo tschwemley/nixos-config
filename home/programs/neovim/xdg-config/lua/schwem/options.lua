cbb8015138371a53005980cd9e9ad16ec54b44a9
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:35 -0500

    update home

--- home/programs/neovim/xdg-config/lua/schwem/options.lua
+++ home/programs/neovim/xdg-config/lua/schwem/options.lua
@@ -19,8 +19,8 @@ vim.o.cindent = true
 -- always use unix line endings by default
 -- TODO: I think this is getting mixed up occasionally but I'm too lazy to fix right now so always
 -- unix LE until I can be assed to look into this some more vim.o.fileformats=unix,dos
-vim.o.fileformats=unix
-vim.o.fileformat=unix
+vim.g.fileformats=unix,dos
+vim.g.fileformat=unix
 
 vim.o.wrap = false
 
