e13d834bd3808d2609dd113e775c3dc0e5c6245b
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:34 -0500

    how the fuck did a backtick get added

--- nixos/modules/services/k3s/agent.nix
+++ nixos/modules/services/k3s/agent.nix
@@ -8,7 +8,7 @@
   services.k3s = {
     role = "agent";
     tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
-    serverAddr = lib.mkDefault "10.0.0.1`6443";
+    serverAddr = lib.mkDefault "10.0.0.1:6443";
     extraFlags = "--node-ip 10.0.0.2 --node-external-ip 10.0.0.2 --container-runtime-endpoint unix:///run/containerd/containerd.sock";
   };
 }
