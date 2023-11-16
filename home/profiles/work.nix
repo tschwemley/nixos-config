d809ee326ef8002af12f78f6ff95efa1d48e4024
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:37 -0500

    no capital N

--- home/profiles/work.nix
+++ home/profiles/work.nix
@@ -1,15 +1,15 @@
-{pkgs, ...}: let
-  userName = "tschwemley";
+let
+  username = "tschwemley";
 in {
   imports = [
     ./.
   ];
 
   home = {
-    inherit userName;
+    inherit username;
     homeDirectory =
       if builtins.currentSystem == "x86_64-linux"
-      then "/home/${userName}"
-      else "/Users/${userName}";
+      then "/home/${username}"
+      else "/Users/${username}";
   };
 }
