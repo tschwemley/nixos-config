a24c95432aa8f8f46d66a3f4d0ba991c40b55526
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:36 -0500

    update machamp

--- nixos/hosts/machamp/default.nix
+++ nixos/hosts/machamp/default.nix
@@ -11,22 +11,21 @@
   # TODO: change this to (and create) a hydra profile
   disk = import ../../modules/hardware/disks/k3s.nix {inherit diskName;}; # TODO: not a k3s machine but this is easy
   profile = import ../../profiles/hydra.nix;
-  impermanence = import ../../modules/system/impermanence.nix {inherit inputs;};
   user = import ../../modules/users/server.nix {
     inherit config;
     userName = hostName;
   };
-  wireguard = import ../../modules/networking/wireguard.nix {
-    inherit config;
-    ip = wireguardIP;
-    peers = [
-      {
-        # articuno
-        PublicKey = "1YcCJFA6eAskLk0/XpBYwdqbBdHgNRaW06ZdkJs8e1s=";
-        AllowedIPs = ["10.0.0.1/32"];
-      }
-    ];
-  };
+  # wireguard = import ../../modules/networking/wireguard.nix {
+  #   inherit config;
+  #   ip = wireguardIP;
+  #   peers = [
+  #     {
+  #       # articuno
+  #       PublicKey = "1YcCJFA6eAskLk0/XpBYwdqbBdHgNRaW06ZdkJs8e1s=";
+  #       AllowedIPs = ["10.0.0.1/32"];
+  #     }
+  #   ];
+  # };
 in {
   imports = [
     disk
@@ -51,8 +50,7 @@ in {
   };
 
   networking.hostName = hostName;
-  # TODO: determine if this is needed for qemu
-  #services.getty.autologinUser = "root";
+  services.getty.autologinUser = "root";
 
   # don't update this
   system.stateVersion = "23.05";
--- nixos/profiles/hydra.nix
+++ nixos/profiles/hydra.nix
@@ -1,3 +1,11 @@
-{...}: {
-  imports = [./.];
+{ inputs,...}: 
+let
+  impermanence = import ../../modules/system/impermanence.nix {inherit inputs;};
+in
+{
+  imports = [
+    impermanence
+    ./server.nix
+    ../modules/services/cloudflared.nix
+  ];
 }
