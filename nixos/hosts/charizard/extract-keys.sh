9c06ac280608b923735a454856aa70d39d0da0df
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:36 -0500

    update script

--- nixos/hosts/charizard/extract-keys.sh
+++ nixos/hosts/charizard/extract-keys.sh
@@ -2,5 +2,7 @@
 ssh_dir="$HOME/.ssh"
 secrets_dir="$HOME/nixos-config/nixos/hosts/charizard/secrets.yaml"
 sops -d --extract '["ssh_config"]'  $secrets_dir > "$ssh_dir/config"
-sops -d --extract '["gh_ssh_key"]' $secrets_dir > "$ssh_dir/github"
+sops -d --extract '["gh_ssh_key"]' $secrets_dir > "$ssh_dir/personal-gh"
 sops -d --extract '["mac_ssh_key"]' $secrets_dir > "$ssh_dir/mac"
+
+chmod 400 $ssh_dir/personal-gh $ssh_dir/mac
