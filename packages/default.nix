dff16695ba93e80d0c9a73b87267844d0f4ff390
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:37 -0500

    add bambu studio package

--- packages/default.nix
+++ packages/default.nix
@@ -1,3 +1,11 @@
-{...}: {
-  imports = [];
+{ ... }: {
+  perSystem = { pkgs, ... }: {
+    bambuStudio = pkgs.appImageTools.wrapType2 {
+      name = "bambu-studio";
+      src = builtins.fetchurl {
+        url = "https://github.com/bambulab/BambuStudio/releases/download/v01.07.04.52/Bambu_Studio_linux_ubuntu-v01.07.04.52.AppImage";
+        hash = "sha256-1s53vj3s9zjg0p1p3fxynxd5c0lw6h0lyywbc3v89zzgghvy9jpk";
+      };
+    };
+  };
 }
