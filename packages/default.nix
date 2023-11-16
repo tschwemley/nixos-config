076cb894992384ea6bf2f59244fcdd7c4f30e60d
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:38 -0500

    move hyprbars plugin to hyprland home module

--- packages/default.nix
+++ packages/default.nix
@@ -7,25 +7,6 @@
     packages = {
       # see: https://github.com/NixOS/nixpkgs/pull/206495/files for merge
       # bambuStudio = pkgs.stdenv.mkDerivation {};
-
-      hyprbars = let
-        hyprland = pkgs.hyprland;
-      in
-        pkgs.stdenv.mkDerivation {
-          pname = "hyprbars";
-          version = "0.1";
-          src = pkgs.fetchFromGitHub {
-            owner = "hyprwm";
-            repo = "hyprland-plugins";
-            rev = "b5d0cfdff726676b8c6df564fdb1732558d6c25b";
-            sha256 = "sha256-US7WfYWqMax+uEaRPpCOn8ib2pLYDLa5Fy8dIjWH0aM=";
-          };
-          sourceRoot = "source/hyprbars";
-
-          inherit (hyprland) nativeBuildInputs;
-
-          buildInputs = [hyprland] ++ hyprland.buildInputs;
-        };
     };
   };
 }
