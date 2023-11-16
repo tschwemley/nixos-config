a1881b9cbbb5c6204e438b75023b2358b20a83f9
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:33 -0500

    update home path

--- nixos/modules/users/schwem.nix
+++ nixos/modules/users/schwem.nix
@@ -5,11 +5,11 @@
     schwem = {
       isNormalUser = true;
       extraGroups = ["wheel" "networkmanager"];
-      openssh.authorizedKeys.keys = [(builtins.readFile ../../secrets/keys/charizard.pub)];
+      # openssh.authorizedKeys.keyFiles = [(builtins.readFile ../../secrets/keys/charizard.pub)];
     };
   };
 
   home-manager.users.schwem.imports = [
-    ../../home/profiles/pc.nix
+    ../../../home/profiles/pc.nix
   ];
 }
