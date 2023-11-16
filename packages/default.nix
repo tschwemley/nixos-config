46d1e9cbdcdb959e476e63e59fe884b108bee023
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:37 -0500

    proper set of packages

--- packages/default.nix
+++ packages/default.nix
@@ -1,10 +1,12 @@
 { ... }: {
   perSystem = { pkgs, ... }: {
-    bambuStudio = pkgs.appimageTools.wrapType2 {
-      name = "bambu-studio";
-      src = builtins.fetchurl {
-        url = "https://github.com/bambulab/BambuStudio/releases/download/v01.07.04.52/Bambu_Studio_linux_ubuntu-v01.07.04.52.AppImage";
-        hash = "sha256-1s53vj3s9zjg0p1p3fxynxd5c0lw6h0lyywbc3v89zzgghvy9jpk";
+    packages = {
+      bambuStudio = pkgs.appimageTools.wrapType2 {
+        name = "bambu-studio";
+        src = builtins.fetchurl {
+          url = "https://github.com/bambulab/BambuStudio/releases/download/v01.07.04.52/Bambu_Studio_linux_ubuntu-v01.07.04.52.AppImage";
+          hash = "sha256-1s53vj3s9zjg0p1p3fxynxd5c0lw6h0lyywbc3v89zzgghvy9jpk";
+        };
       };
     };
   };
