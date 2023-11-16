40f63eea0d56110ef9fd862d7937ac6d60bdb754
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:38 -0500

    update packages

--- packages/default.nix
+++ packages/default.nix
@@ -5,17 +5,8 @@
     ...
   }: {
     packages = {
-      bambuStudio = pkgs.stdenv.mkDerivation rec {
-        builder = "${pkgs.bash}/bin/bash";
-        pname = "bambu-studio";
-        version = "01.07.04.52";
-        src = pkgs.fetchFromGitHub {
-          repo = "BambuStudio";
-          owner = "bambulab";
-          rev = "v${version}";
-          hash = "sha256-JYSSah2KiNn9jb0X/GZFFziQpP11eXc66ozh1L6Zcf0=";
-        };
-      };
+      # see: https://github.com/NixOS/nixpkgs/pull/206495/files for merge
+      # bambuStudio = pkgs.stdenv.mkDerivation {};
 
       localAI = pkgs.stdenv.mkDerivation {
         pname = "local-ai";
@@ -26,13 +17,12 @@
           repo = "LocalAI";
           rev = "9e5fb2996582bc45e5a9cbe6f8668e7f1557c15a";
           sha256 = "bk9mYIgwZJpLB9+aOYM2cjhOLztT8Z2Ftt++GPHptS8=";
-          deepClone = true;
           fetchSubmodules = true;
         };
-        makeFlags = [
-          "build"
-        ];
-        # installTargets = "build";
+
+        buildPhase = ''
+          echo $src
+        '';
       };
     };
   };
