09c167ca1fbd9b00bc0b0dfeca3c0077021da355
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:35 -0500

    clean up nvidia

--- nixos/modules/hardware/nvidia.nix
+++ nixos/modules/hardware/nvidia.nix
@@ -14,11 +14,4 @@
   environment.sessionVariables = {
     LIBVA_DRIVER_NAME = "VDPAU";
   };
-  # TODO: I don't know if this is necessary using vdpau/libvdpau-va-gl. reassess
-  /*
-     environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "VDPAU";
-    #LIBVA_DRIVER_NAME = "iHD";
-  };
-  */
 }
