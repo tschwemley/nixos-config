a400230f9d138d454241039fc7645c2fdc5d58a2
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:38 -0500

    fuck it try this shit

--- home/programs/neovim/config/after/plugin/lsp.lua
+++ home/programs/neovim/config/after/plugin/lsp.lua
@@ -12,27 +12,27 @@ lsp.on_attach(function(client, bufnr)
          D = { vim.lsp.buf.declaration, 'Go To Declaration', { buffer = true } },
       },
    })
-
-   whichKey.register({
-      l = {
-         name = 'LSP',
-         c = { '<cmd>Telescope lsp_document_symbols ignore_symbols=class,function,method,property,variable<cr>',
-            'List Constants',
-            { buffer = true } },
-         r = { vim.lua.lsp.buf.rename, 'List References', { buffer = true } },
-         R = { '<cmd>Telescope lsp_references<cr>', 'List References', { buffer = true } },
-         m = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,property,variable<cr>', 'List Methods',
-            { buffer = true } },
-         p = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,function,method,variable<cr>',
-            'List Properties',
-            { buffer = true } },
-         s = { '<cmd>Telescope lsp_document_symbols<cr>', 'List Symbols', { buffer = true } },
-         v = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,function,method,property<cr>',
-            'List Variables',
-            { buffer = true } },
-      },
-   }, { prefix = '<leader>' })
 end)
+
+whichKey.register({
+   l = {
+      name = 'LSP',
+      c = { '<cmd>Telescope lsp_document_symbols ignore_symbols=class,function,method,property,variable<cr>',
+         'List Constants',
+         { buffer = true } },
+      r = { vim.lua.lsp.buf.rename, 'List References', { buffer = true } },
+      R = { '<cmd>Telescope lsp_references<cr>', 'List References', { buffer = true } },
+      m = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,property,variable<cr>', 'List Methods',
+         { buffer = true } },
+      p = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,function,method,variable<cr>',
+         'List Properties',
+         { buffer = true } },
+      s = { '<cmd>Telescope lsp_document_symbols<cr>', 'List Symbols', { buffer = true } },
+      v = { '<cmd>Telescope lsp_document_symbols ignore_symbols=constant,class,function,method,property<cr>',
+         'List Variables',
+         { buffer = true } },
+   },
+}, { prefix = '<leader>' })
 
 -- see: https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md for naming
 -- lsp.setup_servers({ 'astro', 'gopls', 'intelephense', 'lua_ls', 'tsserver', 'yamlls' })
