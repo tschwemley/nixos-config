ebcff40620fa7b9d43a8a93034476fc7c9b70579
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:34 -0500

    update lsp and completion lua

--- home/xdg-config/nvim/lua/schwem/completion.lua
+++ home/xdg-config/nvim/lua/schwem/completion.lua
@@ -1,14 +1,15 @@
 local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
 	mapping = {
 		-- enable super tab
 		['<Tab>'] = cmp_action.luasnip_supertab(),
 		['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-	}
+	},
 
 	sources = {
 		{ name = 'nvim_lsp' },
 		{ name = 'nvim_lua' },
-	}
+	},
 })
