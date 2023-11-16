a8b0d10c41a0cf60106f411f8dfb4403ad791412
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:38 -0500

    update nnn config

--- home/programs/nnn.nix
+++ home/programs/nnn.nix
@@ -1,6 +1,8 @@
-{pkgs, ...}: let
-  plugins = [];
-in {
+{
+  pkgs,
+  system,
+  ...
+}: {
   home.sessionVariables = {
     NNN_FIFO = "/tmp/nnn.fifo";
   };
@@ -11,14 +13,20 @@ in {
       p = "~/projects";
       n = "~/nixos-config";
     };
-    extraPackages = with pkgs; [imv];
-    plugins = {
-      src = "${pkgs.nnn.src}/plugins";
-      mappings = {
-        f = "finder";
-        p = "preview-tui";
-        v = "imgview";
+    extraPackages =
+      if system == "aarch64-darwin"
+      then []
+      else with pkgs; [imv];
+    plugins =
+      if system == "aarch64-darwin"
+      then {}
+      else {
+        src = "${pkgs.nnn.src}/plugins";
+        mappings = {
+          f = "finder";
+          p = "preview-tui";
+          v = "imgview";
+        };
       };
-    };
   };
 }
