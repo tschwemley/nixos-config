49355c0b8438a2442c245ca6014abde105c21bf0
Tyler Schwemley <tjschwem@gmail.com>
2023-11-15 19:19:39 -0500

    update neovim completion

--- home/programs/neovim/config/after/plugin/completion.lua
+++ home/programs/neovim/config/after/plugin/completion.lua
@@ -1,7 +1,11 @@
-local copilot = require("copilot")
 local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
--- require('luasnip')
+
+local has_words_before = function()
+   if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+   return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+end
 
 vim.api.nvim_create_autocmd("InsertEnter", {
    callback = function()
@@ -10,6 +14,8 @@ vim.api.nvim_create_autocmd("InsertEnter", {
          suggestion = { enabled = false },
          panel = { enabled = false },
       })
+      -- TODO: probably don't want this permanently disabled but for now it's annoying as fuck
+      -- require('copilot_cmp').setup()
    end
 })
 
@@ -25,8 +31,24 @@ cmp.setup({
       { name = 'nvim_lua' },
       { name = 'path' },
    },
-   mapping = cmp.mapping.preset.insert({
-      ['<Tab>'] = cmp_action.luasnip_supertab(),
-      ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
-   })
+   mapping = {
+      ["<Tab>"] = vim.schedule_wrap(function(fallback)
+         if cmp.visible() and has_words_before() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+         else
+            fallback()
+         end
+      end),
+      ["<S-Tab>"] = vim.schedule_wrap(function(fallback)
+         if cmp.visible() and has_words_before() then
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+         else
+            fallback()
+         end
+      end),
+   },
+   -- mapping = cmp.mapping.preset.insert({
+   --    ['<Tab>'] = cmp_action.luasnip_supertab(),
+   --    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+   -- })
 })
