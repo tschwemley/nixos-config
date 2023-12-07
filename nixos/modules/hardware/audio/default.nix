ed568cf339a644505d6f7d7690d5deeba901a1a6
Tyler Schwemley <tjschwem@gmail.com>
2023-12-07 01:18:01 -0500

    update audio config

--- nixos/modules/hardware/audio/default.nix
+++ nixos/modules/hardware/audio/default.nix
@@ -1,15 +1,11 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+{inputs, ...}: {
   imports = [
     inputs.musnix.nixosModules.musnix
+    ./bluetooth.nix
     ./pipewire.nix
     ./scarlett8i6.nix
   ];
 
-  hardware.bluetooth.enable = true;
   musnix.enable = true;
 
   sound.enable = true;
--- nixos/modules/hardware/audio/pipewire.nix
+++ nixos/modules/hardware/audio/pipewire.nix
@@ -1,26 +1,17 @@
 {pkgs, ...}: {
-  # The mSBC codec provides slightly better sound quality in calls than regular HFP/HSP, while the
-  # SBC-XQ provides better sound quality for audio listening. see:
-  # https://www.guyrutenberg.com/2021/03/11/replacing-pulseaudio-with-pipewire/
   environment = {
     etc = {
-      # "pipewire/pipewire.conf.d/92-low-latency.conf".text = ''
-      #   context.properties = {
-      #     default.clock.rate = 48000
-      #     default.clock.quantum = 32
-      #     default.clock.min-quantum = 32
-      #     default.clock.max-quantum = 32
-      #   }
-      # '';
-
-      "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-        bluez_monitor.properties = {
-          ["bluez5.enable-sbc-xq"] = true,
-          ["bluez5.enable-msbc"] = true,
-          ["bluez5.enable-hw-volume"] = true,
-          ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+      # low latency configuration
+      "pipewire/pipewire.conf.d/92-low-latency.conf".text = ''
+        context.properties = {
+          default.clock.rate = 48000
+          default.clock.quantum = 32
+          default.clock.min-quantum = 32
+          default.clock.max-quantum = 32
         }
       '';
+      # TODO: configuration for pulse audio and alsa
+      # see: {https://nixos.wiki/wiki/PipeWire#Low-latency_setup}
     };
 
     systemPackages = with pkgs; [pwvucontrol];
