d3a0a4f56824e77fbbb9c127b603813aec6117a9
Tyler Schwemley <tjschwem@gmail.com>
2023-12-25 22:49:09 -0500

    add randstr alias

--- home/shell/aliases/default.nix
+++ home/shell/aliases/default.nix
@@ -2,5 +2,6 @@ let
   git = import ./git.nix;
   nix = import ./nix.nix;
   qol = import ./qol.nix;
+  useful = import ./useful.nix;
 in
-  git // nix // qol
+  git // nix // qol // useful
---
+++ home/shell/aliases/useful.nix
@@ -0,0 +1,3 @@
+{
+  "randstr" = "head /dev/urandom | tr -dc A-Za-z0-9 | head -c10";
+}
