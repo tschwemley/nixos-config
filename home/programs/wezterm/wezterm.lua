ceea8410fb8f879396721b0c97772f733723976b
Tyler Schwemley <tjschwem@gmail.com>
2023-12-28 22:14:59 -0500

    update wezterm

--- home/programs/wezterm/wezterm.lua
+++ home/programs/wezterm/wezterm.lua
@@ -1,10 +1,44 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
+
 local config = {
    audible_bell = 'Disabled',
    color_scheme = 'Gruvbox dark, medium (base16)',
    enable_wayland = false,
    font = wezterm.font('Hasklig'),
    font_size = 20,
+   keys = {
+      {
+         key = '%',
+         mods = 'CTRL|SHIFT',
+         action = act.SplitHorizontal { domain = 'CurrentPaneDomain' },
+      },
+      {
+         key = '"',
+         mods = 'CTRL|SHIFT',
+         action = act.SplitVertical { domain = 'CurrentPaneDomain' },
+      },
+      {
+         key = 'h',
+         mods = 'CTRL',
+         action = act.ActivatePaneDirection 'Left',
+      },
+      {
+         key = 'l',
+         mods = 'CTRL',
+         action = act.ActivatePaneDirection 'Right',
+      },
+      {
+         key = 'k',
+         mods = 'CTRL',
+         action = act.ActivatePaneDirection 'Up',
+      },
+      {
+         key = 'j',
+         mods = 'CTRL',
+         action = act.ActivatePaneDirection 'Down',
+      },
+   },
    ssh_domains = wezterm.default_ssh_domains(),
    term = "wezterm",
 }
