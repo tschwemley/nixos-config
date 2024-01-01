8e5e75758b8e6f2e825a66db21143548a528e408
rhoffer21 <rhoffer21@gmail.com>
2024-01-01 18:26:12 -0500

    update homemanager config

--- home/profiles/default.nix
+++ home/profiles/default.nix
@@ -1,6 +1,6 @@
-{inputs, ...}: {
+{...}: {
   imports = [
-    inputs.sops.homeManagerModule
+    # inputs.sops.homeManagerModule
     ../programs/bat.nix
     ../programs/neovim
     ../programs/nnn.nix
@@ -13,8 +13,6 @@
   nixpkgs = {
     config = {
       allowUnfree = true;
-      # TODO: check if this is still necessary with my current config
-      # necessary due to home manager BUG: https://github.com/nix-community/home-manager/issues/2942#issuecomment-1119760100
       allowUnfreePredicate = pkg: true;
     };
   };
