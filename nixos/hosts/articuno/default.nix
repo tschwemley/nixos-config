3a2ecd34c07b747c01eec42edd11df05c26cc430
rhoffer21 <rhoffer21@gmail.com>
2024-01-01 20:30:01 -0500

    update psql

--- nixos/hosts/articuno/default.nix
+++ nixos/hosts/articuno/default.nix
@@ -88,9 +88,8 @@ in {
     identMap = ''
       # ArbitraryMapName systemUser DBUser
          superuser_map      root      postgres
-         # superuser_map      postgres  postgres
          # Let other names login as themselves
-         # superuser_map      /^(.*)$   \1
+         superuser_map      /^(.*)$   \1
     '';
 
     # initialScript = pkgs.writeText "backend-initScript" ''
