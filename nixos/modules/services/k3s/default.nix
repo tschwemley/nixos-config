fbc00082736ec4afeaf1f3089c7e57da77b987e6
Tyler Schwemley <tjschwem@gmail.com>
2024-01-03 18:01:04 -0500

    re-enable token

--- nixos/modules/services/k3s/default.nix
+++ nixos/modules/services/k3s/default.nix
@@ -41,15 +41,15 @@ in {
   services.k3s = {
     inherit extraFlags role serverAddr;
     enable = true;
-    # tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
+    tokenFile = lib.mkDefault config.sops.secrets.k3s-server-token.path;
   };
 
-  # sops.secrets = {
-  #   k3s-server-token = {
-  #     sopsFile = ./secrets.yaml;
-  #     path = "/var/lib/rancher/k3s/server/token";
-  #   };
-  # };
+  sops.secrets = {
+    k3s-server-token = {
+      sopsFile = ./secrets.yaml;
+      path = "/var/lib/rancher/k3s/server/token";
+    };
+  };
 
   systemd.services = {
     k3s = {
