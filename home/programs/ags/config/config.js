cd348fdeb93c3e61e34b70a8a67cb06c1f760173
Tyler Schwemley <tjschwem@gmail.com>
2024-01-03 20:20:26 -0500

    add style for ags and update fonts

--- home/programs/ags/config/config.js
+++ home/programs/ags/config/config.js
@@ -1,6 +1,8 @@
+import App from 'resource:///com/github/Aylur/ags/app.js';
 import TopBar from './topbar/index.js';
 
 export default {
+	style: `${App.configDir}/style.css`,
 	windows: [
 		TopBar(),
 	],
--- nixos/modules/system/fonts.nix
+++ nixos/modules/system/fonts.nix
@@ -4,17 +4,11 @@
   ];
 
   fonts = {
-    #TODO: this option was renamed from enableDefaultFonts on 07/24/23. Now is a good time to decide
-    # if I want to keep it or not
-    #enableDefaultPackages = true;
-
     packages = with pkgs; [
-      # NOTE: Some fonts may break colour emojis in Chrome
-      # cf. https://github.com/NixOS/nixpkgs/issues/69073#issuecomment-621982371
-      # If this happens , keep noto-fonts-emoji and try disabling others (nerdfonts, etc.)
-      noto-fonts-emoji
       fira-code
       hasklig
+      nerdfonts
+      noto-fonts-emoji
     ];
 
     fontconfig = {
