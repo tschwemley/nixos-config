d7462f94a5c20b7a88397b0f9a2e8881b5492969
Tyler Schwemley <tjschwem@gmail.com>
2024-01-06 00:09:10 -0500

    test droid

--- droid/default.nix
+++ droid/default.nix
@@ -1,10 +1,7 @@
 {inputs, ...}: {
   flake.nixOnDroidConfigurations.default = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
     modules = [
-      {
-        system.stateVersion = "23.05";
-        home-manager.config = ../home/profiles/pc.nix;
-      }
+      ../nixos/profiles/droid.nix
     ];
   };
 }
