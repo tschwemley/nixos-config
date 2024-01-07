9d5d2717cef25fa86fce2100f8514fbdaf3651a7
Tyler Schwemley <tjschwem@gmail.com>
2024-01-06 21:14:25 -0500

    update nix-on-droid config

--- nixos/profiles/droid.nix
+++ nixos/profiles/droid.nix
@@ -1,32 +1,4 @@
 # TODO: clean this up and standardize
-{
-  inputs,
-  lib,
-  pkgs,
-  ...
-}: {
-  imports = [
-    inputs.sops.nixosModules.sops
-    ../modules/programs/zsh.nix
-    ../modules/services/openssh.nix
-    ../modules/system/nix.nix
-  ];
-
-  # basic tools I want available on every host and managed by the system
-  environment.systemPackages = with pkgs; [
-    curl
-    git
-    gnupg
-    htop
-    jq
-    lsof
-    pinentry
-    ripgrep
-    rsync
-    unzip
-    wget
-    zip
-  ];
-
-  nixpkgs.config.allowUnfree = lib.mkDefault true;
+{...}: {
+  home-manager.config = ../../home/profiles/pc.nix;
 }
