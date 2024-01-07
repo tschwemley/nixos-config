a133cca71f3a85de63fe1b35c03c08527302f562
Tyler Schwemley <tjschwem@gmail.com>
2024-01-07 00:12:57 -0500

    test dns update on articuno

--- nixos/hosts/articuno/default.nix
+++ nixos/hosts/articuno/default.nix
@@ -83,6 +83,8 @@ in {
     ];
   };
 
+  services.resolved.extraConfig = "DNS=10.43.0.10 1.1.1.1 1.0.0.1 2606:4700:4700::1111 2606:4700:4700::1001";
+
   sops = {
     defaultSopsFile = ./secrets.yaml;
     age.keyFile = "/root/.config/sops/age/keys.txt";
