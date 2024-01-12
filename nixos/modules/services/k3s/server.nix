7d73762b15de8561547970c3c3717e26ca58b3c7
Tyler Schwemley <tjschwem@gmail.com>
2024-01-11 21:31:39 -0500

    k9s cluster config

--- nixos/modules/services/k3s/server.nix
+++ nixos/modules/services/k3s/server.nix
@@ -5,6 +5,12 @@
     programs.k9s.enable = true;
   };
 
+  home.file."" = {
+    enable = true;
+    source = ./k9s-config/clusters;
+    target = ".local/share/k9s/clusters";
+  };
+
   networking.firewall.allowedTCPPorts = [
     6443 # api server
   ];
