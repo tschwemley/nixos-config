843e0b72d5442591f8705fa3a4b5f8386a5a8b6e
Tyler Schwemley <tjschwem@gmail.com>
2024-01-16 16:24:48 -0500

    add to sysmonitor and refactor

--- home/programs/ags/config/topbar/widgets/sysmonitor.js
+++ home/programs/ags/config/topbar/widgets/sysmonitor.js
@@ -5,30 +5,34 @@ import Widget from 'resource:///com/github/Aylur/ags/widget.js';
 const iconPath = `${App.configDir}/topbar/icons`;
 const divide = ([total, free]) => free / total;
 
-const cpu = Variable(0, {
-	poll: [2000, 'top -b -n 1', out => divide([100, out.split('\n')
-		.find(line => line.includes('Cpu(s)'))
-		.split(/\s+/)[1]
-		.replace(',', '.')])],
-});
+const variables = {
+	cpu: Variable(0, {
+		poll: [2000, 'top -b -n 1', out => divide([100, out.split('\n')
+			.find(line => line.includes('Cpu(s)'))
+			.split(/\s+/)[1]
+			.replace(',', '.')])],
+	}),
 
-const ram = Variable(0, {
-	poll: [2000, 'free', out => divide(out.split('\n')
-		.find(line => line.includes('Mem:'))
-		.split(/\s+/)
-		.splice(1, 2))],
-});
+	ram: Variable(0, {
+		poll: [2000, 'free', out => divide(out.split('\n')
+			.find(line => line.includes('Mem:'))
+			.split(/\s+/)
+			.splice(1, 2))],
+	}),
 
-const disk = Variable(0, {
-	// poll: [60000, 'df /', out => divide([100, out])]
-	poll: [1000, 'df /', out => divide([100, out.split('\n')
-		.find(line => line.includes('% /'))
-		.split(/\s+/)
-		.splice(4, 1)
-		.pop()
-		.slice(0, -1)])],
-});
+	disk: Variable(0, {
+		// poll: [60000, 'df /', out => divide([100, out])]
+		poll: [1000, 'df /', out => divide([100, out.split('\n')
+			.find(line => line.includes('% /'))
+			.split(/\s+/)
+			.splice(4, 1)
+			.pop()
+			.slice(0, -1)])],
+	}),
 
+};
+
+const { cpu, disk, ram, } = variables;
 const cpuProgress = Widget.CircularProgress({
 	child: Widget.Icon({
 		icon: `${iconPath}/cpu.svg`,
