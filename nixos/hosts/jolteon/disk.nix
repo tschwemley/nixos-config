09ef670a81d7bfcae199dde328c642b922dee3cf
Tyler Schwemley <tjschwem@gmail.com>
2024-01-20 01:33:17 -0500

    fix jolteon disk?

--- nixos/hosts/jolteon/disk.nix
+++ nixos/hosts/jolteon/disk.nix
@@ -6,62 +6,63 @@
 }: let
   # TODO: make this generic
   mainPartitions =
-    if useGrub
-    then [
-      {
-        name = "boot";
-        start = "0";
-        end = "1M";
-        flags = ["bios_grub"];
-      }
-    ]
-    else
-      []
-      // [
+    (
+      if useGrub
+      then [
         {
-          name = "ESP";
-          start = "1MiB";
-          end = "128MiB";
-          bootable = true;
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-          };
+          name = "boot";
+          start = "0";
+          end = "1M";
+          flags = ["bios_grub"];
         }
-        {
-          name = "root";
-          start = "128MiB";
-          end = "100%";
-          part-type = "primary";
-          content = {
-            type = "btrfs";
-            extraArgs = ["-f"];
-            subvolumes = {
-              "/rootfs" = {
-                mountpoint = "/";
-                mountOptions = ["compress=lzo"];
-              };
-              "/home" = {
-                mountpoint = "/home";
-                mountOptions = [];
-              };
-              "/nix" = {
-                mountpoint = "/nix";
-                mountOptions = ["compress=lzo" "noatime"];
-              };
-              "/persist" = {
-                mountpoint = "/persist";
-                mountOptions = ["compress=lzo" "noatime"];
-              };
+      ]
+      else []
+    )
+    // [
+      {
+        name = "ESP";
+        start = "1MiB";
+        end = "128MiB";
+        bootable = true;
+        content = {
+          type = "filesystem";
+          format = "vfat";
+          mountpoint = "/boot";
+        };
+      }
+      {
+        name = "root";
+        start = "128MiB";
+        end = "100%";
+        part-type = "primary";
+        content = {
+          type = "btrfs";
+          extraArgs = ["-f"];
+          subvolumes = {
+            "/rootfs" = {
+              mountpoint = "/";
+              mountOptions = ["compress=lzo"];
+            };
+            "/home" = {
+              mountpoint = "/home";
+              mountOptions = [];
+            };
+            "/nix" = {
+              mountpoint = "/nix";
+              mountOptions = ["compress=lzo" "noatime"];
+            };
+            "/persist" = {
+              mountpoint = "/persist";
+              mountOptions = ["compress=lzo" "noatime"];
             };
           };
-        }
-      ];
+        };
+      }
+    ];
 
   storagePartitions = [
     {
-      name = "root";
+      name = "storage";
       start = "0";
       end = "100%";
       part-type = "primary";
