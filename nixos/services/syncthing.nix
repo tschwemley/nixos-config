2eefaa8fca0714dfb9d8c8626d3b75a03318ab1f
Tyler Schwemley <tjschwem@gmail.com>
2024-02-02 23:43:21 -0500

    update syncthing service

--- nixos/services/syncthing.nix
+++ nixos/services/syncthing.nix
@@ -1,7 +1,6 @@
 {
   lib,
   pkgs,
-  enableDiscovery ? false,
   ...
 }: let
   devices = {
@@ -12,7 +11,7 @@
       id = "7VRFMPP-LVHA2JL-UY43A5S-4OORFB4-VSW3HQS-TX2L74G-2XNQ2MM-E77I7QO";
     };
   };
-in rec {
+in {
   # see: https://search.nixos.org/options?channel=23.11&show=services.syncthing.settings.options&from=0&size=50&sort=relevance&type=packages&query=syncthing
   services = {
     syncthing = {
@@ -39,24 +38,24 @@ in rec {
     };
   };
 
-  systemd.services.syncthing-discovery = lib.mkIf enableDiscovery {
-    enable = true;
-    serviceConfig = {
-      Type = "simple";
-      User = "root";
-      Group = "root";
-
-      ExecStart = "${pkgs.syncthing-discovery}/bin/stdiscosrv -http";
-      WorkingDirectory = "${services.syncthing.dataDir}";
-      StateDirectory = ".discovery";
-      SyslogIdentifier = "stdiscosrv";
-    };
-
-    unitConfig = {
-      After = "network.target";
-      Description = "Syncthing discovery service";
-    };
-
-    wantedBy = ["multi-user.target"];
-  };
+  # systemd.services.syncthing-discovery = lib.mkIf enableDiscovery {
+  #   enable = true;
+  #   serviceConfig = {
+  #     Type = "simple";
+  #     User = "root";
+  #     Group = "root";
+  #
+  #     ExecStart = "${pkgs.syncthing-discovery}/bin/stdiscosrv -http";
+  #     WorkingDirectory = "${services.syncthing.dataDir}";
+  #     StateDirectory = ".discovery";
+  #     SyslogIdentifier = "stdiscosrv";
+  #   };
+  #
+  #   unitConfig = {
+  #     After = "network.target";
+  #     Description = "Syncthing discovery service";
+  #   };
+  #
+  #   wantedBy = ["multi-user.target"];
+  # };
 }
