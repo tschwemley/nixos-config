39faa9693b5c2c3397888c674bcd72c9c9309641
Tyler Schwemley <tjschwem@gmail.com>
2024-02-04 22:30:33 -0500

    fix syncthing config

--- nixos/services/syncthing.nix
+++ nixos/services/syncthing.nix
@@ -5,7 +5,9 @@ let
     # };
     eevee = {
       addresses = [
+        "tcp://10.0.0.2:22000"
         "tcp://10.0.0.3:22000"
+        "tcp://10.0.0.4:22000"
         "tcp://10.0.0.5:22000"
         "tcp://10.0.0.6:22000"
       ];
@@ -13,22 +15,38 @@ let
     };
     flareon = {
       addresses = [
+        "tcp://10.0.0.2:22000"
         "tcp://10.0.0.3:22000"
         "tcp://10.0.0.4:22000"
+        "tcp://10.0.0.5:22000"
         "tcp://10.0.0.6:22000"
       ];
       id = "FDYU7II-QX4EIXB-QCO7GFQ-ZPVBCP6-5HS3GZ4-MY5XGDG-YZUG7VX-74CL3QD";
     };
     jolteon = {
       addresses = [
+        "tcp://10.0.0.2:22000"
         "tcp://10.0.0.3:22000"
         "tcp://10.0.0.4:22000"
         "tcp://10.0.0.5:22000"
+        "tcp://10.0.0.6:22000"
       ];
       id = "UTSR43R-YMCLU6Z-4G7G3XI-IPNXSYO-5XK25ZT-MXYLLNE-G76NHH3-V55CHQQ";
     };
     moltres = {
       addresses = [
+        "tcp://10.0.0.2:22000"
+        "tcp://10.0.0.3:22000"
+        "tcp://10.0.0.4:22000"
+        "tcp://10.0.0.5:22000"
+        "tcp://10.0.0.6:22000"
+      ];
+      id = "BGTTFO3-3BWN3QE-I5NXYJT-34MNN3X-QXRB22Z-GIZYIFS-SKZ2J3W-ZXKACQE";
+    };
+    zapados = {
+      addresses = [
+        "tcp://10.0.0.2:22000"
+        "tcp://10.0.0.3:22000"
         "tcp://10.0.0.4:22000"
         "tcp://10.0.0.5:22000"
         "tcp://10.0.0.6:22000"
@@ -40,7 +58,7 @@ let
   folders = {
     "/storage/media" = {
       id = "media";
-      device = ["moltres" "eevee" "flareon" "jolteon"];
+      devices = ["zapados" "moltres" "eevee" "flareon" "jolteon"];
     };
   };
 in {
