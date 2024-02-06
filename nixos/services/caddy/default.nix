a07ba88d5b90a929db91e9738a614def146e2fc0
Tyler Schwemley <tjschwem@gmail.com>
2024-02-06 14:37:05 -0500

    update caddy config

--- nixos/services/caddy/default.nix
+++ nixos/services/caddy/default.nix
@@ -1,14 +1,15 @@
-{config, ...}: {
-  config.sops.secrets.caddy_email = {
-    sopsFile = ./secrets.yaml;
-  };
-
+{
   networking.allowedTCPPorts = [80 443];
 
   services.caddy = {
     enable = true;
 
     acmeCA = "https://acme-staging-v02.api.letsencrypt.org/directory";
-    email = builtins.readFile config.sops.caddy_email.path;
+    email = "automation@schwem.io";
+  };
+
+  # TODO: decide if worth keeping or not
+  sops.secrets.caddy_email = {
+    sopsFile = ./secrets.yaml;
   };
 }
