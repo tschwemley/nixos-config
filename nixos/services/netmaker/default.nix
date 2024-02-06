b5f542e59208175b04a21f60ba3e4ec8421ee34d
Tyler Schwemley <tjschwem@gmail.com>
2024-02-05 21:41:37 -0500

    allow netmaker mq to listen on all addresses

--- nixos/services/netmaker/default.nix
+++ nixos/services/netmaker/default.nix
@@ -30,7 +30,7 @@
   # services.netclient.enable = true;
 
   services.mosquitto = let
-    address = "127.0.0.1";
+    # address = "127.0.0.1";
     settings = {
       allow_anonymous = false;
       # protocol = "websocket";
@@ -41,11 +41,11 @@
     # includeDirs = [./.];
     listeners = [
       {
-        inherit address settings users;
+        inherit settings users;
         port = 1883;
       }
       {
-        inherit address settings users;
+        inherit settings users;
         port = 8883;
       }
     ];
