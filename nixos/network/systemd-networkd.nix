7caad9c958d1bfe4a1b2ed0a8842b6d41cdd61b4
Tyler Schwemley <tjschwem@gmail.com>
2024-02-28 23:44:12 -0500

    systemd-networkd secret

--- nixos/network/systemd-networkd.nix
+++ nixos/network/systemd-networkd.nix
@@ -5,16 +5,21 @@
     gateway = {};
   };
 
-  sops.templates."10-primary".content = ''
-    [Match]
-    Name=ens3
+  sops.templates."10-primary" = {
+    content = ''
+      [Match]
+      Name=ens3
 
-    [Network]
-    Address=${config.sops.placeholder.publicIP}/24
-    DNS=1.1.1.1 1.0.0.1
+      [Network]
+      Address=${config.sops.placeholder.publicIP}/24
+      DNS=1.1.1.1 1.0.0.1
 
-    [Route]
-    Destination=0.0.0.0/0
-    Gateway=${config.sops.placeholder.gateway}
-  '';
+      [Route]
+      Destination=0.0.0.0/0
+      Gateway=${config.sops.placeholder.gateway}
+    '';
+    owner = config.users.users.systemd-network.name;
+    group = config.users.users.systemd-network.group;
+    path = "/etc/systemd/network/10-primary";
+  };
 }
