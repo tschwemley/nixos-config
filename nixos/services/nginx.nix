04b43da27a9336a12591aae23301ebdf206cd41d
Tyler Schwemley <tjschwem@gmail.com>
2024-02-29 23:26:38 -0500

    fix wildcard cert path

--- nixos/services/nginx.nix
+++ nixos/services/nginx.nix
@@ -1,15 +1,9 @@
 {
-  networking.firewall.allowedTCPPorts = [80 443];
-
-  # TODO: move this to it's own file if shit gets complex enough
-  security.acme.acceptTerms = true;
-  security.acme.defaults.email = "automation@schwem.io";
-  # uncomment when testing
-  # security.acme.defaults.server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+  networking.firewall.allowedTCPPorts = [8080];
 
   services.nginx = {
     enable = true;
     recommendedProxySettings = true;
-    recommendedTlsSettings = true;
+    defaultHTTPListenPort = 8080;
   };
 }
