c5c9746d53b14ef97cb374738b7470b8e02d63af
Tyler Schwemley <tjschwem@gmail.com>
2024-03-02 23:34:42 -0500

    switch back to bambu-studio manual build as less error prone

--- home/programs/bambu-studio.nix
+++ home/programs/bambu-studio.nix
@@ -1,38 +1,34 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [bambu-studio];
+{pkgs, ...}: let
+  bambuStudio = pkgs.appimageTools.wrapType2 {
+    name = "bambu-studio";
+    src = pkgs.fetchurl {
+      url = "https://github.com/bambulab/BambuStudio/releases/download/v01.08.00.57/Bambu_Studio_linux_ubuntu_v01.08.00.57-20231109141031.AppImage";
+      hash = "sha256:0h55qvhzj537akygdwwla38c3ycpf2fxl4w2bj2ssb8g07wl5n36";
+    };
+    extraPkgs = pkgs:
+      with pkgs; [
+        glib
+        glib-networking
+        gst_all_1.gst-plugins-bad
+        webkitgtk
+      ];
+  };
+in {
+  home.packages = [bambuStudio];
+  xdg.desktopEntries.bambuStudio = {
+    categories = ["Graphics" "3DGraphics" "Engineering"];
+    exec = "bambu-studio %F";
+    genericName = "3D Printing Software";
+    icon = "BambuStudio";
+    mimeType = [
+      "model/stl"
+      "application/vnd.ms-3mfdocument"
+      "application/prs.wavefront-obj"
+      "application/x-amf"
+    ];
+    name = "Bambu Studio";
+    startupNotify = false;
+    terminal = false;
+    type = "Application";
+  };
 }
-# {pkgs, ...}: let
-#   bambuStudio = pkgs.appimageTools.wrapType2 {
-#     name = "bambu-studio";
-#     src = pkgs.fetchurl {
-#       url = "https://github.com/bambulab/BambuStudio/releases/download/v01.08.00.57/Bambu_Studio_linux_ubuntu_v01.08.00.57-20231109141031.AppImage";
-#       hash = "sha256:0h55qvhzj537akygdwwla38c3ycpf2fxl4w2bj2ssb8g07wl5n36";
-#     };
-#     extraPkgs = pkgs:
-#       with pkgs; [
-#         glib
-#         glib-networking
-#         gst_all_1.gst-plugins-bad
-#         webkitgtk
-#       ];
-#   };
-# in {
-#   home.packages = [bambuStudio];
-#   xdg.desktopEntries.bambuStudio = {
-#     categories = ["Graphics" "3DGraphics" "Engineering"];
-#     exec = "bambu-studio %F";
-#     genericName = "3D Printing Software";
-#     icon = "BambuStudio";
-#     mimeType = [
-#       "model/stl"
-#       "application/vnd.ms-3mfdocument"
-#       "application/prs.wavefront-obj"
-#       "application/x-amf"
-#     ];
-#     name = "Bambu Studio";
-#     startupNotify = false;
-#     terminal = false;
-#     type = "Application";
-#   };
-# }
-
