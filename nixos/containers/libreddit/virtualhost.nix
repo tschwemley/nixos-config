87ffe19ebe109ea76fb2d4976cf3db8e5923f433
Tyler Schwemley <tjschwem@gmail.com>
2024-03-04 00:28:09 -0500

    fix vhost

--- nixos/containers/libreddit/virtualhost.nix
+++ nixos/containers/libreddit/virtualhost.nix
@@ -2,7 +2,7 @@ let
   ip = "10.10.3.2";
   port = "8080";
 in {
-  services.nginx.virtualHosts."db.schwem.io" = {
+  services.nginx.virtualHosts."reddit.schwem.io" = {
     locations."/" = {proxyPass = "http://${ip}:${port}";};
   };
 }
