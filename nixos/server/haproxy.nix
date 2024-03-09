a26e7846fcd53062035cd922de20f0fe4bf68be0
Tyler Schwemley <tjschwem@gmail.com>
2024-03-09 01:37:41 -0500

    test haproxy for galera

--- nixos/server/haproxy.nix
+++ nixos/server/haproxy.nix
@@ -17,12 +17,26 @@ in {
         option forwardfor
         option http-server-close
 
-      # listen mysql
-      #   mode tcp
-      #   bind *:3306
-      #   option tcpka
-      #   option mysql-check user haproxy
-      #   server articuno 10.90. check
+      listen galera-cluster
+        bind *:3306
+        # balance roundrobin
+        mode tcp
+        option tcpka
+        # option mysql-check user haproxy_check post-41
+        # option mysql-check-passwd /path/to/haproxy_mysql_password
+        server articuno articuno.wyvern-map.ts.net:3306 check port 3306 inter 2000 rise 2 fall 3
+        # server galera-node2 100.a.b.c:3306 check port 3306 inter 2000 rise 2 fall 3
+        default-server init-addr none
+
+      listen galera-replication
+        bind *:4567
+        # balance roundrobin
+        mode tcp
+        option tcpka
+        # option mysql-check user haproxy
+        server articuno articuno-wyvern-map.ts.net:4567 check
+
+
 
       frontend www
         bind *:80
