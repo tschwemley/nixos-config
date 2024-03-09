c09ac46befc41940cc45f182112cceba6b29b81b
Tyler Schwemley <tjschwem@gmail.com>
2024-03-09 15:43:19 -0500

    wip galera

--- nixos/containers/mysql/default.nix
+++ nixos/containers/mysql/default.nix
@@ -1,5 +1,5 @@
 {config, ...}: {
-  networking.firewall.allowedTCPPorts = [3306 4567];
+  networking.firewall.allowedTCPPorts = [3306 4567 4568 4444];
 
   containers.mysql = let
     hostName = config.networking.hostName;
@@ -32,7 +32,7 @@
       pkgs,
       ...
     }: {
-      networking.firewall.allowedTCPPorts = [3306 4567];
+      networking.firewall.allowedTCPPorts = [3306 4567 4568 4444];
       services.mysql = {
         enable = true;
         package = pkgs.mariadb;
@@ -42,16 +42,17 @@
             binlog_format = "row";
             default_storage_engine = "InnoDB";
             innodb_autoinc_lock_mode = 2;
-            innodb_force_primary_key = 1;
-            innodb_doublewrite = 1;
-            innodb_flush_log_at_trx_commit = 0;
+            # innodb_force_primary_key = 1;
+            # innodb_doublewrite = 1;
+            # innodb_flush_log_at_trx_commit = 0;
             wsrep_cluster_name = "schwem.io galera cluster";
             wsrep_cluster_address = "gcomm://articuno.wyvern-map.ts.net,moltres.wyvern-map.ts.net";
             wsrep_node_name = hostName;
             wsrep_node_address = "${hostName}.wyvern-map.ts.net";
             wsrep_on = "ON";
             wsrep_provider = "${pkgs.mariadb-galera}/lib/galera/libgalera_smm.so";
-            wsrep_slave_threads = 4;
+            wsrep_sst_method = "rsync";
+            # wsrep_slave_threads = 4;
           };
         };
       };
