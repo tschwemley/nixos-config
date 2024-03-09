f2b34f09449929e8c77ad8c62a2ca6b7cfae260d
Tyler Schwemley <tjschwem@gmail.com>
2024-03-09 00:13:42 -0500

    activate redlib on articuno

--- nixos/hosts/articuno/default.nix
+++ nixos/hosts/articuno/default.nix
@@ -4,7 +4,7 @@ let
   profile = import ../../profiles/buyvm.nix;
   server = [
     ../../containers/keycloak
-    # ../../containers/libreddit
+    ../../containers/redlib
     ../../containers/mysql
     ../../containers/searxng
     ../../server/acme
