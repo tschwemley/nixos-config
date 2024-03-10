711184a0cd7a9d0a26b4a955a1b52fcaed61feb2
Tyler Schwemley <tjschwem@gmail.com>
2024-03-09 22:50:18 -0500

    another galera config change

--- nixos/containers/mysql/default.nix
+++ nixos/containers/mysql/default.nix
@@ -63,11 +63,10 @@
             wsrep_cluster_address = "gcomm://articuno.wyvern-map.ts.net,moltres.wyvern-map.ts.net";
             wsrep_node_name = hostName;
             wsrep_node_address = "${hostName}.wyvern-map.ts.net";
-            # wsrep_node_address = "127.0.0.1";
             wsrep_on = "ON";
             wsrep_provider = "${pkgs.mariadb-galera}/lib/galera/libgalera_smm.so";
             wsrep_sst_method = "rsync";
-            # wsrep_slave_threads = 4;
+            wsrep_slave_threads = 4;
           };
         };
       };
