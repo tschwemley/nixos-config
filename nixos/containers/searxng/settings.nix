72cfe07394c7d41f344d09050346d3cf8ee84010
Tyler Schwemley <tjschwem@gmail.com>
2024-03-13 01:59:02 -0400

    update searxng settings?

--- nixos/containers/searxng/settings.nix
+++ nixos/containers/searxng/settings.nix
@@ -15,66 +15,12 @@
         general:
             debug: false
             instance_name: Search
-        # engines:
-        #   - name: brave
-        #     engine: brave
-        #     shortcut: br
-        #     time_range_support: true
-        #     paging: true
-        #     categories: [general, web]
-        #     brave_category: search
-        #
-        #   - name: brave.images
-        #     engine: brave
-        #     network: brave
-        #     shortcut: brimg
-        #     categories: [images, web]
-        #     brave_category: images
-        #
-        #   - name: brave.videos
-        #     engine: brave
-        #     network: brave
-        #     shortcut: brvid
-        #     categories: [videos, web]
-        #     brave_category: videos
-        #
-        #   - name: brave.news
-        #     engine: brave
-        #     network: brave
-        #     shortcut: brnews
-        #     categories: news
-        #     brave_category: news
-        #
-        #   - name: duckduckgo
-        #     engine: duckduckgo
-        #     shortcut: ddg
-        #
-        #   - name: google
-        #     engine: google
-        #     shortcut: go
-        #
-        #   - name: wallhaven
-        #     engine: wallhaven
-        #     api_key: ${config.sops.placeholder.wallhaven_api_key}
-        #     safesearch_map: "0"
-        #     shortcut: wh
-        #
-        #   - name: wikibooks
-        #     engine: mediawiki
-        #     weight: 0.5
-        #     shortcut: wb
-        #     categories: [general, wikimedia]
-        #     base_url: "https://{language}.wikibooks.org/"
-        #     search_type: text
-        #     about:
-        #       website: https://www.wikibooks.org/
-        #       wikidata_id: Q367
-        #         search:
-        #             autocomplete: ""
-        #             default_lang: en-US
-        #             langauges:
-        #               - en
-        #               - en-US
+        engines:
+          - name: wallhaven
+            api_key: ${config.sops.placeholder.wallhaven_api_key}
+            safesearch_map: "0"
+          - name: wikibooks
+            disabled: false
         server:
             base_url: https://search.schwem.io
             port: 8888
