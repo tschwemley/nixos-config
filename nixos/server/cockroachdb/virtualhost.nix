27466805b8747e07a14f67464c69a70c2e820819
Tyler Schwemley <tjschwem@gmail.com>
2024-03-14 22:43:05 -0400

    modify virtual host (perms may not work as-is)

--- nixos/server/cockroachdb/virtualhost.nix
+++ nixos/server/cockroachdb/virtualhost.nix
@@ -1,8 +1,10 @@
 let
-  ip = "127.0.0.1";
+  ip = "articuno.wyvern-map.ts.net";
   port = "26080";
 in {
   services.nginx.virtualHosts."db.schwem.io" = {
+    sslCertificate = "/root/.config/cockroachdb/client.root.crt";
+    sslCertificateKey = "/root/.config/cockroachdb/client.root.key";
     locations."/articuno" = {
       proxyPass = "http://${ip}:${port}";
       proxyWebsockets = true;
