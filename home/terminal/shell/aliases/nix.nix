e99c65087addc5d6eaacde511d3da9dca39ea3fe
Tyler Schwemley <tjschwem@gmail.com>
2024-03-15 00:55:50 -0400

    also update aliases

--- home/terminal/shell/aliases/nix.nix
+++ home/terminal/shell/aliases/nix.nix
@@ -1,6 +1,7 @@
 {
   "cdnix" = "cd ~/nixos-config";
   "hms" = "nix run home-manager/master -- switch --flake ";
+  "ncg" = "sudo nix-collect-garbage -d && nix-store --gc";
   "nrbs" = "sudo nixos-rebuild switch --flake .#$HOST";
   "nrepl" = "nix repl ~/nixos-config/nixos/system/repl.nix";
 }
