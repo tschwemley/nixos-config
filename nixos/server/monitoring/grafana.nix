c3bca6e36e9f6a928ef81f74bee67db03496b935
Tyler Schwemley <tjschwem@gmail.com>
2024-03-17 18:53:08 -0400

    grafana

--- nixos/server/monitoring/grafana.nix
+++ nixos/server/monitoring/grafana.nix
@@ -2,5 +2,12 @@
 {
   services.grafana = {
     enable = true;
+    settings = {
+      server = {
+        http_addr = "127.0.0.1";
+        http_port = 3000;
+        domain = "https://monitor.schwem.io";
+      };
+    };
   };
 }
