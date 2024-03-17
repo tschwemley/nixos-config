b96730147d9457aafed4c5db9151d6b7283d3c6c
Tyler Schwemley <tjschwem@gmail.com>
2024-03-17 19:34:28 -0400

    fix port ref

--- nixos/server/monitoring/virtualhost.nix
+++ nixos/server/monitoring/virtualhost.nix
@@ -2,7 +2,7 @@
   services.nginx = {
     virtualHosts."monitor.schwem.io" = {
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.grafana.port}";
+        proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.port}";
         proxyWebsockets = true;
 
         # extraConfig = ''
