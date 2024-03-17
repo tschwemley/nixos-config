763cef58afe6f385112f5a4c6c096910636a8836
Tyler Schwemley <tjschwem@gmail.com>
2024-03-16 21:58:53 -0400

    oauth2-proxy more config needed

--- nixos/server/oauth2-proxy/configuration.nix
+++ nixos/server/oauth2-proxy/configuration.nix
@@ -13,5 +13,6 @@
   sops.templates.oauth2_proxy_env.content = ''
     OAUTH2_PROXY_CLIENT_SECRET=${config.sops.placeholder.oauth2_proxy_client_secret}
     OAUTH2_PROXY_COOKIE_SECRET=${config.sops.placeholder.oauth2_proxy_cookie_secret}
+    OAUTH2_PROXY_OIDC_ISSUER_URL=https://auth.schwem.io/realms/schwem-io
   '';
 }
--- nixos/server/oauth2-proxy/default.nix
+++ nixos/server/oauth2-proxy/default.nix
@@ -1,12 +1,11 @@
 {config, ...}: {
-  imports = [./env.nix];
+  imports = [./configuration.nix];
 
   services.oauth2_proxy = {
     enable = true;
     clientID = "oauth2-proxy";
     email.domains = ["*"];
     keyFile = config.sops.templates."oauth2_proxy_env".path;
-    # loginURL = "https://schwem.io/oauth/authorize";
     nginx = {
       proxy = "https://auth.schwem.io/oauth";
       # these are the virtual hosts protected by oauth2-proxy
