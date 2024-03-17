3b1c0861f82fa46d6f8da3b4553f4481895725d0
Tyler Schwemley <tjschwem@gmail.com>
2024-03-16 22:19:22 -0400

    oauth2-proxy

--- nixos/server/oauth2-proxy/default.nix
+++ nixos/server/oauth2-proxy/default.nix
@@ -5,15 +5,13 @@
     enable = true;
     clientID = "oauth2-proxy";
     email.domains = ["*"];
+    httpAddress = "127.0.0.1:4180";
 
     # NOTE: this contains all the config that doesn't have a baked in nix config option
     keyFile = config.sops.templates."oauth2_proxy_env".path;
 
-    nginx = {
-      proxy = "https://auth.schwem.io/oauth";
-      # these are the virtual hosts protected by oauth2-proxy
-      virtualHosts = [];
-    };
+    # these are the virtual hosts protected by oauth2-proxy
+    nginx.virtualHosts = [];
     provider = "keycloak-oidc";
     reverseProxy = true;
   };
