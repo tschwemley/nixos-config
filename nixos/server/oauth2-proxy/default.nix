d43ec2c3e74e01414f0db4987d151a920b252337
Tyler Schwemley <tjschwem@gmail.com>
2024-03-16 21:14:43 -0400

    fix declaration of template

--- nixos/server/oauth2-proxy/default.nix
+++ nixos/server/oauth2-proxy/default.nix
@@ -4,10 +4,10 @@
   services.oauth2_proxy = {
     enable = true;
     clientID = "oauth2-proxy";
-    keyFile = config.sops.templates.oauth2_proxy_env.path;
+    keyFile = config.sops.templates."oauth2_proxy_env".path;
     # loginURL = "https://schwem.io/oauth/authorize";
     nginx = {
-      proxy = "https://schwem.io/oauth";
+      proxy = "https://auth.schwem.io/oauth";
       # these are the virtual hosts protected by oauth2-proxy
       virtualHosts = [];
     };
--- nixos/server/oauth2-proxy/env.nix
+++ nixos/server/oauth2-proxy/env.nix
@@ -10,7 +10,7 @@
     };
   };
 
-  sops.templates.oauth2_proxy_env = ''
+  sops.templates.oauth2_proxy_env.content = ''
     OAUTH2_PROXY_CLIENT_SECRET=${config.sops.placeholder.oauth2_proxy_client_secret}
     OAUTH2_PROXY_COOKIE_SECRET=${config.sops.placeholder.oauth2_proxy_cookie_secret}
   '';
