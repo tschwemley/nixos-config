ff9597e9a35e3da07e2f969b8d879615af23dcb2
Tyler Schwemley <tjschwem@gmail.com>
2024-03-17 18:42:40 -0400

    typo

--- nixos/hardware/audio/bluetooth.nix
+++ nixos/hardware/audio/bluetooth.nix
@@ -4,14 +4,14 @@
   # The mSBC codec provides slightly better sound quality in calls than regular HFP/HSP, while the
   # SBC-XQ provides better sound quality for audio listening. see:
   # https://www.guyrutenberg.com/2021/03/11/replacing-pulseaudio-with-pipewire/
-  environment.etc = {
-    "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
-      bluez_monitor.properties = {
-        ["bluez5.enable-sbc-xq"] = true,
-        ["bluez5.enable-msbc"] = true,
-        ["bluez5.enable-hw-volume"] = true,
-        ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-      }
-    '';
-  };
+  # environment.etc = {
+  #   "wireplumber/bluetooth.lua.d/51-bluez-config.lua".text = ''
+  #     bluez_monitor.properties = {
+  #       ["bluez5.enable-sbc-xq"] = true,
+  #       ["bluez5.enable-msbc"] = true,
+  #       ["bluez5.enable-hw-volume"] = true,
+  #       ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
+  #     }
+  #   '';
+  # };
 }
