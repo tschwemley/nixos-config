7655333edb2fbaa62911942a079c8d47988c14cb
Tyler Schwemley <tjschwem@gmail.com>
2024-03-17 12:38:56 -0400

    vhost

--- nixos/server/cockroachdb/virtualhost.nix
+++ nixos/server/cockroachdb/virtualhost.nix
@@ -3,14 +3,9 @@
   port = "26080";
 in {
   services.nginx = {
-    upstreams.cockroach-web.servers = {
-      "${config.networking.hostName}.wyvern-map.ts.net:26080" = {};
-    };
-
     virtualHosts."db.schwem.io" = {
       locations."/" = {
-        proxyPass = "cockroach-web";
-        # proxyPass = "https://${ip}:${port}";
+        proxyPass = "https://${ip}:${port}";
         # proxyWebsockets = true;
       };
     };
