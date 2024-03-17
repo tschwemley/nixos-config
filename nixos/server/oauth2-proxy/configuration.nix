d7fbaf524316d542e7a9880115801d5884f9a717
Tyler Schwemley <tjschwem@gmail.com>
2024-03-16 22:09:19 -0400

    add code challenge method declaratino

--- nixos/server/oauth2-proxy/configuration.nix
+++ nixos/server/oauth2-proxy/configuration.nix
@@ -11,8 +11,10 @@
   };
 
   sops.templates.oauth2_proxy_env.content = ''
+    OAUTH2_PROXY_CODE_CHALLENGE_METHOD=S256
+    OAUTH2_PROXY_OIDC_ISSUER_URL=https://auth.schwem.io/realms/schwem-io
+
     OAUTH2_PROXY_CLIENT_SECRET=${config.sops.placeholder.oauth2_proxy_client_secret}
     OAUTH2_PROXY_COOKIE_SECRET=${config.sops.placeholder.oauth2_proxy_cookie_secret}
-    OAUTH2_PROXY_OIDC_ISSUER_URL=https://auth.schwem.io/realms/schwem-io
   '';
 }
--- nixos/server/oauth2-proxy/default.nix
+++ nixos/server/oauth2-proxy/default.nix
@@ -5,7 +5,10 @@
     enable = true;
     clientID = "oauth2-proxy";
     email.domains = ["*"];
+
+    # NOTE: this contains all the config that doesn't have a baked in nix config option
     keyFile = config.sops.templates."oauth2_proxy_env".path;
+
     nginx = {
       proxy = "https://auth.schwem.io/oauth";
       # these are the virtual hosts protected by oauth2-proxy
