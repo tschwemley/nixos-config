0215c12b7c86f50beb1cf2484639c06c4fde879a
Tyler Schwemley <tjschwem@gmail.com>
2024-03-20 00:36:33 -0400

    test seaweed net config

--- nixos/network/seaweedfs.nix
+++ nixos/network/seaweedfs.nix
@@ -2,9 +2,16 @@ let
   tailnetInterface = "tailscale0";
 in {
   networking.firewall = {
+    allowedTCPPorts = [9333 9334 9335 9336 9337 19333];
+
     extraCommands = ''
       # Allow traffic on ports 9333-9337 from the tailnet interface
-      iptables -A INPUT -i ${tailnetInterface} -p tcp --match multiport --dports 9333:9337,19333 -j ACCEPT
+      ip46tables -A INPUT -i ${tailnetInterface} -p tcp --match multiport --dports 9333:9337,19333 -j ACCEPT
+    '';
+
+    extraStopCommands = ''
+      # Allow traffic on ports 9333-9337 from the tailnet interface
+      iptables -A INPUT -i ${tailnetInterface} -p tcp --match multiport --dports 9333:9337,19333 -j ACCEPT || true
     '';
   };
 }
