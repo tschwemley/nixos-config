d47d54cbdaf3ce235655f0e2dd8dd94e07a61f1d
Tyler Schwemley <tjschwem@gmail.com>
2024-03-20 12:51:55 -0400

    add public url route

--- nixos/server/nginx/vhosts/seaweedfs.nix
+++ nixos/server/nginx/vhosts/seaweedfs.nix
@@ -1,4 +1,5 @@
-let
+{config, ...}: let
+  hostName = config.networking.hostName;
   tailnetUrl = "wyvern-map.ts.net";
 in {
   services.nginx.virtualHosts."files.schwem.io" = {
@@ -6,17 +7,35 @@ in {
     locations."/" = {
       proxyPass = "http://moltres.${tailnetUrl}:9333";
       proxyWebsockets = true;
+      extraConfig = ''
+        include ${config.sops.templates.nginx_allow_secure.path};
+      '';
+    };
+
+    # volume
+    locations."${hostName}/ui" = {
+      proxyPass = "http://${hostName}.${tailnetUrl}:9334/ui/";
+      proxyWebsockets = true;
+      extraConfig = ''
+        include ${config.sops.templates.nginx_allow_secure.path};
+      '';
     };
 
     # filers
     locations."/lake" = {
       proxyPass = "http://jolteon.${tailnetUrl}:9336";
       proxyWebsockets = true;
+      extraConfig = ''
+        include ${config.sops.templates.nginx_allow_secure.path};
+      '';
     };
 
     locations."/moltres" = {
       proxyPass = "http://moltres.${tailnetUrl}:9336";
       proxyWebsockets = true;
+      extraConfig = ''
+        include ${config.sops.templates.nginx_allow_secure.path};
+      '';
     };
   };
 }
