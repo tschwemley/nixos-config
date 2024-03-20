007be516ce2a34b77ec58edaa0e7c0b610a54280
Tyler Schwemley <tjschwem@gmail.com>
2024-03-20 00:24:59 -0400

    update swfs networking

--- nixos/network/seaweedfs.nix
+++ nixos/network/seaweedfs.nix
@@ -4,7 +4,7 @@ in {
   networking.firewall = {
     extraCommands = ''
       # Allow traffic on ports 9333-9337 from the tailnet interface
-      iptables -A INPUT -i ${tailnetInterface} -p tcp --match multiport --dports 9333:9337 -j ACCEPT
+      iptables -A INPUT -i ${tailnetInterface} -p tcp --match multiport --dports 9333:9337,19333 -j ACCEPT
     '';
   };
 }
