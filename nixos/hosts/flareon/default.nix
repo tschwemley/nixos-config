eba65d4ab53645dcf0d5dca6525548586b301a26
Tyler Schwemley <tjschwem@gmail.com>
2024-03-21 18:58:46 -0400

    fix

--- nixos/hosts/flareon/default.nix
+++ nixos/hosts/flareon/default.nix
@@ -3,7 +3,7 @@
   disk = (import ../../hardware/disks).proxmox;
   profile = import ../../profiles/proxmox.nix;
   server = [
-    "${inputs.private.outPath}/virtualhosts/stash.nix"
+    "${inputs.nix-private.outPath}/virtualhosts/stash.nix"
     ../../services/seaweedfs/volume.nix
   ];
 in {
