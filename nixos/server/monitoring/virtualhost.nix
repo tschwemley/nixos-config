6990f7f756f6889d1fff5e13aa0fff659f5217ed
Tyler Schwemley <tjschwem@gmail.com>
2024-03-23 19:45:31 -0400

    wip sso

--- nixos/server/monitoring/virtualhost.nix
+++ nixos/server/monitoring/virtualhost.nix
@@ -11,25 +11,18 @@ in {
           auth_request https://auth.schwem.io/oauth2/auth?allowed_groups=role:admin;
           error_page 401 =403 https://auth.schwem.io/oauth2/start?rd=https://monitor.schwem.io&allowed_groups=role:admin;
 
+          auth_request_set $name_upstream_1 $upstream_cookie__oauth2_proxy_1;
           auth_request_set $user   $upstream_http_x_auth_request_user;
-          auth_request_set $email  $upstream_http_x_auth_request_email;
           proxy_set_header X-User  $user;
-          proxy_set_header X-Email $email;
+          proxy_set_header Authorization $http_authorization;
+
+          access_by_lua_block {
+            if ngx.var.name_upstream_1 ~= "" then
+              ngx.header["Set-Cookie"] = "_oauth2_proxy_1=" .. ngx.var.name_upstream_1 .. ngx.var.auth_cookie:match("(; .*)")
+            end
+          }
 
           include ${config.sops.templates.nginx_allow_secure.path};
-
-          # auth_request_set $name_upstream_1 $upstream_cookie__oauth2_proxy_1;
-          #
-          # access_by_lua_block {
-          #   if ngx.var.name_upstream_1 ~= "" then
-          #     ngx.header["Set-Cookie"] = "_oauth2_proxy_1=" .. ngx.var.name_upstream_1 .. ngx.var.auth_cookie:match("(; .*)")
-          #   end
-          # }
-          #
-          # proxy_set_header Authorization $http_authorization;
-          # proxy_set_header X-User $http_x_user;
-          #
-          # proxy_pass http://libreddit;
         '';
 
         # extraConfig = ''
