0f2bf8deba9febfd9938dc374b0faca0998ba65d
Tyler Schwemley <tjschwem@gmail.com>
2024-03-23 16:00:51 -0400

    fix addr

--- nixos/server/oauth2-proxy/default.nix
+++ nixos/server/oauth2-proxy/default.nix
@@ -8,7 +8,7 @@
     enable = true;
     clientID = "oauth2-proxy";
     email.domains = ["*"];
-    httpAddress = "127.0.0.1:4180";
+    httpAddress = "http://127.0.0.1:4180";
 
     # NOTE: this contains all the config that doesn't have a baked in nix config option
     keyFile = config.sops.templates."oauth2_proxy_env".path;
