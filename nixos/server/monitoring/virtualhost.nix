fa7a54e7dc7369e10c0206c60c23c848cc1da701
Tyler Schwemley <tjschwem@gmail.com>
2024-03-23 19:21:04 -0400

    missing ;

--- nixos/server/monitoring/virtualhost.nix
+++ nixos/server/monitoring/virtualhost.nix
@@ -9,7 +9,7 @@ in {
         proxyWebsockets = true;
         extraConfig = ''
           auth_request https://auth.schwem.io/oauth2/auth?allowed_groups=role:admin;
-          error_page 401 =403 https://auth.schwem.io/oauth2/start?rd=https://monitor.schwem.io&allowed_groups=role:admin
+          error_page 401 =403 https://auth.schwem.io/oauth2/start?rd=https://monitor.schwem.io&allowed_groups=role:admin;
 
           auth_request_set $user   $upstream_http_x_auth_request_user;
           auth_request_set $email  $upstream_http_x_auth_request_email;
