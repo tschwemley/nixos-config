dbe71410e38148a11e93aa572656c2933bde8167
Tyler Schwemley <tjschwem@gmail.com>
2024-03-23 01:57:50 -0400

    wip test auth

--- nixos/server/monitoring/virtualhost.nix
+++ nixos/server/monitoring/virtualhost.nix
@@ -5,6 +5,7 @@
         proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
         proxyWebsockets = true;
         extraConfig = ''
+          auth_request https://auth.schwem.io/oauth2/start?rd=https%3A%2F%2F$host$escaped_request_uri&allowed_groups=role:admin;
           include ${config.sops.templates.nginx_allow_secure.path};
         '';
 
