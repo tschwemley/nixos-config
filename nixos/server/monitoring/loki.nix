f4d0f43c1f7832c15c8d44cfad823738401f3689
Tyler Schwemley <tjschwem@gmail.com>
2024-03-23 14:53:29 -0400

    wip auth vhost

--- nixos/server/monitoring/loki.nix
+++
@@ -1,5 +0,0 @@
-{
-  services.loki = {
-    enable = true;
-  };
-}
--- nixos/server/monitoring/virtualhost.nix
+++ nixos/server/monitoring/virtualhost.nix
@@ -1,11 +1,17 @@
-{config, ...}: {
+{config, ...}: let
+  ip = "127.0.0.1";
+  port = toString config.services.grafana.settings.server.http_port;
+in {
   services.nginx = {
     virtualHosts."monitor.schwem.io" = {
       locations."/" = {
-        proxyPass = "http://127.0.0.1:${toString config.services.grafana.settings.server.http_port}";
+        proxyPass = "http://${ip}:${port}";
         proxyWebsockets = true;
         extraConfig = ''
           auth_request https://auth.schwem.io/oauth2/start?rd=https%3A%2F%2Fmonitor.schwem.io&allowed_groups=role:admin;
+          auth_request_set $cookie $upstream_http_set_cookie;
+          add_header Set-Cookie $cookie;
+
           include ${config.sops.templates.nginx_allow_secure.path};
         '';
 
