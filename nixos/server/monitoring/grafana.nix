19cfb9e925069acd9fea490a27b3135370e6e525
Tyler Schwemley <tjschwem@gmail.com>
2024-03-25 21:44:53 -0400

    wip grafana keycloak client authentication

--- nixos/server/monitoring/grafana.nix
+++ nixos/server/monitoring/grafana.nix
@@ -1,12 +1,34 @@
 # TODO: setup w/ cockroachdb. By default it's using sqlite
-{
+{config, ...}: {
   services.grafana = {
     enable = true;
     settings = {
+      "auth.generic_oauth" = {
+        enabled = true;
+        name = "keycloak";
+
+        api_url = "https://auth.schwem.io/realms/schwem-io/protocol/openid-connect/userinfo";
+        auth_url = "https://auth.schwem.io/realms/schwem-io/protocol/openid-connect/auth";
+        client_id = "grafana";
+        client_secret = "$__file{${config.sops.secrets.grafana_oauth_client_secret.path}}";
+        email_attribute_path = "email";
+        login_attribute_path = "username";
+        name_attribute_path = "full_name";
+        role_attribute_path = "contains(roles[*], 'admin') && 'Admin'";
+        scopes = "openid email profile offline_access roles";
+        signout_redirect_url = "https://auth.schwem.io/auth/realms/schwem-io/protocol/openid-connect/logout?post_logout_redirect_uri=https%3A%2F%2Fmonitor.schwem.io%2Flogin";
+        token_url = "https://auth.schwem.io/realms/schwem-io/protocol/openid-connect/token";
+        use_refresh_token = true;
+      };
+
       server = {
         http_addr = "127.0.0.1";
         http_port = 3000;
       };
     };
+  };
+
+  sops.secrets.grafana_oauth_client_secret = {
+    sopsFile = ./secrets.yaml;
   };
 }
--- nixos/server/monitoring/virtualhost.nix
+++ nixos/server/monitoring/virtualhost.nix
@@ -8,18 +8,18 @@ in {
         "/" = {
           proxyPass = "http://${ip}:${port}";
           proxyWebsockets = true;
-          extraConfig = ''
-            auth_request /auth;
-            error_page 401 403 =307 https://auth.schwem.io/oauth2/start?rd=https%3A%2F%2Fmonitor.schwem.io&allowed_groups=role:admin;
-
-            # include ${config.sops.templates.nginx_allow_secure.path};
-          '';
+          # extraConfig = ''
+          #   auth_request /auth;
+          #   error_page 401 403 =307 https://auth.schwem.io/oauth2/start?rd=https%3A%2F%2Fmonitor.schwem.io&allowed_groups=role:admin;
+          #
+          #   # include ${config.sops.templates.nginx_allow_secure.path};
+          # '';
         };
 
-        "/auth" = {
-          proxyPass = "http://127.0.0.1:4180/oauth2/auth?allowed_groups=role:admin";
-          extraConfig = "internal;";
-        };
+        # "/auth" = {
+        #   proxyPass = "http://127.0.0.1:4180/oauth2/auth?allowed_groups=role:admin";
+        #   extraConfig = "internal;";
+        # };
 
         # "/sign-in" = {
         #   proxyPass = "http://127.0.0.1:4180/oauth2/start?rd=https%3A%2F%2Fmonitor.schwem.io&allowed_groups=role:admin";
