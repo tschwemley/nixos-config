7d9c7ed0ded950f31b603682eac9de40a1a313e6
Tyler Schwemley <tjschwem@gmail.com>
2024-03-29 16:35:31 -0400

    eevee update

--- nixos/hosts/eevee/default.nix
+++ nixos/hosts/eevee/default.nix
@@ -3,8 +3,8 @@
   disk = (import ../../hardware/disks).buyvmWithStorage;
   profile = import ../../profiles/buyvm.nix;
   server = [
-    "${inputs.nix-private.outPath}/containers/arr"
-    "${inputs.nix-private.outPath}/containers/p2p"
+    # "${inputs.nix-private.outPath}/containers/arr"
+    # "${inputs.nix-private.outPath}/containers/p2p"
     ../../services/seaweedfs/filer.nix
     ../../services/seaweedfs/volume.nix
   ];
