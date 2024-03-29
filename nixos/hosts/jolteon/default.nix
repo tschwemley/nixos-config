659a4f34b476ba4fe25e4d1fb6560646bf010a6f
Tyler Schwemley <tjschwem@gmail.com>
2024-03-29 14:32:41 -0400

    jolteon testing

--- nixos/hosts/jolteon/default.nix
+++ nixos/hosts/jolteon/default.nix
@@ -3,7 +3,9 @@
   disk = (import ../../hardware/disks).proxmox;
   profile = import ../../profiles/proxmox.nix;
   server = [
+    "${inputs.nix-private.outPath}/containers/arr"
     "${inputs.nix-private.outPath}/containers/invidious"
+    "${inputs.nix-private.outPath}/containers/p2p"
     ../../containers/jellyfin
     ../../services/seaweedfs/filer.nix
     ../../services/seaweedfs/volume.nix
