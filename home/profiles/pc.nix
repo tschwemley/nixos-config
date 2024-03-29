970f904b55dba98f99cb90cb6e9f0f0d475adb48
Tyler Schwemley <tjschwem@gmail.com>
2024-03-29 02:16:27 -0400

    remove shit I'm not using

--- home/profiles/pc.nix
+++ home/profiles/pc.nix
@@ -12,20 +12,16 @@ in {
     ../programs/bambu-studio.nix
     ../programs/brave.nix
     ../programs/cad.nix
-    ../programs/cura.nix
     ../programs/firefox.nix
     ../programs/glow.nix
-    ../programs/godot.nix
     ../programs/ollama.nix
     ../programs/slack.nix
     ../programs/spotify.nix
     # TODO: wait for upstream fix or write overlay
-    ../programs/sonic-pi.nix
     ../programs/taskwarrior.nix
     ../programs/vlc.nix
     ../programs/wayland
     ../programs/wcalc.nix
-    ../programs/wiki-tui.nix
     ../programs/turbovnc.nix
     ../programs/zoom.nix
     ../services/kdeconnect.nix
