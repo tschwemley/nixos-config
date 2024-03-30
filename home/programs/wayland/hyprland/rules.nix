7ee99ee805a0a53682133dc9370f498da74758dc
Tyler Schwemley <tjschwem@gmail.com>
2024-03-30 14:01:48 -0400

    update hyprland rules

--- home/programs/wayland/hyprland/rules.nix
+++ home/programs/wayland/hyprland/rules.nix
@@ -1,12 +1,15 @@
-{
+let
+  rofiRules = ["stayfocused, title:^(rofi).*$"];
+  steamRules = [
+    "float, title:^(Steam)$"
+    "float, title:^(Friends List)$"
+    # the next two rules fix menu focus issues
+    "stayfocused, title:^()$,class:^(steam)$"
+    "minsize 1 1, title:^()$,class:^(steam)$"
+  ];
+in {
   wayland.windowManager.hyprland.settings = {
     layerrule = [];
-    windowrulev2 = [
-      "float, title:^(Steam)$"
-      "float, title:^(Friends List)$"
-      # the next two rules fix menu focus issues
-      "stayfocused, title:^()$,class:^(steam)$"
-      "minsize 1 1, title:^()$,class:^(steam)$"
-    ];
+    windowrulev2 = rofiRules ++ steamRules;
   };
 }
