11d6a93558c6b9b08b4f4a59183e1d02859a502d
Tyler Schwemley <tjschwem@gmail.com>
2024-03-29 22:01:17 -0400

    wip

---
+++ nixos/server/sourcehut/default.nix
@@ -0,0 +1,12 @@
+{
+  services.sourcehut = {
+    enable = true;
+    git.enable = true;
+    hub.enable = true;
+    man.enable = true;
+    nginx.enable = true;
+    pages.enable = true;
+    paste.enable = true;
+    todo.enable = true;
+  };
+}
---
+++ packages/scripts/find-desktop-entries.sh
@@ -0,0 +1,4 @@
+for p in ${XDG_DATA_DIRS//:/ }; do 
+	[[ ! -d $p/applications ]] && continue
+	find $p/applications -name '*.desktop' 
+done
