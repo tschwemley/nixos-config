348008bbba6d1b7bd81e3c55cbc80ac345c107ae
Tyler Schwemley <tjschwem@gmail.com>
2024-04-02 02:02:28 -0400

    eevee net update

--- nixos/hosts/eevee/default.nix
+++ nixos/hosts/eevee/default.nix
@@ -41,8 +41,8 @@ in {
   # read: https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion when ready to update
   system.stateVersion = "23.05";
   tailscaleUpFlags = [
-    # "--accept-routes"
-    "--advertise-routes=10.10.0.0/24"
+    "--accept-routes"
+    "--advertise-routes=10.10.0.0/16"
     "--exit-node=de-fra-wg-304.mullvad.ts.net"
     # "--exit-node-allow-lan-access=true"
   ];
