2ee33c1ada1b3be58a65d71567429c93e3e799a1
Tyler Schwemley <tjschwem@gmail.com>
2024-04-02 23:27:38 -0400

    change seaweedfs for nfttables use

--- nixos/network/seaweedfs.nix
+++ nixos/network/seaweedfs.nix
@@ -3,15 +3,15 @@ let
 in {
   networking.firewall = {
     allowedTCPPorts = [9333 9334 9335 9336 9337 19333];
-
-    extraCommands = ''
-      # Allow traffic on ports 9333-9337 from the tailnet interface
-      ip46tables -A INPUT -i ${tailnetInterface} -p tcp --match multiport --dports 9333:9337,19333 -j ACCEPT
-    '';
-
-    extraStopCommands = ''
-      # Allow traffic on ports 9333-9337 from the tailnet interface
-      ip46tables -A INPUT -i ${tailnetInterface} -p tcp --match multiport --dports 9333:9337,19333 -j ACCEPT || true
-    '';
+    #
+    # extraCommands = ''
+    #   # Allow traffic on ports 9333-9337 from the tailnet interface
+    #   ip46tables -A INPUT -i ${tailnetInterface} -p tcp --match multiport --dports 9333:9337,19333 -j ACCEPT
+    # '';
+    #
+    # extraStopCommands = ''
+    #   # Allow traffic on ports 9333-9337 from the tailnet interface
+    #   ip46tables -A INPUT -i ${tailnetInterface} -p tcp --match multiport --dports 9333:9337,19333 -j ACCEPT || true
+    # '';
   };
 }
