4e30ce765d0f1d52dcb581e313a43d14e8732c89
Tyler Schwemley <tjschwem@gmail.com>
2024-04-06 23:42:09 -0400

    fix mullvad dns settings

--- nixos/network/default.nix
+++ nixos/network/default.nix
@@ -2,8 +2,12 @@
   networking.nftables.enable = true;
 
   # always use systemd-resolved
-  services.resolved.enable = true;
-  services.resolved.fallbackDns = ["194.242.2.2"];
+  services.resolved = {
+    enable = true;
+    dnsovertls = "opportunistic";
+    domains = ["~."];
+    fallbackDns = ["194.242.2.2"];
+  };
   services.openssh = {
     enable = true;
     settings = {
