098ed5ed9c01776d342dd72de38e68606d311d5f
Tyler Schwemley <tjschwem@gmail.com>
2024-04-07 22:02:02 -0400

    comment

--- nixos/hosts/articuno/default.nix
+++ nixos/hosts/articuno/default.nix
@@ -30,5 +30,5 @@ in {
   # read: https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion when ready to update
   system.stateVersion = "23.05";
 
-  tailscaleUpFlags = [];
+  # tailscaleUpFlags = [];
 }
