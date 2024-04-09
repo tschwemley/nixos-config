d36b7f6c25feae6edb3056dbe2e815dd02ec344f
Tyler Schwemley <tjschwem@gmail.com>
2024-04-08 21:45:43 -0400

    clean up jellyfin vhost

--- nixos/containers/jellyfin/virtualhost.nix
+++ nixos/containers/jellyfin/virtualhost.nix
@@ -5,10 +5,7 @@
         proxyPass = "http://10.10.3.2:8096";
         extraConfig = ''
           auth_request /auth;
-          # error_page 401 403 =307 https://auth.schwem.io/oauth2/start?rd=https%3A%2F%2Fjellyfin.schwem.io&allowed_groups=role:admin;
           error_page 401 =403 /sign-in;
-
-          # include ${config.sops.templates.nginx_allow_secure.path};
         '';
       };
 
