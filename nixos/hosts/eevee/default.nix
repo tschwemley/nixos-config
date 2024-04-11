8c3ba052f8e5c15e6c73a78dc5d41b1029fc611c
Tyler Schwemley <tjschwem@gmail.com>
2024-04-10 21:56:54 -0400

    test eevee config

--- nixos/hosts/eevee/default.nix
+++ nixos/hosts/eevee/default.nix
@@ -18,6 +18,11 @@ in {
     ]
     ++ server;
 
+  boot.kernel.sysctl = {
+    "net.ipv4.ip_forward" = 1;
+    "net.ipv6.all.forwarding" = 1;
+  };
+
   networking.hostName = "eevee";
 
   # TODO: change this on all servers
