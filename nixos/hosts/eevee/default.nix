cf3da5316da9f1d0b08dda0016c8faa3e8d43f67
Tyler Schwemley <tjschwem@gmail.com>
2024-04-10 23:40:16 -0400

    eevee modification

--- nixos/hosts/eevee/default.nix
+++ nixos/hosts/eevee/default.nix
@@ -18,21 +18,7 @@ in {
     ]
     ++ server;
 
-  boot.kernel.sysctl = {
-    "net.ipv4.ip_forward" = 1;
-    "net.ipv6.all.forwarding" = 1;
-
-"net.ipv6.conf.all.accept_ra" = 0;
-  "net.ipv6.conf.all.autoconf" = 0;
-  "net.ipv6.conf.all.use_tempaddr" = 0;
-
-    # On WAN, allow IPv6 autoconfiguration and tempory address use.
-    "net.ipv6.conf.ens3.accept_ra" = 2;
-    "net.ipv6.conf.ens3.autoconf" = 1;
-  };
-
   networking.hostName = "eevee";
-  networking.interfaces.ve-sabnzbd.mtu = 1200;
 
   # TODO: change this on all servers
   services.getty.autologinUser = "root";
@@ -45,10 +31,7 @@ in {
   # read: https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion when ready to update
   system.stateVersion = "23.05";
   tailscaleUpFlags = [
-    # "--accept-routes"
-    "--advertise-routes=10.250.0.0/24"
     "--exit-node=100.76.46.81"
     "--exit-node-allow-lan-access=true"
-    # "--snat-subnet-routes=false"
   ];
 }
