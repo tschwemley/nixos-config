1eb8d2ff52f99317139694dd686202146121a40d
Tyler Schwemley <tjschwem@gmail.com>
2024-04-19 23:50:47 -0400

    remove excalidraw from repo

--- nixos/containers/excalidraw/default.nix
+++
@@ -1,9 +0,0 @@
-{
-  imports = [./virtualhost.nix];
-
-  virtualisation.oci-containers.containers.excalidraw = {
-    autoStart = true;
-    image = "excalidraw/excalidraw";
-    ports = ["127.0.0.1:13000:3000"];
-  };
-}
--- nixos/containers/excalidraw/virtualhost.nix
+++
@@ -1,8 +0,0 @@
-let
-  ip = "127.0.0.1";
-  port = "13000";
-in {
-  services.nginx.virtualHosts."draw.schwem.io" = {
-    locations."/" = {proxyPass = "http://${ip}:${port}";};
-  };
-}
