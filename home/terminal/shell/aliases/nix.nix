e3302048b69d4b48bdca9346c3a13b8cfc892f1c
Tyler Schwemley <tjschwem@gmail.com>
2024-04-23 23:17:25 -0400

    add nix flake update alias

--- home/terminal/shell/aliases/nix.nix
+++ home/terminal/shell/aliases/nix.nix
@@ -4,6 +4,7 @@ in {
   "cdnix" = "cd ~/nixos-config";
   "hms" = "nix run home-manager/master -- switch --flake ";
   "ncg" = collectGarbage;
+  "nfu" = "nix flake update && git add flake.lock && git commit -m 'flake update' && git push origin main";
   "ngc" = collectGarbage;
   "nrbs" = "sudo nixos-rebuild switch --flake .#$HOST";
   "nrepl" = "nix repl ~/nixos-config/nixos/system/repl.nix";
