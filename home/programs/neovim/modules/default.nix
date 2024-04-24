7395b838568d9766c9cc92f530a174a4f8140f01
Tyler Schwemley <tjschwem@gmail.com>
2024-04-23 23:19:06 -0400

    cleanup nvim config now that structure is taking shape

--- home/programs/neovim/modules/default.nix
+++ home/programs/neovim/modules/default.nix
@@ -13,28 +13,5 @@
     ./treesitter
     ./schwem
     ./utils
-
-    # ./lsp
-    # ./completion.nix
-    # ./db.nix
-    # ./debugging.nix
-    # ./editing.nix
-    # ./git.nix
-    # ./lsp.nix
-    # ./navigation.nix
-    # ./notes.nix
-    # ./utils.nix
   ];
-
-  # plugins that don't (yet) fit into a module are defined here
-  # programs.neovim.plugins = with pkgs.vimPlugins; [
-  #   better-escape-nvim
-  #   toggleterm-nvim
-  #   which-key-nvim
-  # ];
-
-  # TODO: this is a wip towards simplifying the config
-  # programs.neovim.plugins = [
-  #   (import ../plugins/lsp-plugin.nix {inherit (pkgs) vimPlugins;})
-  # ];
 }
