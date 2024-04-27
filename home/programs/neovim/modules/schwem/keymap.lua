2962f5c71f7ab2bad9307d94a675760ec489ac4a
Tyler Schwemley <tjschwem@gmail.com>
2024-04-27 16:27:29 -0400

    flake update

--- home/programs/neovim/modules/schwem/keymap.lua
+++ home/programs/neovim/modules/schwem/keymap.lua
@@ -12,3 +12,5 @@ vim.keymap.set('n', '<leader>bn', ':bn<cr>', { noremap = true })
 vim.keymap.set('n', '<leader>q', ':q<cr>', { noremap = true })
 vim.keymap.set('n', '<leader>w', ':w<cr>', { noremap = true })
 vim.keymap.set('n', '<leader>wq', ':wq<cr>', { noremap = true })
+
+vim.keymap.set('t', '<esc>', '<C-\\><C-n>', { noremap = true })
--- home/programs/neovim/modules/utils/default.nix
+++ home/programs/neovim/modules/utils/default.nix
@@ -1,4 +1,13 @@
 {pkgs, ...}: let
+  harpoon = pkgs.vimUtils.buildVimPlugin {
+    name = "neogit-nightly";
+    src = pkgs.fetchFromGitHub {
+      owner = "ThePrimeagen";
+      repo = "harpoon";
+      rev = "harpoon2";
+      hash = "sha256-PwSckn4Mo0uaiUN/aiH7ic7W3QVUQ52j1BdVA1CWVbs=";
+    };
+  };
   # NOTE: this is only necessary until official 0.10 support hits neogit master
   neogit-nightly = pkgs.vimUtils.buildVimPlugin {
     name = "neogit-nightly";
@@ -11,11 +20,13 @@
   };
 in {
   programs.neovim.plugins = with pkgs.vimPlugins; [
+    harpoon
     mini-nvim
     neogit-nightly
     nnn-vim
   ];
 
+  xdg.configFile."nvim/after/plugin/harpoon.lua".source = ./harpoon.lua;
   xdg.configFile."nvim/after/plugin/mini-clue.lua".source = ./mini-clue.lua;
   xdg.configFile."nvim/after/plugin/neogit.lua".source = ./neogit.lua;
   xdg.configFile."nvim/after/plugin/nnn.lua".source = ./nnn.lua;
