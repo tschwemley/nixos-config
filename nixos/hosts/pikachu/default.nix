ae6030957132bd11308478fd85e090bc9fed221a
Tyler Schwemley <tjschwem@gmail.com>
2024-04-27 21:30:53 -0400

    add pikachu host init

---
+++ nixos/hosts/pikachu/default.nix
@@ -0,0 +1,71 @@
+{
+  self,
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  boot = (import ../../system/boot.nix).systemd;
+  disk = (import ../../hardware/disks).charizard;
+  hardware = {
+    imports = [
+      inputs.nixos-hardware.nixosModules.common-cpu-intel-cpu-only
+      inputs.nixos-hardware.nixosModules.common-pc
+      inputs.nixos-hardware.nixosModules.common-pc-ssd
+      ../../hardware/amd.nix
+      ../../hardware/audio
+    ];
+  };
+  networking = {
+    imports = [
+      ../../network/containers.nix
+      ../../network/tailscale.nix
+    ];
+  };
+  user = (import ../../system/users.nix {inherit self config pkgs;}).schwem;
+in {
+  imports = [
+    boot
+    disk
+    hardware
+    networking
+    user
+    ./secrets.nix
+    ../../profiles/pc.nix
+  ];
+
+  boot = {
+    initrd = {
+      availableKernelModules = ["xhci_pci" "ahci" "nvme" "usbhid" "uas" "sd_mod"];
+      kernelModules = ["kvm-intel"];
+    };
+    kernelPackages = pkgs.linuxPackages_latest;
+    supportedFilesystems = ["btrfs"];
+  };
+
+  networking = {
+    hostName = "pikachu";
+    networkmanager.enable = true;
+    useDHCP = lib.mkDefault true;
+    wireless.enable = true;
+  };
+
+  services.getty.autologinUser = "schwem";
+
+  system = {
+    autoUpgrade.enable = true;
+
+    # read: https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion when ready to update
+    stateVersion = "24.05";
+  };
+
+  tailscaleUpFlags = [
+    "--exit-node=100.84.59.97"
+    "--exit-node-allow-lan-access=true"
+    "--shields-up"
+  ];
+  time.timeZone = "America/Detroit";
+
+  users.mutableUsers = true; # allow mutable users on non-servers
+}
