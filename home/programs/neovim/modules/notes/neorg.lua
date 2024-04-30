0bc812f39e6f0b6478e00d266469bbf282baf072
Tyler Schwemley <tjschwem@gmail.com>
2024-04-30 16:30:33 -0400

    update neovim notes

--- home/programs/neovim/modules/notes/neorg.lua
+++ home/programs/neovim/modules/notes/neorg.lua
@@ -1,15 +1,21 @@
 require("neorg").setup({
-   load = {
-      ["core.defaults"] = {
-         config = {
-            disable = {
-               -- module list goes here
-               "core.autocommands",
-               "core.itero",
-            },
-         },
-      },
-      ["core.concealer"] = {},
-      ["core.ui.calendar"] = {},
-   },
+	load = {
+		["core.defaults"] = {
+			config = {
+				disable = {
+					-- module list goes here
+					"core.autocommands",
+					"core.itero",
+				},
+			},
+		},
+		["core.completion"] = {
+			config = {
+				engine = "nvim-cmp",
+			},
+		},
+		["core.concealer"] = {},
+		["core.ui"] = {},
+		["core.ui.calendar"] = {},
+	},
 })
