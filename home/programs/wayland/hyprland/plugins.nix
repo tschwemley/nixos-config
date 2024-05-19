97f2014c1e53771c9d4e9b4283e424422a014157
Tyler Schwemley <tjschwem@gmail.com>
2024-05-19 14:32:29 -0400

    flake update

--- home/programs/wayland/hyprland/plugins.nix
+++ home/programs/wayland/hyprland/plugins.nix
@@ -1,22 +1,44 @@
-{pkgs, ...}: let
-  hyprland-easymotion = with pkgs;
-    hyprlandPlugins.mkHyprlandPlugin hyprland {
-      pluginName = "hyprland-easymotion";
-      version = "main";
+{pkgs, ...}: {
+  wayland.windowManager.hyprland.settings = {
+    extraConfig = ''
+        plugin {
+          easymotion {
+            #font size of the text
+            textsize=20
 
-      src = fetchFromGitHub {
-        owner = "zakk4223";
-        repo = "hyprland-easymotion";
-        rev = "main";
-        hash = "sha256-lyv0qM6R0BNBu1B4jMw+mn6K8NYTbr/tmjscfL7Uf78=";
-      };
+            #color of the text, takes standard hyprland color format
+            # textcolor=rgba(000000ff)
 
-      nativeBuildInputs = [cmake];
+            #background color of the label box. alpha is respected
+            bgcolor=rgba(000000ff)
 
-      dontStrip = true;
-    };
-in {
-  wayland.windowManager.hyprland.plugins = [
-    hyprland-easymotion
-  ];
+            #font to use for the label. This is passed directly to the pango font description
+            textfont=Sans
+
+            #padding around th<D-z>e text (inside the label box) size in pixels, adjusted for
+            #monitor scaling. This is the same format as hyprland's gapsin/gapsout workspace layout rule
+            #example: textpadding=2 5 5 2 (spaces not commas)
+            textpadding=0
+
+            #size of the border around the label box. A border size of zero disables border rendering.
+            bordersize=0
+
+            #color of the border. takes the same format as hyprland's border (so it can be a gradient)
+            # bordercolor=rgba(ffffffff)
+
+            #rounded corners? Same as hyprland's 'decoration:rounding' config
+            rounding=0
+
+            #which keys to use for labeling windows
+            motionkeys=abcdefghijklmnopqrstuvwxyz1234567890
+          }
+      }
+    '';
+
+    plugins = [
+      pkgs.hypreasymotion
+      # "/home/schwem/libhyprscroller.so"
+    ];
+
+  };
 }
