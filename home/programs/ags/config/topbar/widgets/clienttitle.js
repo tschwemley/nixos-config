df2c3a4be729e229720172ee55ac8f489a3a5b51
Tyler Schwemley <tjschwem@gmail.com>
2024-06-24 02:42:50 -0400

    remove ags and start clean when I circle back

--- home/programs/ags/config/topbar/widgets/clienttitle.js
+++
@@ -1,9 +0,0 @@
-import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js';
-import Widget from 'resource:///com/github/Aylur/ags/widget.js';
-
-const ClientTitle = () => Widget.Label({
-	class_name: 'client-title',
-	label: Hyprland.active.client.bind('title'),
-});
-
-export default ClientTitle;
