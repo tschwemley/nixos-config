4b93e1d8a442e8755a6e32efae06906d4c29c1b2
Tyler Schwemley <tjschwem@gmail.com>
2024-07-24 23:06:34 -0400

    update flareon

--- nixos/hosts/flareon/default.nix
+++ nixos/hosts/flareon/default.nix
@@ -1,6 +1,7 @@
 {inputs, lib, ...}: let
   boot = (import ../../system/boot.nix).systemd;
-  disk = (import ../../hardware/disks).proxmox;
+  # disk = (import ../../hardware/disks).proxmox;
+  disk = import ../../hardware/disks/ephemeral-root.nix { diskName = "/dev/sdb"; };
   profile = import ../../profiles/proxmox.nix;
   server = [
     "${inputs.nix-private.outPath}/containers/stash"
