fde3360293d7e3dbdd69bf2de2fa02668f6d594a
Tyler Schwemley <tjschwem@gmail.com>
2024-08-08 16:13:52 -0400

    add new nvim cmd

--- home/programs/neovim/modules/diagnostics/diagnostics.lua
+++ home/programs/neovim/modules/diagnostics/diagnostics.lua
@@ -16,3 +16,14 @@ vim.keymap.set('n', '<leader>ta', '<cmd>Trouble<cr>', { noremap = true })
 vim.keymap.set('n', '<leader>td', '<cmd>TodoTrouble<cr>', { noremap = true })
 vim.keymap.set('n', '<leader>tl', '<cmd>Trouble diagnostics toggle<cr>', { noremap = true })
 vim.keymap.set('n', '<leader>tq', '<cmd>Trouble qflist toggle<cr>', { noremap = true })
+
+-- command to automatically watch log
+vim.api.nvim_create_user_command('WatchTail', 'set autoread | au CursorHold * checktime | call feedkeys("G")', {})
+-- vim.api.nvim_create_user_command('WatchTail', function()
+--     vim.opt.autoread = true
+--     vim.api.nvim_create_autocmd("CursorHold", {
+--         pattern = "*",
+--         command = "checktime"
+--     })
+--     vim.api.nvim_feedkeys("G", 'n', false)
+-- end, {})
