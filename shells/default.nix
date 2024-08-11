569798c603160549e62a99ade1115c2548ef9992
Tyler Schwemley <tjschwem@gmail.com>
2024-08-11 15:07:13 -0400

    update dev shell

--- shells/default.nix
+++ shells/default.nix
@@ -8,15 +8,14 @@
     devShells.default = pkgs.mkShell {
       buildInputs = with pkgs; [
         age
-        manix
+        disko
+        #manix
         # BUG: issue with https://github.com/jelmer/dulwich/archive/refs/tags/0.22.1.tar.gz misisng
         # nix-prefetch-scripts
-        pkgs.sops
+        sops
         self'.packages.build-host
         self'.packages.build-all-hosts
-        sqlite # this is for if the nix-store fucks me for the Nth time
         ssh-to-age
-        wireguard-tools
       ];
     };
   };
