c8aa1c3cf6636e7fa9d00f2d83eecd118ae6039f
Tyler Schwemley <tjschwem@gmail.com>
2024-08-11 21:43:50 -0400

    fix file args

--- nixos/hardware/disks/proxmox.nix
+++ nixos/hardware/disks/proxmox.nix
@@ -1,12 +1,7 @@
 let
-  # these names are consistent across the proxmox nodes
-  rootName = "/dev/sda";  
-  storageName = "/dev/sdb";  
-  # rootName = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0";  
-  # storageName = "/dev/disk-by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi1";  
-
-  root = import ./ephemeral-root.nix {diskName = rootName;};
-  storage = import ./block-storage.nix {diskName = storageName;};
+  # these disk names are consistent across the proxmox nodes
+  root = import ./ephemeral-root.nix "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0";
+  storage = import ./block-storage.nix "/dev/disk-by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi1";
 in {
   imports = [
     root
