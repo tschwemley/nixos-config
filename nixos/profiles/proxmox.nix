6e712dd3fbfcb9d5df8ae31de4dca3cf635a7ec0
Tyler Schwemley <tjschwem@gmail.com>
2024-08-11 21:27:12 -0400

    fix jolteon

--- nixos/profiles/proxmox.nix
+++ nixos/profiles/proxmox.nix
@@ -1,8 +1,5 @@
 {
-  imports = [
-    ../hardware/disks/proxmox.nix
-    ./server.nix
-  ];
+  imports = [./server.nix];
 
   boot.initrd.availableKernelModules = ["ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" "virtio_blk"];
   ethDev = "ens18";
