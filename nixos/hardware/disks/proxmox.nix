6e118c7f1da7575d29314cebe4654c6171de8d54
Tyler Schwemley <tjschwem@gmail.com>
2024-08-11 21:24:24 -0400

    wtf

--- nixos/hardware/disks/proxmox.nix
+++ nixos/hardware/disks/proxmox.nix
@@ -1,7 +1,7 @@
 let
   # these names are consistent across the proxmox nodes
-  rootName = "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0";  
-  storageName = "/dev/disk-by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi1";  
+  rootName = /dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0;  
+  storageName = /dev/disk-by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi1;  
 
   root = import ./ephemeral-root.nix {diskName = rootName;};
   storage = import ./block-storage.nix {diskName = storageName;};
