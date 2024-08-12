2bb523f7e90506603f92977fb1f28a2443c5251d
Tyler Schwemley <tjschwem@gmail.com>
2024-08-11 22:05:19 -0400

    fix disk path

--- nixos/hardware/disks/proxmox.nix
+++ nixos/hardware/disks/proxmox.nix
@@ -1,7 +1,7 @@
 let
   # these disk names are consistent across the proxmox nodes
   root = import ./ephemeral-root.nix "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi0";
-  storage = import ./block-storage.nix "/dev/disk-by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi1";
+  storage = import ./block-storage.nix "/dev/disk/by-id/scsi-0QEMU_QEMU_HARDDISK_drive-scsi1";
 in {
   imports = [
     root
