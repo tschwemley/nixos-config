06a4fb2894464c13fc5a867167c68d7ab3160383
Tyler Schwemley <tjschwem@gmail.com>
2024-08-15 02:29:34 -0400

    test flareon refactor

--- nixos/hosts/flareon/default.nix
+++ nixos/hosts/flareon/default.nix
@@ -1,3 +1,18 @@
+{
+  imports = [
+    ../../profiles/proxmox.nix
+    ../../services/samba.nix
+  ];
+
+  networking.hostName = "flareon";
+  sops.defaultSopsFile = ./secrets.yaml;
+
+  # read: https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion/ when ready to update
+  system.stateVersion = "23.05";
+}
+
+
+/*
 {lib, ...}: let
   boot = (import ../../system/boot.nix).systemd;
   profile = import ../../profiles/proxmox.nix;
@@ -19,3 +34,4 @@ in {
   # read: https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion when ready to update
   system.stateVersion = "23.05";
 }
+*/
