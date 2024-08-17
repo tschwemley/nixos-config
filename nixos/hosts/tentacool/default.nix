29973247fdac51759fb285611d3a1ae2de89ab3c
Tyler Schwemley <tjschwem@gmail.com>
2024-08-17 01:15:53 -0400

    add additional hardware to tentacool

--- nixos/hosts/tentacool/default.nix
+++ nixos/hosts/tentacool/default.nix
@@ -1,9 +1,21 @@
-let 
+{
+  inputs,
+  lib,
+  ...
+}: let
   partitions = import ../../hardware/disks/efi-partitions.nix;
   rootDisk = import ../../hardware/disks/ephemeral-root.nix "/dev/sda" partitions;
 in {
   imports = [
+    # hardware
     rootDisk
+    inputs.nixos-hardware.nixosModules.common-cpu-intel
+    inputs.nixos-hardware.nixosModules.common-gpu-intel-kaby-lake
+    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
+    inputs.nixos-hardware.nixosModules.common-pc-laptop
+    inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
+
+    # system + profile
     ../../system/boot/systemd.nix
     ../../profiles/server.nix
 
@@ -11,16 +23,32 @@ in {
     ../../server/jellyfin
   ];
 
+  boot = {
+    kernelParams = ["acpi_rev_override"];
+    kernelModules = ["kvm-intel"];
+  };
+
+  hardware.nvidia.prime = {
+    # Bus ID of the Intel GPU.
+    intelBusId = "PCI:0:2:0";
+
+    # Bus ID of the NVIDIA GPU.
+    nvidiaBusId = "PCI:1:0:0";
+  };
+
   networking = {
     hostName = "tentacool";
     wireless = {
       enable = true;
       environmentFile = "/run/secrets/wireless.env";
       networks."Where the Wild Pings Are" = {
-        psk = "@PSK_HOME@"; 
+        psk = "@PSK_HOME@";
       };
+      extraConfig = "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=wheel";
     };
   };
+
+  services.thermald.enable = lib.mkDefault true;
 
   sops = {
     defaultSopsFile = ./secrets.yaml;
