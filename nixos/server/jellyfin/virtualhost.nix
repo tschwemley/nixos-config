9d4f35fdfcfb4556a8bcaf0b3ec5453c089b2618
Tyler Schwemley <tjschwem@gmail.com>
2024-08-24 00:43:20 -0400

    update jellfyin vhost

--- nixos/server/jellyfin/virtualhost.nix
+++ nixos/server/jellyfin/virtualhost.nix
@@ -4,9 +4,13 @@
       locations = {
         "/" = {
           proxyPass = "http://127.0.0.1:8096";
+          extraConfig = ''
+            proxy_buffering off;
+          '';
           # proxyWebsockets = true;
         };
         "/socket" = {
+          proxyPass = "http://127.0.0.1:8096";
           proxyWebsockets = true;
           # extraConfig = ''
           #   proxy_set_header X-Forwarded-Protocol $scheme;
