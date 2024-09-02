e69405a11acffe84698b112b7c6b57e46676090b
Tyler Schwemley <tjschwem@gmail.com>
2024-09-02 00:14:19 -0400

    change secrets def

--- nixos/hosts/charizard/secrets.nix
+++ nixos/hosts/charizard/secrets.nix
@@ -3,7 +3,7 @@
 
   sops = {
     defaultSopsFile = ./secrets.yaml;
-    age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
+    # age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
     age.keyFile = "/home/schwem/.config/sops/age/keys.txt";
   };
 }
