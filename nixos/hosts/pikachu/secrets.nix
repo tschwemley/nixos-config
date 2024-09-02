01ca0994aed0dfc1edf077cae705a4b4af000af7
Tyler Schwemley <tjschwem@gmail.com>
2024-09-02 00:31:35 -0400

    test pikachu additions

--- nixos/hosts/pikachu/secrets.nix
+++ nixos/hosts/pikachu/secrets.nix
@@ -1,9 +1,11 @@
-{
+{lib, ...}: {
   imports = [./../../../home/secrets/ssh.nix];
 
   sops = {
+    gnupg.sshKeyPaths = lib.mkForce [];
     defaultSopsFile = ./secrets.yaml;
     # age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
-    age.keyFile = "/home/schwem/.config/sops/age/keys.txt";
+    age.sshKeyPaths = lib.mkForce [];
+    age.keyFile = lib.mkForce "/home/schwem/.config/sops/age/keys.txt";
   };
 }
