044ce207771450c68b2e201015ad509e376681a4
Tyler Schwemley <tjschwem@gmail.com>
2024-09-02 21:46:32 -0400

    update P keybind fugitive

--- home/programs/neovim/modules/filetypes/ftplugin/fugitive.lua
+++ home/programs/neovim/modules/filetypes/ftplugin/fugitive.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<tab>", "<Plug>fugitive:=", { buffer = 0 })
+vim.keymap.set("n", "P", "<Plug>fugitive:P<cr>", { buffer = 0 })
 vim.keymap.set("n", "q", "<Plug>fugitive:gq", { buffer = 0 })
