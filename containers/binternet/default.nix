a0ad1b92fdd56a7021acbbd8e3a740a36efc20fe
Tyler Schwemley <tjschwem@gmail.com>
2024-09-05 03:41:30 -0400

    fix binternet port

--- containers/binternet/default.nix
+++ containers/binternet/default.nix
@@ -9,6 +9,6 @@
   virtualisation.oci-containers.containers."binternet" = {
     autoStart = true;
     image = "ghcr.io/ahwxorg/binternet:latest";
-    ports = [ "127.0.0.1:${config.portMap.binternet}:8009" ];
+    ports = [ "127.0.0.1:${config.portMap.binternet}:8080" ];
   };
 }
