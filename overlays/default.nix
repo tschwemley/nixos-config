3351ff3bae97e3cad3d5ee9f4bd6111bd5171cca
Tyler Schwemley <tjschwem@gmail.com>
2024-09-04 22:45:54 -0400

    add forgejo overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -6,6 +6,14 @@ self': [
       json2go
       wl-ocr
       ;
+
+    forgejo = prev.forgejo.override {
+      postInstall = ''
+        mv $out/bin/gitea $out/bin/forgejo
+        ${builtins.replaceStrings [ "gitea" ] [ "forgejo" ] prev.forgejo}
+      '';
+    };
+
     vimPlugins = prev.vimPlugins // {
       inherit (self'.packages) codecompanion;
     };
