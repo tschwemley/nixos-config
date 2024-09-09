d3527d4bc3aa422cc9608d71d7060e06179bd06a
Tyler Schwemley <tjschwem@gmail.com>
2024-09-08 23:11:18 -0400

    psql update

--- nixos/server/postgresql/default.nix
+++ nixos/server/postgresql/default.nix
@@ -14,7 +14,7 @@
 
     authentication = lib.mkOverride 10 ''
       #type database  DBuser    auth-method optional_ident_map
-      local all       postgres  peer
+      # local all       postgres  peer
       local sameuser  all       peer        map=user_map
     '';
   };
