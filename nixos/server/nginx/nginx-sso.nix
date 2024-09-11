2a99184f24fb1852314904b62f6945947b4373dd
Tyler Schwemley <tjschwem@gmail.com>
2024-09-11 02:32:01 -0400

    syntax

--- nixos/server/nginx/nginx-sso.nix
+++ nixos/server/nginx/nginx-sso.nix
@@ -16,7 +16,7 @@ in
         extraConfig = ''
           internal;
 
-          proxy_pass http://127.0.0.1:${config.portMap.nginx-sso}/auth
+          proxy_pass http://127.0.0.1:${config.portMap.nginx-sso}/auth;
           proxy_pass_request_body off;
           proxy_set_header Content-Length "";
           proxy_set_header X-Origin-URI $request_uri;
