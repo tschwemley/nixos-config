422fe3bced1331f4b19742401beb1724e5ca75c1
Tyler Schwemley <tjschwem@gmail.com>
2024-09-17 01:33:51 -0400

    update samba nix config

--- nixos/services/samba.nix
+++ nixos/services/samba.nix
@@ -1,21 +1,22 @@
-{config, ...}: {
+{ config, ... }:
+{
   services.samba = {
     enable = true;
     securityType = "user";
     openFirewall = true;
-    extraConfig = ''
-      workgroup = WORKGROUP
-      server string = ${config.networking.hostName}
-      netbios name = ${config.networking.hostName}
-      security = user
+    settings = {
+      workgroup = "WORKGROUP";
+      "server string" = config.networking.hostName;
+      "netbios name" = config.networking.hostName;
+      security = "user";
       #use sendfile = yes
       #max protocol = smb2
       # note: localhost is the ipv6 localhost ::1
-      hosts allow = 192.168.1.0/24 127.0.0.1 localhost
-      hosts deny = 0.0.0.0/0
-      guest account = nobody
-      map to guest = bad user
-    '';
+      "hosts allow" = "192.168.1.0/24 127.0.0.1 localhost";
+      "hosts deny" = "0.0.0.0/0";
+      "guest account" = "nobody";
+      "map to guest" = "bad user";
+    };
     shares = {
       private = {
         path = "/storage/shh";
