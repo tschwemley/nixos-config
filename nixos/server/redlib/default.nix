c863975d178580126f37525f6e0041e4446aad42
Tyler Schwemley <tjschwem@gmail.com>
2024-09-17 03:17:00 -0400

    redlib service update

--- nixos/server/redlib/default.nix
+++ nixos/server/redlib/default.nix
@@ -11,15 +11,6 @@ let
 in
 {
   services = {
-    # TODO: remove this after upstream bug is fixed
-    # BUG: https://github.com/redlib-org/redlib/issues/229
-    cron = {
-      enable = true;
-      systemCronJobs = [
-        "*/5 * * * * systemctl restart redlib.service"
-      ];
-    };
-
     nginx.virtualHosts."reddit.schwem.io" = {
       locations."/" = {
         proxyPass = "http://${address}:${config.portMap.redlib}";
@@ -43,6 +34,13 @@ in
       REDLIB_DEFAULT_USE_HLS = "on";
       REDLIB_DEFAULT_HIDE_HLS_NOTIFICATION = "on";
       # see: https://github.com/redlib-org/redlib?tab=readme-ov-file#configuration
+    };
+
+    # TODO: remove this after upstream bug is fixed
+    # BUG: https://github.com/redlib-org/redlib/issues/229
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = "300s";
     };
   };
 }
