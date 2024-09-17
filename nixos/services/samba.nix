ef700a9b82aa7627764bda39cbb16f41cfa19d41
Tyler Schwemley <tjschwem@gmail.com>
2024-09-17 01:51:51 -0400

    modernize samba config

--- nixos/services/samba.nix
+++ nixos/services/samba.nix
@@ -5,17 +5,21 @@
     securityType = "user";
     openFirewall = true;
     settings = {
-      workgroup = "WORKGROUP";
-      "server string" = config.networking.hostName;
-      "netbios name" = config.networking.hostName;
-      security = "user";
+      global = {
+        workgroup = "WORKGROUP";
+        security = "user";
+      };
+      server.string = config.networking.hostName;
+      netbios.name = config.networking.hostName;
       #use sendfile = yes
       #max protocol = smb2
       # note: localhost is the ipv6 localhost ::1
-      "hosts allow" = "192.168.1.0/24 127.0.0.1 localhost";
-      "hosts deny" = "0.0.0.0/0";
-      "guest account" = "nobody";
-      "map to guest" = "bad user";
+      hosts = {
+        allow = "192.168.1.0/24 127.0.0.1 localhost";
+        deny = "0.0.0.0/0";
+      };
+      # "guest account" = "nobody";
+      # "map to guest" = "bad user";
     };
     shares = {
       private = {
