0c1cec8deba1f24a1d026e47f95db02a09bd75c5
Tyler Schwemley <tjschwem@gmail.com>
2024-09-26 14:20:25 -0400

    add check token endpoint

--- nixos/server/oidc_sso/default.nix
+++ nixos/server/oidc_sso/default.nix
@@ -10,6 +10,9 @@ let
 in
 {
   services.nginx.virtualHosts."auth.schwem.io".locations = {
+    "check-token" = {
+      proxyPass = "http://127.0.0.1:${config.portMap.oidc-sso}/check-token";
+    };
     "/login" = {
       proxyPass = "http://127.0.0.1:${config.portMap.oidc-sso}$request_uri";
     };
