083078245ef5493fdfb308daf6398ede3542e335
Tyler Schwemley <tjschwem@gmail.com>
2024-10-06 01:36:11 -0400

    update ssh config

--- home/xdg/ssh/personal.nix
+++ home/xdg/ssh/personal.nix
@@ -1,16 +1,36 @@
-{ osConfig, ... }:
+{ config, ... }:
 {
   programs.ssh.matchBlocks = {
     charizard = {
       hostname = "charizard";
-      # TODO: fill out the IdentityFile portions and create the secrets
-      # IdentityFile = osConfig.sops.secrets.charizard_ssh_key.path;
+      identityFile = config.sops.secrets.charizard_ssh_key.path;
       user = "schwem";
     };
     pikachu = {
       hostname = "pikachu";
-      # IdentityFile = osConfig.sops.secrets.pikachu_ssh_key.path;
+      identityFile = config.sops.secrets.pikachu_ssh_key.path;
       user = "schwem";
     };
+    personalGithub = {
+      host = "gh github.com";
+      hostname = "github.com";
+      identityFile = config.sops.secrets.personal_gh_key.path;
+      user = "git";
+    };
   };
+
+  sops.secrets = builtins.listToAttrs (
+    map
+      (name: {
+        inherit name;
+        value = {
+          sopsFile = ./keys.yaml;
+        };
+      })
+      [
+        "charizard_ssh_key"
+        "personal_gh_key"
+        "pikachu_ssh_key"
+      ]
+  );
 }
--- home/xdg/ssh/servers.nix
+++ home/xdg/ssh/servers.nix
@@ -1,21 +1,36 @@
+{ config, ... }:
 {
-  programs.ssh.matchBlocks = builtins.listToAttrs (
-    map
-      (name: {
-        inherit name;
-        value = {
-          hostname = name;
-          user = "root";
-        };
-      })
-      [
-        "articuno"
-        "zapados"
-        "moltres"
-        "eevee"
-        "jolteon"
-        "flareon"
-        "tentacool"
-      ]
-  );
+  programs.ssh.matchBlocks =
+    builtins.listToAttrs (
+      map
+        (name: {
+          inherit name;
+          value = {
+            hostname = name;
+            user = "root";
+          };
+        })
+        [
+          "articuno"
+          "zapados"
+          "moltres"
+          "eevee"
+          "jolteon"
+          "flareon"
+          "tentacool"
+        ]
+    )
+    // {
+      forgejo = {
+        host = "forgejo git.schwem.io";
+        hostname = "git.schwem.io";
+        identityFile = config.sops.secrets.forgejo_schwem_io_key.path;
+        port = 2222;
+        user = "forgejo";
+      };
+    };
+
+  sops.secrets.forgejo_schwem_io_key = {
+    sopsFile = ./keys.yaml;
+  };
 }
