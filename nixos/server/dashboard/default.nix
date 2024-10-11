2a30d2b06323822434caa3cd68973a9740403a2c
Tyler Schwemley <tjschwem@gmail.com>
2024-10-11 00:26:02 -0400

    create dashboard service

--- nixos/server/dashboard/default.nix
+++ nixos/server/dashboard/default.nix
@@ -1,4 +1,13 @@
-{ config, ... }:
+{
+  config,
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  pkg = inputs.dashboard.packages.${pkgs.system}.default;
+  stateDir = "/var/lib/dashboard";
+in
 {
   services.nginx.virtualHosts."dash.schwem.io" = {
     extraConfig = ''
@@ -21,6 +30,66 @@
       };
 
       "@error401".return = "302 https://auth.schwem.io/login?rd=https://dash.schwem.io";
+    };
+  };
+
+  systemd.services.dashboard = {
+    enable = true;
+    serviceConfig = {
+      Type = "simple";
+      User = "dashboard";
+      Group = "dashboard";
+
+      SyslogIdentifier = "dashboard";
+      WorkingDirectory = stateDir;
+
+      ExecStart = "${pkg}/bin/dashboard -e ${config.sops.secrets.dashboard_env.path}";
+
+      # Hardening options
+      LockPersonality = "yes";
+      ProtectSystem = "yes";
+      ProtectClock = "yes";
+      ProtectControlGroups = "yes";
+      ProtectHome = "yes";
+      ProtectHostname = "yes";
+      ProtectKernelLogs = "yes";
+      ProtectKernelModules = "yes";
+      ProtectKernelTunables = "yes";
+      ProtectProc = "invisible";
+      RemoveIPC = "yes";
+      RestrictAddressFamilies = "AF_INET AF_INET6 AF_UNIX AF_NETLINK";
+      RestrictNamespaces = "yes";
+      RestrictRealtime = "yes";
+      NoNewPrivileges = "yes";
+      PrivateDevices = "yes";
+      PrivateTmp = "yes";
+    };
+
+    description = "dashboard";
+    after = [
+      "network.target"
+      "keycloak.service"
+    ];
+    requires = [
+      "keycloak.service"
+      "nginx.service"
+    ];
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  sops.secrets.dashboard_env = {
+    group = "dashboard";
+    owner = "dashboard";
+    path = "${stateDir}/.env";
+    mode = "0440";
+    sopsFile = ./secrets.yaml;
+  };
+
+  users = {
+    groups.dashboard = { };
+    users.dashboard = {
+      group = "dashboard";
+      isSystemUser = true;
     };
   };
 }
