fb7744e035a96d01781d23ed6a4adcadb335fda3
Tyler Schwemley <tjschwem@gmail.com>
2024-10-23 21:22:36 -0400

    fixup init of arr

--- nixos/server/arr/sabnzbd/default.nix
+++ nixos/server/arr/sabnzbd/default.nix
@@ -19,11 +19,13 @@
       ];
     };
 
-    tmpfiles.rules = [
-      "d /storage/downloads 0660 sabznbd arr - -"
-      "f /var/lib/sabnzbd/scripts/post-process 0750 sabnzbd sabnzbd -"
-      "w /var/lib/sabnzbd/scripts/post-process 0750 sabnzbd sabnzbd - ${./post-process}"
-    ];
+    tmpfiles = {
+      rules = [
+        "d /storage/downloads 0660 sabznbd arr - -"
+        "d /var/lib/sabnzbd 0760 sabznbd sabnzbd - -"
+        "C+ /var/lib/sabnzbd/scripts/post-process 0755 sabnzbd sabnzbd - ${./post-process}"
+      ];
+    };
   };
 
   users.users.sabnzbd.extraGroups = [ "arr" ];
