f7aac1be124b9a19ce4bbcacd31b439edbed5ec4
Tyler Schwemley <tjschwem@gmail.com>
2024-10-25 16:14:47 -0400

    neovim plugin tweaks

--- home/programs/neovim/modules/db/default.nix
+++ home/programs/neovim/modules/db/default.nix
@@ -1,14 +1,22 @@
 {
+  lib,
   pkgs,
   ...
 }:
 {
-  programs.neovim.plugins = with pkgs.vimPlugins; [
-    nvim-dbee
-    vim-dadbod
-    vim-dadbod-completion
-    vim-dadbod-ui
-  ];
+  programs.neovim.plugins =
+    with pkgs.vimPlugins;
+    let
+      dbee = nvim-dbee.overrideAttrs {
+        meta.platforms = lib.platforms.linux ++ lib.platforms.darwin;
+      };
+    in
+    [
+      dbee
+      vim-dadbod
+      vim-dadbod-completion
+      vim-dadbod-ui
+    ];
 
   xdg.configFile."nvim/after/plugin/db.lua".source = ./db.lua;
 }
---
+++ home/programs/neovim/modules/filetypes/ftplugin/http.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "R", vim.cmd("Rest run"), { buffer = 0 })
