2491140a405667d3da9cd9f2fc450babcd4eb3d8
Tyler Schwemley <tjschwem@gmail.com>
2024-10-29 02:28:09 -0400

    clean up wip nzbhydra2

--- nixos/server/arr/nzbhydra2.nix
+++ nixos/server/arr/nzbhydra2.nix
@@ -1,10 +1,8 @@
 {
-  config,
   pkgs,
   secretsPath,
   ...
-}:
-{
+}: {
   services = {
     # TODO: uncomment if exposing via web; otherwise delete
     # nginx.virtualHosts."nzbhydra.schwem.io".locations = {
@@ -19,19 +17,9 @@
   systemd.services.nzbhydra2 = {
     environment.SOPS_AGE_KEY_FILE = "/etc/sops/age-keys.txt";
     preStart = ''
-      #${pkgs.sops}/bin/sops --config '/etc/sops/age-keys.txt' -d ${secretsPath}/server/nzbhydra.yaml > /var/lib/nzbhydra2/nzbhydra.yml
       ${pkgs.sops}/bin/sops -d ${secretsPath}/server/nzbhydra.yaml > /var/lib/nzbhydra2/nzbhydra.yml
     '';
 
-    serviceConfig.ReadPaths = [ "/etc/sops/age-keys.txt" ];
+    serviceConfig.ReadPaths = ["/etc/sops/age-keys.txt"];
   };
-
-  # sops.secrets."nzbhydra.yaml" = {
-  #   group = config.systemd.services.nzbhydra2.serviceConfig.Group;
-  #   owner = config.systemd.services.nzbhydra2.serviceConfig.User;
-  #   mode = "0600";
-  #   path = "/var/lib/nzbhydra2/nzbhydra.yml";
-  #   sopsFile = "${secretsPath}/nzbhydra.yaml";
-  #   format = "binary";
-  # };
 }
