472c304e0c43e96af853d2d61e9137411736b1c8
Tyler Schwemley <tjschwem@gmail.com>
2024-10-30 03:55:20 -0400

    add vhost entry for tiddly

--- nixos/server/knowledge/tiddlywiki.nix
+++ nixos/server/knowledge/tiddlywiki.nix
@@ -1,9 +1,15 @@
 {config, ...}: {
-  services.tiddlywiki = {
-    enable = true;
-    listenOptions = {
-      # for available options see: https://tiddlywiki.com/#WebServer
-      port = config.portMap.tiddlywiki;
+  services = {
+    nginx.virtualHosts.jolteon = {
+      locations."/".proxyPass = "127.0.0.1:${config.portMap.tiddlywiki}";
+    };
+
+    tiddlywiki = {
+      enable = true;
+      listenOptions = {
+        # for available options see: https://tiddlywiki.com/#WebServer
+        port = config.portMap.tiddlywiki;
+      };
     };
   };
 }
