448b627c6209c1e898eee6aaa5d3b8cbc1b66fa0
Tyler Schwemley <tjschwem@gmail.com>
2024-10-31 20:42:27 -0400

    update moltres definition for age key file

--- nixos/hosts/moltres/default.nix
+++ nixos/hosts/moltres/default.nix
@@ -15,10 +15,7 @@
 
   networking.hostName = "moltres";
 
-  sops = {
-    defaultSopsFile = ./secrets.yaml;
-    age.keyFile = "/root/.config/sops/age/keys.txt";
-  };
+  sops.defaultSopsFile = ./secrets.yaml;
 
   # read: https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion when ready to update
   system.stateVersion = "23.05";
