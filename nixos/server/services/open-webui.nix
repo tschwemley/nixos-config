6c08b8afea8ba81497b549ba113cfd5d3d3584ad
Tyler Schwemley <tjschwem@gmail.com>
2024-11-04 11:40:50 -0500

    update dev deps

--- nixos/server/services/open-webui.nix
+++ nixos/server/services/open-webui.nix
@@ -1,10 +1,40 @@
 {
+  config,
+  secretsPath,
+  ...
+}: {
   services.open-webui = {
     enable = true;
-    environment = {
-      ANONYMIZED_TELEMETRY = "False";
-      DO_NOT_TRACK = "True";
-      SCARF_NO_ANALYTICS = "True";
+    environmentFile = config.sops.templates.open-webui-env.path;
+    # environment = {
+    #   ANONYMIZED_TELEMETRY = "False";
+    #   DO_NOT_TRACK = "True";
+    #   SCARF_NO_ANALYTICS = "True";
+    # };
+  };
+
+  sops = {
+    secrets = let
+      secretAttrs = {
+        sopsFile = "${secretsPath}/server/open-webui.yaml";
+      };
+    in {
+      oauth_client_secret = secretAttrs;
+    };
+
+    templates.open-webui-env = {
+      mode = "0500";
+      content =
+        /*
+        dotenv
+        */
+        ''
+          ENABLE_OAUTH_SIGNUP=true
+          OAUTH_CLIENT_ID=open-webui
+          OAUTH_CLIENT_SECRET=${config.sops.placeholder.oauth_client_secret}
+          OPENID_PROVIDER_URL=https://auth.schwem.io/realms/schwem-io/.well-known/openid-configuration
+          OPENID_REDIRECT_URL=https://ai.schwem.io/
+        '';
     };
   };
 }
