0efdfdaa80202c05bd5d19c57986d16f17125b73
Tyler Schwemley <tjschwem@gmail.com>
2024-11-16 20:04:14 -0500

    bambu studio dep bug now merged into unstable

--- home/programs/bambu-studio.nix
+++ home/programs/bambu-studio.nix
@@ -1,10 +1,3 @@
-# {pkgs, ...}: {
-#   home.packages = with pkgs; [bambu-studio];
-# }
-{
-  inputs,
-  pkgs,
-  ...
-}: {
-  home.packages = [inputs.nixpkgs-master.legacyPackages.${pkgs.system}.bambu-studio];
+{pkgs, ...}: {
+  home.packages = with pkgs; [bambu-studio];
 }
