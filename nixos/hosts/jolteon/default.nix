1b8f0d9348e4b1f97f0954b00e2817c5c6da47fc
Tyler Schwemley <tjschwem@gmail.com>
2024-11-27 04:40:47 -0500

    disable a couple of services on jolteon temporarily

--- nixos/hosts/jolteon/default.nix
+++ nixos/hosts/jolteon/default.nix
@@ -7,8 +7,8 @@
 
     ../../server/alt-frontends/anonymous-overflow.nix
     ../../server/development/forgejo
-    ../../server/infrastructure/postgresql
-    ../../server/infrastructure/webhooks
+    # ../../server/infrastructure/postgresql
+    # ../../server/infrastructure/webhooks
 
     # TODO: fix or delete... not a priority atm.
     # Fails during start with: Lucky::ForceSSLHandler was nil, but the setting is required.
