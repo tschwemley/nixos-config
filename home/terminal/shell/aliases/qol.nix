f6cab4565b335ccd6a00df88f62a8dee63c4fe95
Tyler Schwemley <tjschwem@gmail.com>
2024-12-08 23:14:51 -0500

    add alias

--- home/terminal/shell/aliases/qol.nix
+++ home/terminal/shell/aliases/qol.nix
@@ -5,11 +5,12 @@
   checkmullvad = "curl -sS https://am.i.mullvad.net/json | jq";
   copy = "wl-copy";
   dirsizes = "sudo du -h --max-depth=1 | sort -hr";
-  notes = "nvim -c 'Neorg workspace notes' -c 'Neorg journal today'";
+  jl = "journalctl -xeu";
   l = "ls";
   ll = "ls -alh";
   lgroups = "bat /etc/group";
   lusers = "bat /etc/passwd";
+  notes = "nvim -c 'Neorg workspace notes' -c 'Neorg journal today'";
   randstr = "head /dev/urandom | tr -dc A-Za-z0-9 | head -c10";
   userctl = "systemctl --user";
 }
