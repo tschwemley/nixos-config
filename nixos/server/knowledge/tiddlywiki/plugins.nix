f46033815badcb2c193e2bc776c9581f925e4f87
Tyler Schwemley <tjschwem@gmail.com>
2024-12-13 00:45:48 -0500

    expand on tiddlywiki plugins

--- nixos/server/knowledge/tiddlywiki/plugins.nix
+++ nixos/server/knowledge/tiddlywiki/plugins.nix
@@ -1,23 +1,36 @@
 {pkgs, ...}: let
-  tiddlywikiPlugins = pkgs.stdenv.mkDerivation {
+  tiddlywikiPlugins = pkgs.stdenvNoCC.mkDerivation {
     name = "tiddlywikiPlugins";
-    src = pkgs.fetchFromGitHub {
-      owner = "flibbles";
-      repo = "tw5-relink";
-      rev = "v2.4.4";
-      hash = "sha256-AZtbakbRM3nFcXkDTDkhMZlFG1h9Fiw7t0MNCZ1/LfY=";
-    };
+
+    srcs = [
+      # projectify plugin
+      (pkgs.fetchFromGitHub {
+        owner = "ThaddeusJiang";
+        repo = "projectify";
+        rev = "0.14.3";
+        hash = "sha256-2WyZUaBP+BB9ysmxkrUNcGQLPJJuC/8116myn5s6LXU=";
+      })
+      # relink plugins
+      (pkgs.fetchFromGitHub {
+        owner = "flibbles";
+        repo = "tw5-relink";
+        rev = "v2.4.4";
+        hash = "sha256-AZtbakbRM3nFcXkDTDkhMZlFG1h9Fiw7t0MNCZ1/LfY=";
+      })
+    ];
+
+    unpackPhase = ''
+      runHook preUnpack;
+
+      mkdir -p $out/plugins
+      for _src in $srcs; do
+        cp -r $_src/plugins/* $out/plugins/
+      done
+
+      runHook postUnpack;
+    '';
 
     dontBuild = true;
-
-    installPhase = ''
-      runHook preInstall
-
-      mkdir $out
-      cp -r $src/plugins $out/
-
-      runHook postInstall
-    '';
   };
 in {
   systemd.services.tiddlywiki = {
