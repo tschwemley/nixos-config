4359374954e56cf47fd5c89e9e9ff73cf2cb7a47
Tyler Schwemley <tjschwem@gmail.com>
2025-01-09 01:39:33 -0500

    wip pulseaudio config

--- nixos/hardware/audio/pipewire.nix
+++ nixos/hardware/audio/pipewire.nix
@@ -1,26 +1,16 @@
-{
-  lib,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   environment = {
     # TODO: not really sure if these are necessary for anything I'm running or not yet reeval later
-    sessionVariables = {
-      SDL_AUDIODRIVER = "pipewire";
-      ALSOFT_DRIVERS = "pipewire";
-    };
+    # sessionVariables = {
+    #   SDL_AUDIODRIVER = "pipewire";
+    #   ALSOFT_DRIVERS = "pipewire";
+    # };
 
     systemPackages = with pkgs; [
       pavucontrol
       pamixer
     ];
   };
-
-  hardware.pulseaudio.enable = lib.mkDefault false;
-
-  # Enables the RealtimeKit system service, which hands out realtime scheduling priority to user
-  # processes on demand. For example, the PulseAudio server uses this to acquire realtime priority.
-  security.rtkit.enable = true;
 
   services.pipewire = {
     enable = true;
@@ -34,4 +24,8 @@
     pulse.enable = true;
     wireplumber.enable = true;
   };
+
+  # Enables the RealtimeKit system service, which hands out realtime scheduling priority to user
+  # processes on demand. For example, the PulseAudio server uses this to acquire realtime priority.
+  security.rtkit.enable = true;
 }
