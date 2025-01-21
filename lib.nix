f63e163468834274f0d4f35c7e4e4ea5f9ac2d22
Tyler Schwemley <tjschwem@gmail.com>
2025-01-21 01:11:36 -0500

    lib.nix add

--- lib.nix
+++ lib.nix
@@ -1,8 +1,14 @@
-{
+lib:
+lib
+// {
+  mkStrOption = attrs: lib.mkOption ({type = lib.types.str;} // attrs);
+
   # define secret paths as constants for convenience
   secrets = {
     home = ./secrets/home;
     nixos = ./secrets/nixos;
     server = ./secrets/server;
   };
+
+  stringList = lib.types.listOf lib.types.str;
 }
