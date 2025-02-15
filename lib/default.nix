3a31a39653b0e093a523178b729faae70be31d7c
Tyler Schwemley <tjschwem@gmail.com>
2025-02-15 01:36:53 -0500

    update port mappings retrieval

---
+++ lib/default.nix
@@ -0,0 +1,66 @@
+lib: 
+lib
+// rec {
+  # getPort = name: port-map.${name};
+  #
+  # getPortAsString = name: lib.strings.toInt (getPort name);
+  #
+  # port-map = import ./port-map.nix;
+
+  flattenAttrs = attrset: builtins.concatLists (builtins.attrValues attrset);
+
+  mkStrOption = attrs: lib.mkOption ({type = lib.types.str;} // attrs);
+
+  nixpkgsPatch = system: nixpkgs:
+    (import nixpkgs {
+      inherit system;
+      # config.allowUnfree = true;
+    })
+    .applyPatches {
+      name = "nixpkgs-patched";
+      src = nixpkgs;
+      patches = import ./patches.nix;
+    };
+
+  # define secret paths as constants for convenience
+  secrets = {
+    home = ../secrets/home;
+    nixos = ../secrets/nixos;
+    server = ../secrets/server;
+  };
+
+  stringList = lib.types.listOf lib.types.str;
+
+  port-map = {
+    anonymous-overflow = "8010";
+    binternet = "8009";
+    dashboard = "6980";
+    excalidraw = "8380";
+    freetar = "22000";
+    forgejo = "8020";
+    invidious = "3100";
+    it-tools = "7001";
+    nginx-sso = "8082";
+    nzbhydra2 = "5076";
+    oidcproxy = "1337";
+    open-webui = "4141";
+    priviblur = "8040";
+    proxitok = "8050";
+    pyload = "8000";
+    qtbittorrentWeb = "8880";
+    qtbittorrentTorrent = "8881";
+    redlib = "8180";
+    rimgo = "8030";
+    sabnzbd = "8314";
+    safetwitch-frontend = "8280";
+    safetwitch-backend = "7100";
+    scribe = "7000";
+    searxng = "8888";
+    stash = "6969";
+    threadfin = "34400";
+    tiddlywiki = "4242";
+    webhooks = "7780";
+
+  };
+
+}
--- overlays.nix
+++
@@ -1,5 +0,0 @@
-{
-  inputs,
-  packages,
-}: {
-}
