3becf15c80f13f0e453285b408a3fe48c3c10fd3
Tyler Schwemley <tjschwem@gmail.com>
2025-03-02 19:09:54 -0500

    attempt reenabling articuno block storage

--- nixos/hosts/articuno/default.nix
+++ nixos/hosts/articuno/default.nix
@@ -1,29 +1,30 @@
-# let
-# storageDisks = [
-#   (import ../../hardware/disks/block-storage.nix {device = "/dev/disk/by-id/scsi-0BUYVM_SLAB_VOLUME-28656";})
-#   (import ../../hardware/disks/block-storage.nix {
-#     device = "/dev/disk/by-id/scsi-0BUYVM_SLAB_VOLUME-28639";
-#     mountpoint = "/storage2";
-#   })
-# ];
-# in {
-{
-  imports = [
-    (import ../../profiles/buyvm.nix "")
-
-    # server imports
-    # ../../server/ai/open-webui.nix
-    ../../server/dashboard
-    ../../server/development/cyberchef.nix
-    ../../server/infrastructure/haproxy
-    ../../server/infrastructure/monitoring
-    ../../server/infrastructure/postgresql
-    ../../server/security/acme
-    ../../server/security/auth/keycloak
-    ../../server/services/searxng
+let
+  storageDisks = [
+    (import ../../hardware/disks/block-storage.nix {
+      device = "/dev/disk/by-id/scsi-0BUYVM_SLAB_VOLUME-28656";
+    })
+    (import ../../hardware/disks/block-storage.nix {
+      device = "/dev/disk/by-id/scsi-0BUYVM_SLAB_VOLUME-28639";
+      mountpoint = "storage2";
+    })
   ];
-  # FIXME: storage disks causing boot failure
-  # ++ storageDisks;
+in {
+  imports =
+    [
+      (import ../../profiles/buyvm.nix "")
+
+      # server imports
+      # ../../server/ai/open-webui.nix
+      ../../server/dashboard
+      ../../server/development/cyberchef.nix
+      ../../server/infrastructure/haproxy
+      ../../server/infrastructure/monitoring
+      ../../server/infrastructure/postgresql
+      ../../server/security/acme
+      ../../server/security/auth/keycloak
+      ../../server/services/searxng
+    ]
+    ++ storageDisks;
 
   networking.hostName = "articuno";
 
