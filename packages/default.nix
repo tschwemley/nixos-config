c1a5148e7578c1e73b6fd08b909c45f0c997d9a0
Tyler Schwemley <tjschwem@gmail.com>
2025-03-03 00:17:56 -0500

    update package default

--- packages/default.nix
+++ packages/default.nix
@@ -5,5 +5,6 @@ pkgs: {
   scripts = import ./scripts pkgs;
   tailscale = pkgs.callPackage ./tailscale.nix {};
   tree-sitter-bruno = pkgs.callPackage ./tree-sitter-bruno.nix {};
+  trmnl-server = pkgs.callPackage ./trmnl-server.nix {};
   wl-ocr = pkgs.callPackage ./wl-ocr.nix {};
 }
