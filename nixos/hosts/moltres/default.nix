3baba53fc29bb37e7f23fd945bf799bedb66b564
Tyler Schwemley <tjschwem@gmail.com>
2025-03-11 22:37:54 -0400

    temporarily disable some services on moltres

--- nixos/hosts/moltres/default.nix
+++ nixos/hosts/moltres/default.nix
@@ -2,15 +2,14 @@
   imports = [
     ../../profiles/racknerd.nix
 
-    ../../server/ai/open-webui.nix
-    ../../server/ai/sillytavern.nix
+    # ../../server/ai/open-webui.nix
+    # ../../server/ai/sillytavern.nix
     ../../server/alt-frontends/freetar.nix
     ../../server/alt-frontends/redlib.nix
     ../../server/knowledge/excalidraw
-    ../../server/infrastructure/haproxy
+    # ../../server/infrastructure/haproxy
 
-    # TODO: decide if this will be one of the servers to load balance redlib on
-    # ../../server/alt-frontends/redlib.nix
+    ../../server/alt-frontends/redlib.nix
 
     #../../server/alt-frontends/rimgo.nix
     # ../../server/alt-frontends/scribe
