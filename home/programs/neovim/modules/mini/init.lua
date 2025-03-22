8c627e0a77e34f5f2394a5b654b93d2cc0c62a73
Tyler Schwemley <tjschwem@gmail.com>
2025-03-22 18:42:38 -0400

    disable mini.diff

--- home/programs/neovim/modules/mini/init.lua
+++ home/programs/neovim/modules/mini/init.lua
@@ -2,6 +2,7 @@
 -- This file contains mini modules with minimal to no deviation from the default setup config
 --
 -- Plugins I've tried and not liked:
+--    MAYBE: mini.diff => makes rnu number a different color based on changes? fkn hate that
 --    * mini.git coloring all fucked; git wrapper seems useless [e.g. :Git blame works in fugitive
 --       w/ sane defaults. mini.git requires args to be passed. Then why don't I just use !git ...])
 
@@ -10,7 +11,7 @@ require("mini.icons").setup()
 require("mini.statusline").setup()
 
 -- General WorkFlow
-require("mini.diff").setup()
+-- require("mini.diff").setup()
 require("mini.visits").setup()
 
 -- Text Editing
