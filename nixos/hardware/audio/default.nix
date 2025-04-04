f4f2efe3559ed8f2a0f04685293085b5bfa2ceb9
Tyler Schwemley <tjschwem@gmail.com>
2025-04-04 01:53:18 -0400

    realtime audio changes

--- nixos/hardware/audio/default.nix
+++ nixos/hardware/audio/default.nix
@@ -1,7 +1,24 @@
 {
+  inputs,
+  pkgs,
+  ...
+}: {
   imports = [
+    inputs.musnix.nixosModules.musnix
     ./bluetooth.nix
     ./pipewire.nix
     ./scarlett8i6.nix
   ];
+
+  musnix = {
+    enable = true;
+
+    kernel = {
+      packages = pkgs.linuxPackages_latest_rt;
+      realtime = true;
+    };
+
+    rtcqs.enable = true;
+    soundcardPciId = "00:1f.3"; # TODO: check this value on pikachu and define via host config
+  };
 }
