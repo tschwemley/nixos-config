af6ab9259455565926d6420bfb762e31f05355cb
Tyler Schwemley <tjschwem@gmail.com>
2025-04-05 04:43:41 -0400

    fix sillytavern

--- nixos/server/ai/sillytavern.nix
+++ nixos/server/ai/sillytavern.nix
@@ -1,19 +1,23 @@
 {config, ...}: let
   stateDir = "/var/lib/sillytavern";
+  user = "sillytavern";
 in {
-  systemd.tmpfiles.rules = ["d ${stateDir} 0770 root users - -"];
+  systemd.tmpfiles.rules = ["d ${stateDir} 0770 ${user} users - -"];
+
+  users.users.${user} = {
+    isNormalUser = true;
+  };
 
   virtualisation.oci-containers.containers.silly-tavern = {
     autoStart = true;
     image = "ghcr.io/sillytavern/sillytavern:latest";
-    # extraOptions = ["--network=host"];
     ports = ["127.0.0.1:${config.variables.ports.silly-tavern}:8000"];
+    pull = "always";
     volumes = [
-      "${stateDir}/config:/home/node/app/config"
-      "${stateDir}/data:/home/node/app/data"
-      "${stateDir}/plugins:/home/node/app/plugins"
+      "${stateDir}:/home/node/app"
+      # "${stateDir}:/home/node/app/config"
+      # "${stateDir}/data:/home/node/app/data"
+      # "${stateDir}/plugins:/home/node/app/plugins"
     ];
   };
 }
-# original docker command:
-# docker run -d --device /dev/kfd --device /dev/dri -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama:rocm
