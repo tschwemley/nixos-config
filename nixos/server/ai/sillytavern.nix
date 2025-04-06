b54e0b1fb7ac7ee91a5416a641a4cf0859760859
Tyler Schwemley <tjschwem@gmail.com>
2025-04-05 22:24:39 -0400

    update sillytavern

--- nixos/server/ai/sillytavern.nix
+++ nixos/server/ai/sillytavern.nix
@@ -2,15 +2,21 @@
   stateDir = "/var/lib/sillytavern";
   user = "sillytavern";
 in {
-  systemd.tmpfiles.rules = ["d ${stateDir} 0770 ${user} users - -"];
+  services.nginx.virtualHosts."sillytavern.schwem.io".
 
-  users.users.${user} = {
-    isNormalUser = true;
-  };
+  systemd.tmpfiles.rules = [
+    "d ${stateDir}/config 0750 ${user} users - -"
+    "d ${stateDir}/data 0750 ${user} users - -"
+    "d ${stateDir}/extensions 0750 ${user} users - -"
+    "d ${stateDir}/plugins 0750 ${user} users - -"
+  ];
+
+  users.users.${user}.isNormalUser = true;
 
   virtualisation.oci-containers.containers.silly-tavern = {
     autoStart = true;
     image = "ghcr.io/sillytavern/sillytavern:latest";
+    podman = {inherit user;};
     ports = ["127.0.0.1:${config.variables.ports.silly-tavern}:8000"];
     pull = "always";
     volumes = [
