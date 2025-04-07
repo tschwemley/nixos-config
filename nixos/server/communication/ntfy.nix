205ab4ff3b302c8ab0e2700a73d51b53a218800a
Tyler Schwemley <tjschwem@gmail.com>
2025-04-07 02:23:07 -0400

    update ntfy

--- nixos/server/communication/ntfy.nix
+++ nixos/server/communication/ntfy.nix
@@ -1,6 +1,22 @@
-{
-  services.ntfy-sh = {
-    enable = true;
-    base-url = "ntfy.schwem.io";
+let
+  socketPath = "/run/ntfy/ntfy.sock";
+in {
+  services = {
+    nginx = {
+      upstreams.servers."unix://${socketPath}" = {};
+      virtualHosts."search.schwem.io".locations."/".proxyPass = "http://ntfy";
+    };
+
+    ntfy-sh = {
+      enable = true;
+
+      # REF: https://docs.ntfy.sh/config/#config-options
+      settings = {
+        auth-default-access = "deny-all";
+        base-url = "ntfy.schwem.io";
+        behind-proxy = true;
+        listen-unix = socketPath;
+      };
+    };
   };
 }
