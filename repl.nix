7f78a7bcb89f2b60638b025739ea78ef80bf97a6
Tyler Schwemley <tjschwem@gmail.com>
2025-05-02 15:12:27 -0400

    update repl

--- repl.nix
+++ repl.nix
@@ -2,13 +2,15 @@ let
   curHost = builtins.head (builtins.match "(.*)\n" (builtins.readFile "/etc/hostname"));
   flake = builtins.getFlake (toString ./.);
   self = flake.nixosConfigurations.${curHost};
-  testHosts = import ./test-hosts.nix;
+
+  username = if (self.config.home-manager.users ? schwem) then "schwem" else "root";
+  hm = self.config.home-manager.users.${username};
 in
   {
-    inherit curHost flake self testHosts;
+    inherit curHost flake self hm;
     inherit (flake) lib;
     inherit (self) config pkgs;
     inherit (self.pkgs) system;
   }
+// flake.nixosConfigurations
   // flake
-  // flake.nixosConfigurations
