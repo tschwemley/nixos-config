525d04b3edd49d782756d09d70bbec0d0ad7f332
Tyler Schwemley <tjschwem@gmail.com>
2025-05-06 23:29:59 -0400

    add todo comment config for reference/docs

--- home/programs/neovim/modules/diagnostics/diagnostics.lua
+++ home/programs/neovim/modules/diagnostics/diagnostics.lua
@@ -1,7 +1,16 @@
-require("todo-comments").setup()
 require("trouble").setup()
 
-local trouble = require("trouble.sources.telescope")
+require("todo-comments").setup({
+   keywords = {
+      REF = {
+         icon = "󰈙 ",
+         color = "info",
+         alt = { "DOCS", "DOC", "DOCUMENTATION", "REFERENCE", "LINK", "LINKS" },
+      },
+   },
+})
+
+-- local trouble = require("trouble.sources.telescope")
 
 vim.keymap.set("n", "<leader>ta", "<cmd>Trouble<cr>", { noremap = true })
 vim.keymap.set("n", "<leader>td", "<cmd>TodoTrouble<cr>", { noremap = true })
@@ -10,11 +19,3 @@ vim.keymap.set("n", "<leader>tq", "<cmd>Trouble qflist toggle<cr>", { noremap = 
 
 -- command to automatically watch log
 vim.api.nvim_create_user_command("WatchTail", 'set autoread | au CursorHold * checktime | call feedkeys("G")', {})
--- vim.api.nvim_create_user_command('WatchTail', function()
---     vim.o.autoread = true
---     vim.api.nvim_create_autocmd("CursorHold", {
---         pattern = "*",
---         command = "checktime"
---     })
---     vim.api.nvim_feedkeys("G", 'n', false)
--- end, {})
