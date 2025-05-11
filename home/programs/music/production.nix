a8cdd198f33cf9592ea339655fbd559d967c508b
Tyler Schwemley <tjschwem@gmail.com>
2025-05-10 21:03:06 -0400

    update comments on sonic-pi failure

--- home/programs/music/production.nix
+++ home/programs/music/production.nix
@@ -2,7 +2,9 @@
   home.packages = with pkgs; [
     cardinal
     reaper
-    # BUG: failing build as of 2025-05-02. Uncomment when working again. 
+    # BUG: failing build as of 2025-05-02. Uncomment when working again.
+    # NOTE: seems like a problem with ruby dependencies (specifically fetchMix...)
+    # TODO: try writing a custom package from a checkpoint of a working derivation
     # sonic-pi
     supercollider
     yabridge
