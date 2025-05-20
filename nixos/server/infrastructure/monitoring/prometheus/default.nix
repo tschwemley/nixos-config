5d301b1af6d89b2cac8babe52bef921e7dd7d8c0
Tyler Schwemley <tjschwem@gmail.com>
2025-05-20 01:47:47 -0400

    prometheus test changing way metrics surface

--- nixos/server/infrastructure/monitoring/prometheus/default.nix
+++ nixos/server/infrastructure/monitoring/prometheus/default.nix
@@ -1,22 +1,33 @@
 {config, ...}: let
-  mkNodeExporterScrapeConfig = name: {
-    job_name = "${name}";
-    static_configs = [
-      {
-        targets = ["${name}:${toString config.services.prometheus.exporters.node.port}"];
-      }
-    ];
-  };
+  # mkNodeExporterScrapeConfig = name: {
+  #   job_name = "${name}";
+  #   static_configs = [
+  #     {
+  #       targets = ["${name}:${toString config.services.prometheus.exporters.node.port}"];
+  #     }
+  #   ];
+  # };
+  target = name: "${name}:${toString config.services.prometheus.exporters.node.port}";
 in {
   services.prometheus = {
     enable = true;
 
     scrapeConfigs = [
-      (mkNodeExporterScrapeConfig "articuno")
-      (mkNodeExporterScrapeConfig "zapados")
-      (mkNodeExporterScrapeConfig "moltres")
-      (mkNodeExporterScrapeConfig "flareon")
-      (mkNodeExporterScrapeConfig "jolteon")
+      {
+        job_name = "node-details";
+        targets = [
+          (target "articuno")
+          (target "zapados")
+          (target "moltres")
+          (target "flareon")
+          (target "jolteon")
+        ];
+      }
+      # (mkNodeExporterScrapeConfig "articuno")
+      # (mkNodeExporterScrapeConfig "zapados")
+      # (mkNodeExporterScrapeConfig "moltres")
+      # (mkNodeExporterScrapeConfig "flareon")
+      # (mkNodeExporterScrapeConfig "jolteon")
     ];
   };
 }
---
+++ nixos/server/services/taskchampion-sync-server.nix
@@ -0,0 +1,5 @@
+{
+  services.taskchampion-sync-server = {
+    enable = true;
+  };
+}
