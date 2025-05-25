8280822b35ed08e701b6a8ec54eb73beaa7bff0f
Tyler Schwemley <tjschwem@gmail.com>
2025-05-24 21:22:44 -0400

    fix ethtool bin path

--- nixos/network/tailscale.nix
+++ nixos/network/tailscale.nix
@@ -15,7 +15,7 @@
         rules."50-tailscale" = {
           onState = ["routable"];
           script = ''
-            ${lib.getExe ethtool} -K eth0 rx-udp-gro-forwarding on rx-gro-list off
+            ${lib.getExe pkgs.ethtool} -K eth0 rx-udp-gro-forwarding on rx-gro-list off
           '';
         };
       };
