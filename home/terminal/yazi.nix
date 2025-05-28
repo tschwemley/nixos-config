7f274415a0244b554683251cf4c5e2c57605e0f1
Tyler Schwemley <tjschwem@gmail.com>
2025-05-28 03:25:00 -0400

    minor cleanup

--- home/terminal/yazi.nix
+++ home/terminal/yazi.nix
@@ -8,11 +8,6 @@
     poppler-utils # pdf preview
   ];
 
-  nix.settings = {
-    extra-substituters = ["https://yazi.cachix.org"];
-    extra-trusted-public-keys = ["yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="];
-  };
-
   programs.yazi = {
     enable = true;
 
