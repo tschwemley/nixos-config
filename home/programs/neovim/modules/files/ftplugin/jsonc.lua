5237d606cd906a3aead5f7ee3e0b66832d2a47ec
Tyler Schwemley <tjschwem@gmail.com>
2025-05-30 05:39:37 -0400

    add jsonc filetype

---
+++ home/programs/neovim/modules/files/ftplugin/jsonc.lua
@@ -0,0 +1,21 @@
+require("schwem.helpers").set_tabs(2)
+
+local function prettifyJSON()
+   vim.cmd("%!jq .")
+end
+
+local function minifyJSON()
+   vim.cmd("%!jq -r tostring")
+end
+
+local function toggleFormat()
+   if vim.api.nvim_buf_line_count(0) == 1 then
+      prettifyJSON()
+   else
+      minifyJSON()
+   end
+end
+
+vim.keymap.set("n", "<leader>lf", toggleFormat, { buffer = true, noremap = true })
+vim.keymap.set("n", "<leader>lm", minifyJSON, { buffer = true, noremap = true })
+vim.keymap.set("n", "<leader>lp", prettifyJSON, { buffer = true, noremap = true })
