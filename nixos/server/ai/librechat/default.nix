8553fa712f191ba8233e08d0c9801d9d59324e31
Tyler Schwemley <tjschwem@gmail.com>
2025-06-08 04:21:07 -0400

    enable mongodb as part of librechat config

--- nixos/server/ai/librechat/default.nix
+++ nixos/server/ai/librechat/default.nix
@@ -1,40 +1,58 @@
 {
   self,
   config,
+  pkgs,
   ...
 }: {
   imports = [./module.nix];
 
-  services.librechat = {
-    enable = true;
+  services = {
+    librechat = {
+      enable = true;
 
-    credentials = let
-      secretPath = config.sops.secrets.librechat.path;
-    in {
-      CREDS_KEY = secretPath;
-      CREDS_IV = secretPath;
-      JWT_KEY = secretPath;
-      JWT_REFRESH_KEY = secretPath;
-      MEILI_MASTER_KEY = secretPath;
+      credentials = let
+        secretPath = config.sops.secrets.librechat.path;
+      in {
+        CREDS_KEY = secretPath;
+        CREDS_IV = secretPath;
+        JWT_KEY = secretPath;
+        JWT_REFRESH_KEY = secretPath;
+        MEILI_MASTER_KEY = secretPath;
+      };
+
+      env = {
+        ALLOW_REGISTRATION = "false";
+        HOST = "127.0.0.1";
+      };
+
+      # NOTE: settings is free-form nix attribute set that will be converted to librechat.yaml
+      settings = {};
     };
 
-    env = {
-      ALLOW_REGISTRATION = "false";
-      HOST = "127.0.0.1";
+    mongodb = {
+      enable = true;
+      initialRootPasswordFile = config.sops.secrets.librechat-mongo.path;
+      package = pkgs.mongodb-ce;
     };
-
-    # NOTE: settings is free-form nix attribute set that will be converted to librechat.yaml
-    settings = {};
   };
 
-  sops.secrets = {
+  sops.secrets = let
+    owner = config.services.librechat.user;
+    group = config.services.librechat.user;
+    mode = "0400";
+  in {
     librechat = {
-      owner = config.services.librechat.user;
-      group = config.services.librechat.user;
-      mode = "0400";
+      inherit group owner mode;
 
       format = "dotenv";
       sopsFile = self.lib.secret "server" "librechat-creds.env";
+    };
+
+    librechat-mongo = {
+      inherit group owner mode;
+
+      key = "mongo_pw";
+      sopsFile = self.lib.secret "server" "librechat.yaml";
     };
   };
 }
