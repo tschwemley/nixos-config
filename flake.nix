b2beba322bf03993febc3d847a9f9fd33371f848
Tyler Schwemley <tjschwem@gmail.com>
2025-06-16 02:24:18 -0400

    switch to using spicetify

--- flake.nix
+++ flake.nix
@@ -139,6 +139,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    spicetify-nix = {
+      url = "github:Gerg-L/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     zen-browser = {
       url = "github:youwen5/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
--- home/programs/media/spotify.nix
+++ home/programs/media/spotify.nix
@@ -1,6 +1,46 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    spicetify-cli
-    spotify
-  ];
+{
+  self,
+  pkgs,
+  ...
+}: {
+  imports = [self.inputs.spicetify-nix.homeManagerModules.spicetify];
+
+  # home.packages = with pkgs; [
+  #   librespot
+  #   mopidy
+  #   # spicetify-cli
+  #   spotify
+  #   spot
+  # ];
+
+  # REF: https://gerg-l.github.io/spicetify-nix/
+  programs.spicetify = let
+    # For Flakeless:
+    # spicePkgs = spicetify-nix.packages;
+    # With flakes:
+    spicePkgs = self.inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.system};
+  in {
+    enable = true;
+    enabledExtensions = with spicePkgs.extensions; [
+      # adblockify
+      addToQueueTop
+      allOfArtist
+      bestMoment
+      betterGenres
+      copyToClipboard
+      hidePodcasts
+      keyboardShortcut # REF: https://spicetify.app/docs/advanced-usage/extensions/#keyboard-shortcut
+      phraseToPlaylist
+      powerBar
+      queueTime
+      sectionMarker
+      shuffle # shuffle+ (special characters are sanitized out of extension names)
+      skipStats
+      songStats
+      # volumeProfiles
+      wikify
+      writeify
+    ];
+    theme = spicePkgs.themes.text;
+  };
 }
