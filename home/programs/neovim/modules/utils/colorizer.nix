21b7ae08c1a2e42cecb3863ffc7a7b6b2fb4a91c
Tyler Schwemley <tjschwem@gmail.com>
2025-07-25 01:25:13 -0400

    add colorizer plugin neovim

--- home/programs/neovim/modules/utils/colorizer.nix
+++ home/programs/neovim/modules/utils/colorizer.nix
@@ -5,6 +5,16 @@ with vimPlugins; {
   config =
     # lua
     ''
-      require("colorizer").setup()
+      require("colorizer").setup({
+        names_custom = {
+          aqua = "#8ec07c",
+          blue = "#83a598",
+          green = "#b8bb26",
+          orange = "#fe8019",
+          purple = "#d3869b",
+          red = "#fb4934",
+          yellow = "#fabd2f",
+        },
+      })
     '';
 }
