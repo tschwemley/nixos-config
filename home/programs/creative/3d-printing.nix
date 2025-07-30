f4ac661689aa2aad21568baf142dc09fa69f5a81
Tyler Schwemley <tjschwem@gmail.com>
2025-07-29 23:29:25 -0400

    modify printing modules

--- home/programs/creative/3d-printing.nix
+++ home/programs/creative/3d-printing.nix
@@ -1,6 +1,3 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    # bambu-studio
-    orca-slicer
-  ];
+  home.packages = [pkgs.orca-slicer];
 }
--- nixos/services/printing.nix
+++ nixos/services/printing.nix
@@ -1,5 +1,20 @@
 {
-  services.printing = {
-    enable = true;
+  hardware.printers = {
+    ensureDefaultPrinter = "Brother_HL-L2340D";
+    ensurePrinters = [
+      {
+        deviceUri = "ipp://192.168.1.33/ipp";
+        location = "home";
+        name = "Brother_HL-L2340D";
+        model = "everywhere";
+      }
+    ];
+  };
+
+  services.printing.enable = true;
+
+  systemd.services.ensure-printers = {
+    after = ["NetworkManager-wait-online"];
+    requires = ["NetworkManager-wait-online"];
   };
 }
