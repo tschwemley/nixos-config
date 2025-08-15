02d4485d5bea9d0031d6c4778295a8949d47dfd9
Tyler Schwemley <tjschwem@gmail.com>
2025-08-15 04:14:15 -0400

    remove theme line

--- home/theme/default.nix
+++ home/theme/default.nix
@@ -1,10 +1,10 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   home.pointerCursor = {
     package = pkgs.bibata-cursors;
     name = "Bibata-Modern-Classic";
     size = 24;
     gtk.enable = true;
     hyprcursor.enable = true;
-    # x11.enable = true; # TODO: remvoe this after testing the config a bit
   };
 }
