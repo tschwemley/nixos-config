c1076a053761186abc181173a667ecb42a64119b
Tyler Schwemley <tjschwem@gmail.com>
2025-09-21 23:43:04 -0400

    update ssh

--- home/xdg/ssh/default.nix
+++ home/xdg/ssh/default.nix
@@ -1,5 +1,20 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
+
+    # NOTE: these are the settings that used to be enabled by default (now deprecated)
+    matchBlocks."*" = {
+      addKeysToAgent = "no";
+      compression = false;
+      controlMaster = "no";
+      controlPath = "~/.ssh/master-%r@%n:%p";
+      controlPersist = "no";
+      forwardAgent = false;
+      hashKnownHosts = false;
+      serverAliveCountMax = 3;
+      serverAliveInterval = 0;
+      userKnownHostsFile = "~/.ssh/known_hosts";
+    };
   };
 }
