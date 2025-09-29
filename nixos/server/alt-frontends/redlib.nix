ba474a60434c263553a64e6373bc7d765baf5322
Tyler Schwemley <tjschwem@gmail.com>
2025-09-29 03:41:02 -0400

    test change to anubis

--- nixos/server/alt-frontends/redlib.nix
+++ nixos/server/alt-frontends/redlib.nix
@@ -11,8 +11,8 @@ in
   services = {
     nginx.virtualHosts."reddit.schwem.io" = {
       locations."/" = {
-        # proxyPass = "http://${address}:${self.lib.port-map.redlib}";
-        proxyPass = "http://${address}${config.services.anubis.instances.redlib.settings.BIND}";
+        # proxyPass = "http://${address}${config.services.anubis.instances.redlib.settings.BIND}";
+        proxyPass = "http://unix:${address}${config.services.anubis.instances.redlib.settings.BIND}";
       };
     };
 
@@ -21,6 +21,8 @@ in
 
       inherit address;
       port = lib.toInt self.lib.port-map.redlib;
+
+      # REF: https://github.com/redlib-org/redlib?tab=readme-ov-file#configuration
       settings = {
         REDLIB_ROBOTS_DISABLE_INDEXING = "on";
         REDLIB_DEFAULT_THEME = "gruvboxdark";
@@ -30,23 +32,4 @@ in
       };
     };
   };
-
-  # TODO: this should now be resolved... delete after confirming
-  # NOTE: using official container build instead of nix pkg (or custom package) because:
-  #         1. redlib is prone to breaking changes from reddit
-  #         2. it's updated the most regularly by the maintainer
-  # virtualisation.oci-containers.containers."redlib" = {
-  #   autoStart = true;
-  #   image = "quay.io/redlib/redlib:latest";
-  #   ports = ["${listenAddress}:${self.lib.port-map.redlib}:8080"];
-  #   extraOptions = ["--pull=always"];
-  #   environment = {
-  #     # see: https://github.com/redlib-org/redlib?tab=readme-ov-file#configuration
-  #     REDLIB_ROBOTS_DISABLE_INDEXING = "on";
-  #     REDLIB_DEFAULT_THEME = "gruvboxdark";
-  #     REDLIB_DEFAULT_SHOW_NSFW = "on";
-  #     REDLIB_DEFAULT_USE_HLS = "on";
-  #     REDLIB_DEFAULT_HIDE_HLS_NOTIFICATION = "on";
-  #   };
-  # };
 }
--- nixos/server/security/anubis.nix
+++ nixos/server/security/anubis.nix
@@ -4,11 +4,13 @@ let
 in
 {
   services.anubis = {
+    defaultOption.group = "nginx";
+
     instances = {
       redlib = {
         settings = {
-          BIND = ":${self.lib.port-map.anubis}";
-          BIND_NETWORK = "tcp";
+          # BIND = ":${self.lib.port-map.anubis}";
+          # BIND_NETWORK = "tcp";
           TARGET = "http://${address}:${self.lib.port-map.redlib}";
         };
       };
