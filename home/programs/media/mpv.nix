a1e985ee28ef7cdd4c36e39f049402bec7dde157
Tyler Schwemley <tjschwem@gmail.com>
2025-10-08 21:14:14 -0400

    add script to mpv

--- home/programs/media/mpv.nix
+++ home/programs/media/mpv.nix
@@ -1,11 +1,29 @@
+{ pkgs, ... }:
 {
   programs.mpv = {
     enable = true;
+
     config = ''
       hwdec=auto-safe
       vo=gpu
       profile=gpu-hq
       gpu-context=wayland
     '';
+
+    scripts =
+      let
+        imageViewer = with pkgs.mpvScripts.mpv-image-viewer; [
+          detect-image
+          equalizer
+          freeze-window
+          image-positioning
+          minimap
+          ruler
+          status-line
+        ];
+      in
+      [ ] ++ imageViewer;
   };
 }
+
+# REF: https://mpv.io/manual/master/
