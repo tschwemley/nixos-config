49c0648b99a1313453dfe5d9132c3f8edc2e77a8
Tyler Schwemley <tjschwem@gmail.com>
2025-10-19 04:44:24 -0400

    fix indentation of rendered script

--- packages/scripts/search-nerdfonts.nix
+++ packages/scripts/search-nerdfonts.nix
@@ -1,15 +1,15 @@
-{writeShellScriptBin}:
+{ writeShellScriptBin }:
 writeShellScriptBin "search-nf" /* bash */ ''
-	pattern="nf-\w*-.*?''${1}"
-	res=$(curl -sL 'https://nerdfonts.com/cheat-sheet' \
-			 | rg "$pattern" \
-			 | awk -F'"' '{printf "| %c | %s |\n", strtonum("0x" $4), $2}'
-	)
+  pattern="nf-\w*-.*?''${1}"
+  res=$(curl -sL 'https://nerdfonts.com/cheat-sheet' \
+       | rg "$pattern" \
+       | awk -F'"' '{printf "| %c | %s |\n", strtonum("0x" $4), $2}'
+  )
 
 
-	cat <<MARKDOWN
-	| Icon | NF Class  |
-	| :--- | :-------  |
-	$res
-	MARKDOWN
+  cat <<MARKDOWN
+  | Icon | NF Class  |
+  | :--- | :-------  |
+  $res
+  MARKDOWN
 ''
