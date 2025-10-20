b1430f9b0da0e9688a5b0ee6f7bfcf9d05704c2b
Tyler Schwemley <tjschwem@gmail.com>
2025-10-20 00:16:37 -0400

    Configuration limit moved to proper location

--- nixos/system/boot/systemd.nix
+++ nixos/system/boot/systemd.nix
@@ -2,14 +2,14 @@
   imports = [ ./. ];
 
   boot = {
-    configurationLimit = 10;
-
     initrd = {
       systemd.enable = true;
     };
 
     loader.systemd-boot = {
       enable = true;
+
+      configurationLimit = 10;
       editor = false; # leaving true allows for root access to be gained via passing kernel param
     };
   };
