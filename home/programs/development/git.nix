83c3d43132d004f08a231618ad596fc0bac20048
Tyler Schwemley <tjschwem@gmail.com>
2025-10-25 05:14:27 -0400

    fix git settings for upcoming deprecation

--- home/programs/development/git.nix
+++ home/programs/development/git.nix
@@ -1,30 +1,30 @@
 { pkgs, ... }:
-let
-  aliases = {
-    b = "branch";
-    bd = "branch --delete";
-    cb = "checkout --branch";
-    co = "checkout";
-    cm = "!git add . && git commit -am";
-    s = "status";
-  };
-in
 {
   home.packages = with pkgs; [ gh ];
   programs.git = {
-    inherit aliases;
 
     enable = true;
     lfs.enable = true;
-    userEmail = "tjschwem@gmail.com";
-    userName = "Tyler Schwemley";
 
-    extraConfig = {
+    settings = {
       init.defaultBranch = "main";
       pull.rebase = true;
+      userEmail = "tjschwem@gmail.com";
+      userName = "Tyler Schwemley";
+
+      alias = {
+        b = "branch";
+        bd = "branch --delete";
+        cb = "checkout --branch";
+        co = "checkout";
+        cm = "!git add . && git commit -am";
+        s = "status";
+      };
+
       core = {
         whitespace = "trailing-space,space-before-tab";
       };
+
       url = {
         "ssh://forgejo" = {
           insteadOf = "https://git.schwem.io";
