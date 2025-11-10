a64f17076facebabb24259d57e284ebd873de217
Tyler Schwemley <tjschwem@gmail.com>
2025-11-10 15:03:59 -0500

    update nix lsp

--- home/programs/neovim/modules/lsp/lspconfigs/nixd.lua
+++ home/programs/neovim/modules/lsp/lspconfigs/nixd.lua
@@ -1,22 +1,25 @@
 local getFlakeExpr = '(builtins.getFlake "' .. os.getenv("HOME") .. '/nixos-config")'
 local host = vim.fn.hostname()
-local user = vim.fn.getenv("USER")
+-- local user = vim.fn.getenv("USER")
 
 return {
-   settings = {
-      nixd = {
-         nixpkgs = {
-            expr = "import " .. getFlakeExpr .. ".inputs.nixpkgs { }",
-         },
-         options = {
-            nixos = {
-               expr = getFlakeExpr .. ".nixosConfigurations." .. host .. ".options",
-            },
-            -- home_manager = {
-            --    expr = getFlakeExpr .. ".homeConfigurations." .. user .. ".options",
-            -- },
-         },
-         ["semantic-tokens"] = true,
-      },
-   },
+	settings = {
+		nixd = {
+			nixpkgs = {
+				expr = "import " .. getFlakeExpr .. ".inputs.nixpkgs { }",
+			},
+			options = {
+				nixos = {
+					expr = getFlakeExpr .. ".nixosConfigurations." .. host .. ".options",
+				},
+				home_manager = {
+					expr = getFlakeExpr
+						.. ".nixosConfigurations."
+						.. host
+						.. "options.home-manager.users.type.getSubOptions []",
+				},
+			},
+			["semantic-tokens"] = true,
+		},
+	},
 }
