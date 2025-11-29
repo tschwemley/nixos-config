f723d4997076ad510ff0001e2b0bbdb9c7f86a5e
Tyler Schwemley <tjschwem@gmail.com>
2025-11-28 21:01:02 -0500

    update khal

--- home/programs/productivity/khal.nix
+++ home/programs/productivity/khal.nix
@@ -1,5 +1,15 @@
 {
   programs.khal = {
     enable = true;
+    settings = {
+      personal = {
+        path = "~/.calendars/personal";
+      };
+
+      work = {
+        path = "~/.calendars/work";
+        readonly = true;
+      };
+    };
   };
 }
