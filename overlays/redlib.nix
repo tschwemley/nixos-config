332e584f8610c1e9126e55d0dbafcc5febce80ee
Tyler Schwemley <tjschwem@gmail.com>
2025-12-02 01:42:01 -0500

    add patch to redlib and re-enable overlay

--- overlays/redlib.nix
+++ overlays/redlib.nix
@@ -1,1 +1,14 @@
-self: (_: prev: { redlib = self.inputs.redlib.packages.${prev.system}.default; })
+self:
+(_: prev: {
+  # NOTE: this is in case of want/need to revert back to using flake instead of nixpkgs version
+  # redlib = self.inputs.redlib.packages.${prev.system}.default;
+  redlib = prev.redlib.overrideAttrs {
+    patches = [
+      (prev.fetchpatch {
+        url = "https://patch-diff.githubusercontent.com/raw/redlib-org/redlib/pull/507.diff";
+        hash = "sha256-D4/RynxxutHexyepXGs80C9qncQ7/p/s+S+g5BmZx/Y=";
+      })
+    ]
+    ++ prev.redlib.patches;
+  };
+})
