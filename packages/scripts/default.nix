f52b1fffec0f973326e2c4d318ac9ee5c9b59cd9
Tyler Schwemley <tjschwem@gmail.com>
2026-01-20 02:45:45 -0500

    remove find desktop entries script

--- packages/scripts/default.nix
+++ packages/scripts/default.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }:
 {
   buildHost = pkgs.callPackage ./build-host.nix { };
-  findDesktopEntries = pkgs.callPackage ./find-desktop-entries.nix { };
   rotateSecrets = pkgs.callPackage ./rotate-secrets.nix { };
 }
 
