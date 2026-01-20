dc8b470326e708c2d942ce599f86b7d7d5e8f950
Tyler Schwemley <tjschwem@gmail.com>
2026-01-20 03:50:05 -0500

    remove hyprland services in first step towards removal

--- home/programs/wayland/hyprland/services/hyprpaper.nix
+++
@@ -1,9 +0,0 @@
-{
-  services.hyprpaper = {
-    enable = true;
-    settings = {
-      preload = [ "~/nixos-config/home/theme/gruvbox-castle.png" ];
-      wallpaper = [ ", ~/nixos-config/home/theme/gruvbox-castle.png" ];
-    };
-  };
-}
