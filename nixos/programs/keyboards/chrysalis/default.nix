aab9c7c5c36ef0642b1ecfd21e6a3c1f26e80c17
Tyler Schwemley <tjschwem@gmail.com>
2026-01-20 02:38:37 -0500

    update keyboard config

--- nixos/programs/keyboards/chrysalis/default.nix
+++
@@ -1,6 +0,0 @@
-{pkgs, ...}: {
-  environment.systemPackages = [pkgs.chrysalis];
-
-  # add req. udev rules so non-root can make changes
-  services.udev.packages = [pkgs.chrysalis];
-}
--- nixos/programs/keyboards/default.nix
+++ nixos/programs/keyboards/default.nix
@@ -1,27 +1,13 @@
 { pkgs, ... }:
-let
-  packages = with pkgs; [
-  ];
-in
 {
   environment.systemPackages = with pkgs; [
-    chrysalis
     qmk
     qmk_hid
+
     # open source gui and qmk fork -- udev built along with package
     vial
   ];
 
   hardware.keyboard.qmk.enable = true;
-
-  # TODO: I want to test this to see if necessary to include when using nixpkgs provided package
-  # that contains udev rules -- IF SO: UNCOMMENT
-  # services.udev.packages = with pkgs; [
-  #   chrysalis
-  #   qmk
-  #   vial
-  # ];
-
-  # add req. udev rules so non-root can make changes
-  # services.udev.packages = [pkgs.chrysalis];
+  programs.chrysalis.enable = true;
 }
