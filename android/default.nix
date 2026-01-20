e25f9376b275dd9702e68a3695a431157dc5efe4
Tyler Schwemley <tjschwem@gmail.com>
2026-01-20 02:22:23 -0500

    fix typo

--- android/default.nix
+++ android/default.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = [ pkgs.andorid-tools ];
+  environment.systemPackages = [ pkgs.android-tools ];
   nixpkgs.config.android_sdk.accept_license = true;
   virtualisation.waydroid.enable = true;
 }
