713ec7de1b6a2c7ef6f6557357d44570d34f2765
Tyler Schwemley <tjschwem@gmail.com>
2026-01-26 03:17:01 -0500

    add kdeconnect port ranges

--- nixos/programs/kdeconnect.nix
+++ nixos/programs/kdeconnect.nix
@@ -1,3 +1,13 @@
 {
   programs.kdeconnect.enable = true;
+
+  networking.firewall = rec {
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
 }
