4e09967f67043ca07ffe24df06d8a92a76ba23b9
Tyler Schwemley <tjschwem@gmail.com>
2026-01-26 03:15:33 -0500

    alias wcalc for sensible args

--- home/programs/wcalc.nix
+++ home/programs/wcalc.nix
@@ -1,3 +1,11 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [wcalc];
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  home.packages = with pkgs; [ wcalc ];
+
+  programs.zsh.shellAliases.wcalc = lib.mkIf config.programs.zsh.enable "wcalc -EE -P4";
 }
