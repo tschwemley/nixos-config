ccdcd7766ca141516e8764cd687626f975aa30cf
Tyler Schwemley <tjschwem@gmail.com>
2026-01-26 01:44:10 -0500

    simplify bluetooth config

--- nixos/hardware/audio/bluetooth.nix
+++ nixos/hardware/audio/bluetooth.nix
@@ -1,15 +1,16 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    bluetuith
-  ];
-
   hardware.bluetooth = {
     enable = true;
     package = pkgs.bluez-experimental;
     powerOnBoot = true;
 
+    settings = {
+      General.Experimental = true;
+      Policy.AutoEnable = true;
+    };
   };
 
+  # Enables blueman service which provides blueman-applet and blueman-manager.
   services.blueman.enable = true;
 }
