93ef2e8c78dfd79b7991a3950910b0a625d98148
Tyler Schwemley <tjschwem@gmail.com>
2026-01-26 03:19:06 -0500

    add default config for lan mouse

--- home/programs/lan-mouse.nix
+++ home/programs/lan-mouse.nix
@@ -3,4 +3,21 @@
   imports = [ self.inputs.lan-mouse.homeManagerModules.default ];
 
   programs.lan-mouse.enable = true;
+
+  xdg.configFile."lan-mouse/config.toml".text = /* toml */ ''
+    # configure release bind
+    # release_bind = [ "KeyA", "KeyS", "KeyD", "KeyF" ]
+
+    # optional port (defaults to 4242)
+    port = 4242
+
+    # define a client on the right side with host name "iridium"
+    [[clients]]
+    activate_on_startup = true
+    hostname = "192.168.1.103"
+    ips = ["192.168.1.103"]
+
+    # position (left | right | top | bottom)
+    position = "right"
+  '';
 }
