2139ab1fa4cf31c2942f474decab0b653e7758da
Tyler Schwemley <tjschwem@gmail.com>
2026-01-26 01:44:51 -0500

    wip python template update

--- templates/python/flake.nix
+++ templates/python/flake.nix
@@ -6,41 +6,44 @@
     systems.url = "github:nix-systems/default-linux";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    systems,
-    ...
-  }: let
-    inherit (nixpkgs) lib;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      ...
+    }:
+    let
+      inherit (nixpkgs) lib;
 
-    pkgsForSystem = lib.genAttrs (import systems) (system: import nixpkgs {inherit system;});
-    eachSystem = fn: lib.genAttrs (import systems) (system: fn pkgsForSystem.${system});
-  in {
-    inherit lib;
+      pkgsForSystem = lib.genAttrs (import systems) (system: import nixpkgs { inherit system; });
+      eachSystem = fn: lib.genAttrs (import systems) (system: fn pkgsForSystem.${system});
+    in
+    {
+      inherit lib;
 
-    devShells = eachSystem (pkgs: {
-      default = pkgs.mkShell {
-        buildInputs = [
-          self.packages.${builtins.currentSystem}.default
-          # (pkgs.python313.withPackages (pythonPkgs:
-          #   with pythonPkgs; [
-          #     daphne
-          #     django_5
-          #     playwright
-          #     wand
-          #     python-dotenv
-          #   ]))
-          # script to generate gemset.nix
-          # (pkgs.writeShellScriptBin "gem-to-nix" ''
-          # ${pkgs.bundix}/bin/bundix --gemfile ${gemDir}/Gemfile --lockfile ${gemDir}/Gemfile.lock
-          # '')
-        ];
-      };
-    });
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            self.packages.${pkgs.stdenv.hostPlatform.system}.default
+            # (pkgs.python313.withPackages (pythonPkgs:
+            #   with pythonPkgs; [
+            #     daphne
+            #     django_5
+            #     playwright
+            #     wand
+            #     python-dotenv
+            #   ]))
+            # script to generate gemset.nix
+            # (pkgs.writeShellScriptBin "gem-to-nix" ''
+            # ${pkgs.bundix}/bin/bundix --gemfile ${gemDir}/Gemfile --lockfile ${gemDir}/Gemfile.lock
+            # '')
+          ];
+        };
+      });
 
-    packages = eachSystem (pkgs: {
-      default = pkgs.python313.withPackages (pythonPkgs: with pythonPkgs; []);
-    });
-  };
+      packages = eachSystem (pkgs: {
+        default = pkgs.python313.withPackages (pythonPkgs: with pythonPkgs; [ ]);
+      });
+    };
 }
