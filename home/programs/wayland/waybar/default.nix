51f7ef0014183db0539be3e1e189ace3e843e223
Tyler Schwemley <tjschwem@gmail.com>
2026-02-04 00:58:49 -0500

    add battery info to waybar

--- home/programs/wayland/waybar/default.nix
+++ home/programs/wayland/waybar/default.nix
@@ -27,9 +27,27 @@
           "pulseaudio/slider"
           "gamemode"
           "tray"
+          "battery"
           "custom/weather"
           "clock"
         ];
+
+        battery = {
+          interval = 60;
+          states = {
+            warning = 30;
+            critical = 15;
+          };
+          format = "{capacity}% {icon}";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+          max-length = 25;
+        };
 
         clock = {
           actions = {
