8fa90315a0a9253705a7d7cfa19ce61a2a8bb52a
Tyler Schwemley <tjschwem@gmail.com>
2026-02-12 23:46:40 -0500

    resolve comments

--- home/programs/music/production.nix
+++ home/programs/music/production.nix
@@ -1,6 +1,5 @@
 { pkgs, ... }:
 {
-  # TODO: split this up further than just "production"
   home.packages = with pkgs; [
     cardinal
     # carla
@@ -10,12 +9,8 @@
     # yabridge
     # yabridgectl
 
-    tuxguitar
-
-    # TODO: also look into similar projects e.g.
-    #         [proteus](https://guitarml.com/#products)
-    #         [amperium nnm](https://www.st-rock.com/vst/)
     neural-amp-modeler-lv2
+    tuxguitar
   ];
 
   # home.file."".source = "${pkgs.neural-amp-modeler-lv2}/lib/lv2/neural_amp_modeler.lv2";
