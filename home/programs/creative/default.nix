2bb407d63f8e14dd5199423a7afa77b6181a6284
Tyler Schwemley <tjschwem@gmail.com>
2026-03-07 12:41:32 -0500

    disable krita due to boost issues

--- home/programs/creative/default.nix
+++ home/programs/creative/default.nix
@@ -1,9 +1,10 @@
 {
   imports = [
+    # TODO: uncomment cad/krita once build issues go away (2026-02-27)
     ./cad.nix
     ./gimp.nix
     ./inkscape.nix
-    ./krita.nix
+    # ./krita.nix
     ./orca-slicer.nix
 
     # ./aseprite.nix
