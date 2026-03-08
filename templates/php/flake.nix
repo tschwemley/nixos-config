db7cdc12acf48ef900a9f19679b0c9a1a9215087
Tyler Schwemley <tjschwem@gmail.com>
2026-03-07 20:28:46 -0500

    update php template

--- templates/php/flake.nix
+++ templates/php/flake.nix
@@ -3,49 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    sops.url = "github:Mic92/sops-nix";
   };
 
   outputs =
-    inputs@{
-      self,
-      flake-parts,
-      nixpkgs,
-      sops,
-      ...
-    }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-      ];
+    inputs@{ nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
 
-      perSystem =
-        {
-          self',
-          config,
-          lib,
-          pkgs,
-          system,
-          ...
-        }:
-        {
-          # makes pkgs available to all perSystem functions
-          _module.args.pkgs = import nixpkgs {
-            inherit system;
-            config.allowUnfree = true;
-          };
-
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              age
-              php
-              phpunit
-              php82Packages.composer
-              php82Packages.psysh
-              pkgs.sops
-            ];
-          };
-        };
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          php
+          phpunit
+          psysh
+        ];
+      };
     };
 }
