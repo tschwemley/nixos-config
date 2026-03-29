29b4c3ddc0c235a68b7482bd230d56dce27e91d9
Tyler Schwemley <tjschwem@gmail.com>
2026-03-29 05:53:17 -0400

    tidy xdg/default.nix

--- home/xdg/default.nix
+++ home/xdg/default.nix
@@ -2,26 +2,13 @@
   config,
   pkgs,
   ...
-}: {
-  imports = [./mimeapps.nix];
+}:
+{
+  imports = [ ./mimeapps.nix ];
 
   xdg = {
     enable = true;
     cacheHome = config.home.homeDirectory + "/.local/cache";
-
-    # TODO: reasses if this should be enabled or not (or is nixos portal fine)
-    #
-    # portal = {
-    #   enable = true;
-    #
-    #   config = {
-    #     common.default = ["gtk"];
-    #     hyprland.default = ["gtk" "hyprland"];
-    #   };
-    #
-    #   extraPortals = with pkgs; [xdg-desktop-portal-gtk];
-    #   xdgOpenUsePortal = true;
-    # };
 
     userDirs = {
       enable = true;
@@ -33,12 +20,6 @@
   };
 
   home.packages = [
-    # TODO: is necessary?
-    #
-    # used by `gio open` and xdp-gtk
-    # (pkgs.writeShellScriptBin "xdg-terminal-exec" ''
-    #   foot "$@"
-    # '')
     pkgs.xdg-utils
   ];
 }
