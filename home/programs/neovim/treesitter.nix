06d9ca23fd76bec8ab85644db254ca0c385f9130
Tyler Schwemley <tjschwem@gmail.com>
2026-03-29 02:56:06 -0400

    update treesitter neovim

--- home/programs/neovim/treesitter.nix
+++ home/programs/neovim/treesitter.nix
@@ -1,59 +1,11 @@
 { pkgs, ... }:
-let
-  nvim-treesitter = pkgs.vimPlugins.nvim-treesitter.withPlugins (
-    parser: with parser; [
-      astro
-      awk
-      bash
-      cmake
-      comment # see: https://github.com/stsewd/tree-sitter-comment
-      css
-      csv
-      diff
-      go
-      gotmpl
-      gomod
-      gosum
-      html
-      http
-      ini
-      javascript
-      jq
-      json
-      kdl
-      lua
-      luadoc
-      make
-      markdown
-      markdown_inline
-      nix
-      php
-      python
-      regex
-      sql
-      ssh_config
-      superhtml
-      templ
-      toml
-      tsx
-      typescript
-      vim
-      vimdoc
-      xml
-      yaml
-      zig
-    ]
-  );
-in
 {
-  # home.packages = [ pkgs.tree-sitter ];
   programs.neovim = with pkgs; {
     extraPackages = [ tree-sitter ];
 
-    plugins = [
-      vimPlugins.nvim-treesitter.withAllGrammars
-      # nvim-treesitter
-      # pkgs.vimPlugins.nvim-treesitter-textobjects
+    plugins = with pkgs.vimPlugins; [
+      nvim-treesitter.withAllGrammars
+      nvim-treesitter-textobjects
     ];
   };
 }
