a33edcdb2819b91d0b3d91c7eced778c3c0d16a6
Tyler Schwemley <tjschwem@gmail.com>
2026-04-07 01:16:55 -0400

    minor zen settings tweaks

--- home/programs/browsers/zen/profiles/default/settings.nix
+++ home/programs/browsers/zen/profiles/default/settings.nix
@@ -3,5 +3,13 @@
     startup.homepage = "https://schwem.io";
   };
 
-  layout.css.prefers-color-scheme.content-override = 3;
+  layout = {
+    css.prefers-color-scheme.content-override = 3;
+  };
+
+  zen = {
+    theme = {
+      use-system-colors = true;
+    };
+  };
 }
