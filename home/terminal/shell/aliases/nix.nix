2072bdb39044c78c9790e17a5c39c25bdaeab49f
Tyler Schwemley <tjschwem@gmail.com>
2026-04-13 02:41:03 -0400

    update nix collect garbage alias

--- home/terminal/shell/aliases/nix.nix
+++ home/terminal/shell/aliases/nix.nix
@@ -2,7 +2,7 @@
   cdnix = "cd ~/nixos-config";
   hms = "nix run home-manager/master -- switch --flake ";
   nfc = "nix flake check";
-  ngc = "sudo nix-collect-garbage --delete-older-than 3d";
+  ngc = "sudo nix-collect-garbage --delete-older-than 1d";
   nixconf = "cd ~/nixos-config";
   nrbs = "sudo nixos-rebuild switch --flake .#$HOST |& nom";
   systemd2nix = "nix run github:DavHau/systemd2nix";
