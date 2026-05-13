d10f694b07b9c2be4e270cb12ba5f3d1bc57f74e
Tyler Schwemley <tjschwem@gmail.com>
2026-05-13 19:13:27 -0400

    zen set as default

--- home/programs/browsers/zen/default.nix
+++ home/programs/browsers/zen/default.nix
@@ -1,11 +1,16 @@
-{ self, pkgs, ... }:
+{
+  self,
+  lib,
+  pkgs,
+  ...
+}:
 {
   imports = [ self.inputs.zen-browser.homeModules.default ];
 
   programs.zen-browser = {
     enable = true;
-
     nativeMessagingHosts = [ pkgs.firefoxpwa ];
+    setAsDefaultBrowser = true;
 
     policies = import ./policies.nix;
     profiles = import ./profiles pkgs;
