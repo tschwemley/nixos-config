95afb5f1e56bac235b25bec1062732b1dde5529c
Tyler Schwemley <tjschwem@gmail.com>
2026-05-13 19:22:43 -0400

    only include desktop entry for pc machine

--- home/programs/neovim/default.nix
+++ home/programs/neovim/default.nix
@@ -1,4 +1,9 @@
-{ lib, ... }:
+{
+  self,
+  lib,
+  nixosConfig,
+  ...
+}:
 {
   imports = [
     # ./env.nix
@@ -7,6 +12,8 @@
     ./plugins.nix
     ./treesitter.nix
   ];
+
+  nixpkgs.overlays = [ self.inputs.neovim-nightly-overlay.overlays.default ];
 
   programs.neovim = {
     enable = true;
@@ -22,5 +29,24 @@
   xdg = {
     configFile."nvim".source = ./config;
     dataFile."nvim/snippets".source = ./snippets;
+
+    desktopEntries.nvim-terminal =
+      with lib;
+      mkIf (isPC nixosConfig.networking.hostName) {
+        name = "Neovim (Terminal)";
+        genericName = "Text Editor";
+        exec = "wezterm -- nvim %F";
+        terminal = true;
+
+        categories = [
+          "Utility"
+          "TextEditor"
+        ];
+
+        mimeType = [
+          "application/xml"
+          "text/xml"
+        ];
+      };
   };
 }
