6f44e74732fc396d9b175d2e4839facbb43b6a9e
Tyler Schwemley <tjschwem@gmail.com>
2026-06-01 00:29:07 -0400

    update wine config packages

--- nixos/programs/wine.nix
+++ nixos/programs/wine.nix
@@ -1,9 +1,13 @@
 { pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-    # bottles
-    wineWow64Packages.waylandFull
+    wine-wayland
     winetricks
+
+    wine64
+    wine64Packages.waylandFull
+
+    # wineWow64Packages.waylandFull
 
     # deps for various wine packages
     samba
