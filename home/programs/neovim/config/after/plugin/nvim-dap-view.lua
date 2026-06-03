1c60e06ac4b63764bb4be796a3fd4c66383c763c
Tyler Schwemley <tjschwem@gmail.com>
2026-06-03 02:18:15 -0400

    nvim dap view config setup

---
+++ home/programs/neovim/config/after/plugin/nvim-dap-view.lua
@@ -0,0 +1,12 @@
+require("dap-view").setup({
+	winbar = {
+		default_section = "scopes",
+	},
+
+	auto_toggle = true,
+
+	-- Reopen dapview when switching to a different tab
+	-- Can also be a function to dynamically choose when to follow, by returning a boolean
+	-- If a function, receives the name of the adapter for the current session as an argument
+	follow_tab = false,
+})
