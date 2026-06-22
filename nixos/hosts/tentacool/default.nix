95c2a2812ca8020521f157a07dfffb76c18bf787
Tyler Schwemley <tjschwem@gmail.com>
2026-06-22 05:36:53 -0400

    tentacool style changes

--- nixos/hosts/tentacool/default.nix
+++ nixos/hosts/tentacool/default.nix
@@ -1,5 +1,5 @@
 {
-  inputs,
+  self,
   lib,
   pkgs,
   ...
@@ -10,13 +10,13 @@ let
 in
 {
   imports = [
-    # hardware
+    self.inputs.nixos-hardware.nixosModules.common-cpu-intel
+    self.inputs.nixos-hardware.nixosModules.common-gpu-intel
+    self.inputs.nixos-hardware.nixosModules.common-gpu-nvidia
+    self.inputs.nixos-hardware.nixosModules.common-pc-laptop
+    self.inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
+
     rootDisk
-    inputs.nixos-hardware.nixosModules.common-cpu-intel
-    inputs.nixos-hardware.nixosModules.common-gpu-intel
-    inputs.nixos-hardware.nixosModules.common-gpu-nvidia
-    inputs.nixos-hardware.nixosModules.common-pc-laptop
-    inputs.nixos-hardware.nixosModules.common-pc-laptop-ssd
 
     # system + profile
     ../../system/boot/systemd.nix
