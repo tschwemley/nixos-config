3325b94f41f863c972254a9dce3e203f33bfdf82
Tyler Schwemley <tjschwem@gmail.com>
2026-06-22 05:34:01 -0400

    increase terminal font size

--- home/terminal/wezterm/config/fonts.lua
+++ home/terminal/wezterm/config/fonts.lua
@@ -5,7 +5,7 @@ local M = {}
 function M.apply_to_config(config)
 	config.cell_width = 1.03
 	config.font = wezterm.font("CaskaydiaCove Nerd Font")
-	config.font_size = 14
+	config.font_size = 16
 	config.line_height = 1.05
 end
 
