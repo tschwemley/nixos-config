c7381da5767039c6288874d302c3e74c0bdc43f2
Tyler Schwemley <tjschwem@gmail.com>
2026-06-22 05:38:17 -0400

    creative programs config updates

--- home/programs/creative/default.nix
+++ home/programs/creative/default.nix
@@ -1,12 +1,11 @@
 {
   imports = [
-    # TODO: uncomment cad/krita once build issues go away (2026-02-27)
     ./cad.nix
     ./gimp.nix
-    # ./inkscape.nix
-    # ./krita.nix
+    ./inkscape.nix
     ./orca-slicer.nix
 
+    # TODO: fix aseprite w/ plugins
     # ./aseprite.nix
   ];
 }
