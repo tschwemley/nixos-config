7d4cd381dcf349fc42910b0e3ad0c7d961885ec3
Tyler Schwemley <tjschwem@gmail.com>
2026-06-22 05:29:58 -0400

    update flake

--- flake.nix
+++ flake.nix
@@ -82,8 +82,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nur = {
-      url = "github:nix-community/NUR";
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
