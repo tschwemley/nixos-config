6de923d830463e5880d273a36f34d4431c0b3159
Tyler Schwemley <tjschwem@gmail.com>
2026-07-14 06:35:55 -0400

    add fonts to home-manager minimal config

--- home/profiles/minimal.nix
+++ home/profiles/minimal.nix
@@ -1,4 +1,4 @@
-{ username, ... }: {
+{ pkgs, username, ... }: {
   imports = [
     ../programs/neovim
     # ../programs/yazi
@@ -12,5 +12,43 @@
     homeDirectory = "/Users/${username}";
     sessionVariables.TERM = "wezterm";
     stateVersion = "26.05";
+
+    packages = with pkgs; [
+      fontconfig
+      search-nf
+
+      gum # TODO: remove me after testing
+    ];
+  };
+
+  fonts = {
+    fontconfig = {
+      enable = true;
+
+      # defaultFonts = {
+      #   emoji = [ "Twitter Color Emoji" ];
+      #   monospace = [ "CaskaydiaCove Nerd Font" ];
+      #   serif = [ "Inter" ];
+      #   sansSerif = [ "Inter" ];
+      # };
+
+      # Hinting aligns glyphs to pixel boundaries to improve rendering sharpness at low resolution
+      hinting = {
+        enable = true;
+
+        # slight (fuzzier but more grid align, retains shape) -> medium -> full (opposite slight)
+        style = "full";
+      };
+
+      useEmbeddedBitmaps = true;
+    };
+
+    fontDir.enable = true;
+
+    packages = with pkgs; [
+      inter # used for reaper theme(s)
+      nerd-fonts.caskaydia-cove
+      twemoji-color-font
+    ];
   };
 }
